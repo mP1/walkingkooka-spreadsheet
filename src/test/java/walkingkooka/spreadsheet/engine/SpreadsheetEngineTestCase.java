f8560598a0aa33b9b93849090cc7a05d9b7b27f9
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2018-09-13 23:19:11 +1000

    Renamed SpreadsheetEngine loadCell/saveCell/deleteCell

--- src/test/java/walkingkooka/spreadsheet/engine/SpreadsheetEngineTestCase.java
+++ src/test/java/walkingkooka/spreadsheet/engine/SpreadsheetEngineTestCase.java
@@ -15,67 +15,67 @@ import static org.junit.Assert.fail;
 public abstract class SpreadsheetEngineTestCase<E extends SpreadsheetEngine> extends PackagePrivateClassTestCase<E> {
 
     @Test(expected = NullPointerException.class)
-    public final void testLoadNullCellFails() {
-        this.createSpreadsheetEngine().load(null, SpreadsheetEngineLoading.COMPUTE_IF_NECESSARY);
+    public final void testLoadCellNullCellFails() {
+        this.createSpreadsheetEngine().loadCell(null, SpreadsheetEngineLoading.COMPUTE_IF_NECESSARY);
     }
 
     @Test(expected = NullPointerException.class)
-    public final void testLoadNullLoadingFails() {
-        this.createSpreadsheetEngine().load(SpreadsheetCellReference.with(SpreadsheetReferenceKind.ABSOLUTE.column(1), SpreadsheetReferenceKind.ABSOLUTE.row(2)),
+    public final void testLoadCellNullLoadingFails() {
+        this.createSpreadsheetEngine().loadCell(SpreadsheetCellReference.with(SpreadsheetReferenceKind.ABSOLUTE.column(1), SpreadsheetReferenceKind.ABSOLUTE.row(2)),
                 null);
     }
 
     @Test(expected = NullPointerException.class)
     public final void testSetNullCellFails() {
-        this.createSpreadsheetEngine().save(null);
+        this.createSpreadsheetEngine().saveCell(null);
     }
 
     abstract E createSpreadsheetEngine();
 
-    final SpreadsheetCell loadOrFail(final SpreadsheetEngine engine,
-                                     final SpreadsheetCellReference reference,
-                                     final SpreadsheetEngineLoading loading) {
-        final Optional<SpreadsheetCell> cell = engine.load(reference, loading);
+    final SpreadsheetCell loadCellOrFail(final SpreadsheetEngine engine,
+                                         final SpreadsheetCellReference reference,
+                                         final SpreadsheetEngineLoading loading) {
+        final Optional<SpreadsheetCell> cell = engine.loadCell(reference, loading);
         if(!cell.isPresent()) {
             fail("Loading " + reference + " should have succeeded");
         }
         return cell.get();
     }
 
-    final void loadFailCheck(final SpreadsheetCellReference reference,
-                             final SpreadsheetEngineLoading loading) {
-        this.loadFailCheck(this.createSpreadsheetEngine(), reference, loading);
+    final void loadCellFailCheck(final SpreadsheetCellReference reference,
+                                 final SpreadsheetEngineLoading loading) {
+        this.loadCellFailCheck(this.createSpreadsheetEngine(), reference, loading);
     }
 
-    final void loadFailCheck(final SpreadsheetEngine engine,
-                             final SpreadsheetCellReference reference,
-                             final SpreadsheetEngineLoading loading) {
-        final Optional<SpreadsheetCell> cell = engine.load(reference, loading);
+    final void loadCellFailCheck(final SpreadsheetEngine engine,
+                                 final SpreadsheetCellReference reference,
+                                 final SpreadsheetEngineLoading loading) {
+        final Optional<SpreadsheetCell> cell = engine.loadCell(reference, loading);
         assertEquals("Expected reference " + reference + " to fail", Optional.empty(), cell);
     }
 
-    final void loadAndCheckWithoutValueOrError(final SpreadsheetCellReference reference, final SpreadsheetEngineLoading loading) {
-        this.loadAndCheckWithoutValueOrError(this.createSpreadsheetEngine(), reference, loading);
+    final void loadCellAndCheckWithoutValueOrError(final SpreadsheetCellReference reference, final SpreadsheetEngineLoading loading) {
+        this.loadCellAndCheckWithoutValueOrError(this.createSpreadsheetEngine(), reference, loading);
     }
 
-    final void loadAndCheckWithoutValueOrError(final SpreadsheetEngine engine,
-                                               final SpreadsheetCellReference reference,
-                                               final SpreadsheetEngineLoading loading) {
-        final SpreadsheetCell cell = this.loadOrFail(engine, reference, loading);
+    final void loadCellAndCheckWithoutValueOrError(final SpreadsheetEngine engine,
+                                                   final SpreadsheetCellReference reference,
+                                                   final SpreadsheetEngineLoading loading) {
+        final SpreadsheetCell cell = this.loadCellOrFail(engine, reference, loading);
         assertEquals("values from returned cells=" + cell,
                 null,
                 this.valueOrError(cell, null));
     }
 
-    final void loadAndCheck(final SpreadsheetCellReference reference, final SpreadsheetEngineLoading loading, final Object value) {
-        this.loadAndCheck(this.createSpreadsheetEngine(), reference, loading, value);
+    final void loadCellAndCheck(final SpreadsheetCellReference reference, final SpreadsheetEngineLoading loading, final Object value) {
+        this.loadCellAndCheck(this.createSpreadsheetEngine(), reference, loading, value);
     }
 
-    final void loadAndCheck(final SpreadsheetEngine engine,
-                            final SpreadsheetCellReference reference,
-                            final SpreadsheetEngineLoading loading,
-                            final Object value) {
-        final SpreadsheetCell cell = this.loadOrFail(engine, reference, loading);
+    final void loadCellAndCheck(final SpreadsheetEngine engine,
+                                final SpreadsheetCellReference reference,
+                                final SpreadsheetEngineLoading loading,
+                                final Object value) {
+        final SpreadsheetCell cell = this.loadCellOrFail(engine, reference, loading);
         assertEquals("values from returned cells=" + cell,
                 value,
                 this.valueOrError(cell, "Value and Error absent (" + cell + ")"));
