7446885d97fde56606b5affc07f7253092477962
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2018-09-14 20:16:09 +1000

    Added GuavaTableSpreadsheetCellStore.

--- src/main/java/walkingkooka/spreadsheet/store/GuavaTableSpreadsheetCellStore.java
+++ src/main/java/walkingkooka/spreadsheet/store/GuavaTableSpreadsheetCellStore.java
@@ -5,8 +5,10 @@ import com.google.common.collect.TreeBasedTable;
 import org.checkerframework.checker.units.qual.C;
 import walkingkooka.spreadsheet.SpreadsheetCell;
 import walkingkooka.text.cursor.parser.spreadsheet.SpreadsheetCellReference;
+import walkingkooka.text.cursor.parser.spreadsheet.SpreadsheetColumnReference;
 import walkingkooka.text.cursor.parser.spreadsheet.SpreadsheetRowReference;
 
+import java.util.Objects;
 import java.util.Optional;
 
 /**
@@ -14,28 +16,46 @@ import java.util.Optional;
  */
 final class GuavaTableSpreadsheetCellStore implements SpreadsheetCellStore{
 
+    /**
+     * Creates an empty {@link SpreadsheetCellStore} which uses a {@link Table}
+     */
     static GuavaTableSpreadsheetCellStore create() {
         return new GuavaTableSpreadsheetCellStore();
     }
 
+    /**
+     * Private ctor.
+     */
     private GuavaTableSpreadsheetCellStore() {
         super();
     }
 
     @Override
     public Optional<SpreadsheetCell> load(final SpreadsheetCellReference reference) {
-        return Optional.empty();
+        Objects.requireNonNull(reference, "reference");
+
+        return Optional.ofNullable(this.cells.get(reference.row(), reference.column()));
     }
 
     @Override
     public void save(final SpreadsheetCell cell) {
+        Objects.requireNonNull(cell, "cell");
 
+        final SpreadsheetCellReference reference = cell.reference();
+        this.cells.put(reference.row(), reference.column(), cell);
     }
 
     @Override
     public void delete(final SpreadsheetCellReference reference) {
+        Objects.requireNonNull(reference, "reference");
 
+        this.cells.remove(reference.row(), reference.column());
     }
 
-    private Table<SpreadsheetRowReference, SpreadsheetCellReference, SpreadsheetCell> cells = TreeBasedTable.create();
+    private Table<SpreadsheetRowReference, SpreadsheetColumnReference, SpreadsheetCell> cells = TreeBasedTable.create();
+
+    @Override
+    public String toString() {
+        return this.cells.toString();
+    }
 }
