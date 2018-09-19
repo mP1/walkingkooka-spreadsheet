4d5cd921b5a6af62b52fa866af03a5d06732d87d
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2018-09-19 18:08:36 +1000

    Moved SpreadsheetCell expression, value, error to SpreadsheetFormula.
    - SpreadsheetFormula holds the cached expression/value/error.

--- src/test/java/walkingkooka/spreadsheet/SpreadsheetCellEqualityTest.java
+++ src/test/java/walkingkooka/spreadsheet/SpreadsheetCellEqualityTest.java
@@ -12,44 +12,22 @@ public final class SpreadsheetCellEqualityTest extends HashCodeEqualsDefinedEqua
 
     @Test
     public void testDifferentReference() {
-        this.checkNotEquals(this.cell(reference(88, 99), this.formula(), this.expression(), this.value(), this.error()));
+        this.checkNotEquals(this.cell(reference(88, 99), this.formula()));
     }
 
     @Test
     public void testDifferentFormula() {
-        this.checkNotEquals(this.cell(reference(),this.formula("=99+999"), this.expression(), this.value(), this.error()));
-    }
-
-    @Test
-    public void testDifferentExpression() {
-        checkNotEquals(this.cell(reference(), this.formula(), this.expression("44"), this.value(), this.error()));
-    }
-
-    @Test
-    public void testDifferentValue() {
-        checkNotEquals(this.cell(reference(), this.formula(), this.expression(), this.value(), SpreadsheetCell.NO_ERROR),
-                this.cell(reference(), this.formula(), this.expression(), this.value("different-value"), SpreadsheetCell.NO_ERROR));
-    }
-
-    @Test
-    public void testDifferentError() {
-        checkNotEquals(this.cell(reference(), this.formula(), this.expression(), this.value(), this.error("different error message")));
+        this.checkNotEquals(this.cell(reference(),this.formula("=99+999")));
     }
 
     @Override
     protected SpreadsheetCell createObject() {
-        return this.cell(reference(), this.formula(), this.expression(), this.value(), this.error());
+        return this.cell(reference(), this.formula());
     }
 
     private SpreadsheetCell cell(final SpreadsheetCellReference reference,
-                                 final SpreadsheetFormula formula,
-                                 final Optional<ExpressionNode> expression,
-                                 final Optional<Object> value,
-                                 final Optional<SpreadsheetError> error) {
-        return SpreadsheetCell.with(reference, formula)
-                .setExpression(expression)
-                .setValue(value)
-                .setError(error);
+                                 final SpreadsheetFormula formula) {
+        return SpreadsheetCell.with(reference, formula);
     }
 
     private static SpreadsheetCellReference reference() {
@@ -66,29 +44,5 @@ public final class SpreadsheetCellEqualityTest extends HashCodeEqualsDefinedEqua
 
     private SpreadsheetFormula formula() {
         return this.formula("=1+2");
-    }
-
-    private Optional<ExpressionNode> expression(final String text) {
-        return Optional.of(ExpressionNode.text(text));
-    }
-
-    private Optional<ExpressionNode> expression() {
-        return this.expression("=1+2");
-    }
-
-    private Optional<Object> value(final Object text) {
-        return Optional.of(text);
-    }
-
-    private Optional<Object> value() {
-        return this.value(3);
-    }
-
-    private Optional<SpreadsheetError> error(final String text) {
-        return Optional.of(SpreadsheetError.with(text));
-    }
-
-    private Optional<SpreadsheetError> error() {
-        return this.error("Error message #1");
     }
 }
