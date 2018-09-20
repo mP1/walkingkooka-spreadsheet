cc138836d2ffa49819680e6f4685d5e9292657ab
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2018-09-20 17:03:53 +1000

    Defined enums: HorizontalAlignment, VerticalAlignment, TextWrapping

---
+++ src/test/java/walkingkooka/spreadsheet/style/VerticalAlignmentTest.java
@@ -0,0 +1,11 @@
+package walkingkooka.spreadsheet.style;
+
+import walkingkooka.test.EnumTestCase;
+
+public final class VerticalAlignmentTest extends EnumTestCase<VerticalAlignment> {
+
+    @Override
+    protected Class<VerticalAlignment> type() {
+        return VerticalAlignment.class;
+    }
+}
