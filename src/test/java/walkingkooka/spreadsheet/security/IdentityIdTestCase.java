12f4bf68bbfb661c97c8b572f51bb9eef65873de
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2018-11-12 13:47:12 +1100

    User, UserId, Group, GroupId, GroupName.

---
+++ src/test/java/walkingkooka/spreadsheet/security/IdentityIdTestCase.java
@@ -0,0 +1,10 @@
+package walkingkooka.spreadsheet.security;
+
+import walkingkooka.test.PublicClassTestCase;
+
+public abstract class IdentityIdTestCase<I extends IdentityId> extends PublicClassTestCase<I> {
+
+    IdentityIdTestCase() {
+        super();
+    }
+}
