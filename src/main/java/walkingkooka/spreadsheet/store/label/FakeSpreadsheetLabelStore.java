c2fcd91abe27dec6599629a8430c4e53b08f0164
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2018-11-12 17:47:05 +1100

    FakeStore introduced, reworked other FakeXXXStores to sub class.

--- src/main/java/walkingkooka/spreadsheet/store/label/FakeSpreadsheetLabelStore.java
+++ src/main/java/walkingkooka/spreadsheet/store/label/FakeSpreadsheetLabelStore.java
@@ -1,6 +1,7 @@
 package walkingkooka.spreadsheet.store.label;
 
 import walkingkooka.spreadsheet.SpreadsheetLabelMapping;
+import walkingkooka.spreadsheet.store.FakeStore;
 import walkingkooka.test.Fake;
 import walkingkooka.text.cursor.parser.spreadsheet.SpreadsheetLabelName;
 
@@ -8,30 +9,7 @@ import java.util.Collection;
 import java.util.Objects;
 import java.util.Optional;
 
-public class FakeSpreadsheetLabelStore implements SpreadsheetLabelStore, Fake {
-
-    @Override
-    public Optional<SpreadsheetLabelMapping> load(final SpreadsheetLabelName label) {
-        Objects.requireNonNull(label, "label");
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public void save(final SpreadsheetLabelMapping mapping) {
-        Objects.requireNonNull(mapping, "mapping");
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public void delete(final SpreadsheetLabelName label) {
-        Objects.requireNonNull(label, "label");
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public int count() {
-        throw new UnsupportedOperationException();
-    }
+public class FakeSpreadsheetLabelStore extends FakeStore<SpreadsheetLabelName, SpreadsheetLabelMapping> implements SpreadsheetLabelStore, Fake {
 
     @Override
     public Collection<SpreadsheetLabelMapping> all() {
