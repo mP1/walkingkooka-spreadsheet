efcefb11a66183880459d63334088a858eb9f4e3
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2018-11-12 17:54:57 +1100

    UserStore, fake and testcase.

---
+++ src/main/java/walkingkooka/spreadsheet/store/security/SpreadsheetUserStore.java
@@ -0,0 +1,17 @@
+package walkingkooka.spreadsheet.store.security;
+
+import walkingkooka.net.email.EmailAddress;
+import walkingkooka.spreadsheet.security.User;
+import walkingkooka.spreadsheet.security.UserId;
+import walkingkooka.spreadsheet.store.Store;
+
+/**
+ * A {@link Store} that holds all users.
+ */
+public interface SpreadsheetUserStore extends Store<User, UserId> {
+
+    /**
+     * Loads the user with the provided {@link walkingkooka.net.email.EmailAddress}
+     */
+    User loadWithEmail(final EmailAddress email);
+}
