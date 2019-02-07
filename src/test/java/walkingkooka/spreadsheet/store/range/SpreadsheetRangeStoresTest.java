4faaf889eb86b85fc825270aa0c8b61d503051b9
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2019-02-07 20:41:15 +1100

    walkingkooka-d4ac26b447f0f79ab09d366abbe91a05c0bd9115 (2019/02/07)

--- src/test/java/walkingkooka/spreadsheet/store/range/SpreadsheetRangeStoresTest.java
+++ src/test/java/walkingkooka/spreadsheet/store/range/SpreadsheetRangeStoresTest.java
@@ -1,17 +1,26 @@
 package walkingkooka.spreadsheet.store.range;
 
-import walkingkooka.test.PublicStaticHelperTestCase;
+import walkingkooka.test.ClassTestCase;
+import walkingkooka.test.PublicStaticHelperTesting;
+import walkingkooka.type.MemberVisibility;
 
 import java.lang.reflect.Method;
 
-public final class SpreadsheetRangeStoresTest extends PublicStaticHelperTestCase<SpreadsheetRangeStores> {
+public final class SpreadsheetRangeStoresTest extends ClassTestCase<SpreadsheetRangeStores>
+        implements PublicStaticHelperTesting<SpreadsheetRangeStores> {
+
     @Override
     public Class<SpreadsheetRangeStores> type() {
         return SpreadsheetRangeStores.class;
     }
 
     @Override
-    protected boolean canHavePublicTypes(final Method method) {
+    public boolean canHavePublicTypes(final Method method) {
         return false;
+    }
+
+    @Override
+    public MemberVisibility typeVisibility() {
+        return MemberVisibility.PUBLIC;
     }
 }
