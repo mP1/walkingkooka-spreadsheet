188a16d5431c425f78b151735606b629729df633
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2019-02-18 21:37:03 +1100

    UserId & GroupId implement HasJsonNode.

--- src/main/java/walkingkooka/spreadsheet/security/UserId.java
+++ src/main/java/walkingkooka/spreadsheet/security/UserId.java
@@ -1,9 +1,15 @@
 package walkingkooka.spreadsheet.security;
 
+import walkingkooka.tree.json.JsonNode;
+
 /**
  * The primary key for a {@link User}.
  */
 public final class UserId extends IdentityId {
+
+    public static UserId fromJsonNode(final JsonNode node) {
+        return with(JsonNode.fromJsonNodeLong(node));
+    }
 
     static UserId with(final long value) {
         return new UserId(value);
