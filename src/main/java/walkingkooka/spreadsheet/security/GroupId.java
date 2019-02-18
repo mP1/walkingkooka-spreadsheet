b9cf4f4d6e4b0ba4fc6aed9d2a6edd24fd4bd1dc
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2019-02-18 21:37:03 +1100

    UserId & GroupId implement HasJsonNode.

--- src/main/java/walkingkooka/spreadsheet/security/GroupId.java
+++ src/main/java/walkingkooka/spreadsheet/security/GroupId.java
@@ -1,9 +1,15 @@
 package walkingkooka.spreadsheet.security;
 
+import walkingkooka.tree.json.JsonNode;
+
 /**
  * The primary key for a {@link Group}.
  */
 public final class GroupId extends IdentityId {
+
+    public static GroupId fromJsonNode(final JsonNode node) {
+        return with(JsonNode.fromJsonNodeLong(node));
+    }
 
     static GroupId with(final long value) {
         return new GroupId(value);
--- src/test/java/walkingkooka/spreadsheet/security/IdentityIdTestCase.java
+++ src/test/java/walkingkooka/spreadsheet/security/IdentityIdTestCase.java
@@ -1,18 +1,43 @@
 package walkingkooka.spreadsheet.security;
 
+import org.junit.jupiter.api.Test;
 import walkingkooka.test.ClassTesting2;
 import walkingkooka.test.ToStringTesting;
+import walkingkooka.tree.json.HasJsonNodeTesting;
+import walkingkooka.tree.json.JsonNode;
 import walkingkooka.type.MemberVisibility;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public abstract class IdentityIdTestCase<I extends IdentityId> implements ClassTesting2<I>,
+        HasJsonNodeTesting<I>,
         ToStringTesting<I> {
 
     IdentityIdTestCase() {
         super();
     }
 
+    @Test
+    public final void testCreate() {
+        final Long value = 123L;
+        final I id = this.createId(value);
+        assertEquals(value, id.value(), "value");
+    }
+
+    @Test
+    public final void testToJson() {
+        this.toJsonNodeAndCheck(this.createId(123L), JsonNode.wrapLong(123L));
+    }
+
+    @Test
+    public final void testToJsonRoundtrip() {
+        this.toJsonNodeRoundTripTwiceAndCheck(this.createId(123L));
+    }
+
     @Override
     public final MemberVisibility typeVisibility() {
         return MemberVisibility.PUBLIC;
     }
+
+    abstract I createId(long value);
 }
