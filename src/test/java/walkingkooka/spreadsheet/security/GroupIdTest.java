09523bc9f44ee1f570ba9bfba8a097cca92109c5
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2019-02-18 21:37:03 +1100

    UserId & GroupId implement HasJsonNode.

--- src/test/java/walkingkooka/spreadsheet/security/GroupIdTest.java
+++ src/test/java/walkingkooka/spreadsheet/security/GroupIdTest.java
@@ -1,6 +1,8 @@
 package walkingkooka.spreadsheet.security;
 
 import org.junit.jupiter.api.Test;
+import walkingkooka.net.header.Link;
+import walkingkooka.tree.json.JsonNode;
 
 public final class GroupIdTest extends IdentityIdTestCase<GroupId> {
 
@@ -10,7 +12,19 @@ public final class GroupIdTest extends IdentityIdTestCase<GroupId> {
     }
 
     @Override
+    GroupId createId(final long value) {
+        return GroupId.with(value);
+    }
+
+    @Override
     public Class<GroupId> type() {
         return GroupId.class;
+    }
+
+    // HasJsonNodeTesting..................................................................
+
+    @Override
+    public final GroupId fromJsonNode(final JsonNode from) {
+        return GroupId.fromJsonNode(from);
     }
 }
