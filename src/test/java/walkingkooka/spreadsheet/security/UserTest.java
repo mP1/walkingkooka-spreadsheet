8c5f77f356bf3182866ace187814a0c689b5c5db
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2019-02-18 21:50:54 +1100

    User implements HasJsonNode

--- src/test/java/walkingkooka/spreadsheet/security/UserTest.java
+++ src/test/java/walkingkooka/spreadsheet/security/UserTest.java
@@ -2,11 +2,14 @@ package walkingkooka.spreadsheet.security;
 
 import org.junit.jupiter.api.Test;
 import walkingkooka.net.email.EmailAddress;
+import walkingkooka.tree.json.HasJsonNodeTesting;
+import walkingkooka.tree.json.JsonNode;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public final class UserTest implements IdentityTesting<User, UserId> {
+public final class UserTest implements IdentityTesting<User, UserId>,
+        HasJsonNodeTesting<User> {
 
     private final static EmailAddress EMAIL = EmailAddress.parse("user@example.com");
 
@@ -24,6 +27,57 @@ public final class UserTest implements IdentityTesting<User, UserId> {
         assertEquals(id, user.value(), "id");
         assertEquals(EMAIL, user.email(), "email");
     }
+
+    // HasJsonNodeTesting.................................................................................................
+
+    @Test
+    public void testFromJsonNodeBooleanFails() {
+        this.fromJsonNodeFails(JsonNode.booleanNode(true));
+    }
+
+    @Test
+    public void testFromJsonNodeNullFails() {
+        this.fromJsonNodeFails(JsonNode.nullNode());
+    }
+
+    @Test
+    public void testFromJsonNodeNumberFails() {
+        this.fromJsonNodeFails(JsonNode.number(12));
+    }
+
+    @Test
+    public void testFromJsonNodeArrayFails() {
+        this.fromJsonNodeFails(JsonNode.array());
+    }
+
+    @Test
+    public void testFromJsonNodeObjectFails() {
+        this.fromJsonNodeFails(JsonNode.object());
+    }
+
+    @Test
+    public void testFromJsonNode() {
+        this.fromJsonNodeAndCheck(this.jsonNode(),
+                User.with(this.createId(), EMAIL));
+    }
+
+    @Test
+    public void testToJsonNode() {
+        this.toJsonNodeAndCheck(this.createObject(), this.jsonNode());
+    }
+
+    private JsonNode jsonNode() {
+        return JsonNode.object()
+                .set(User.ID_PROPERTY, createId().toJsonNode())
+                .set(User.EMAIL_PROPERTY, EMAIL.toJsonNode());
+    }
+
+    @Test
+    public void testToJsonNodeRoundtripTwice() {
+        this.toJsonNodeRoundTripTwiceAndCheck(this.createObject());
+    }
+
+    // ToStringTesting.................................................................................................
 
     @Test
     public void testToString() {
@@ -43,5 +97,12 @@ public final class UserTest implements IdentityTesting<User, UserId> {
     @Override
     public Class<User> type() {
         return User.class;
+    }
+
+    // HasJsonNodeTesting..................................................................
+
+    @Override
+    public final User fromJsonNode(final JsonNode from) {
+        return User.fromJsonNode(from);
     }
 }
