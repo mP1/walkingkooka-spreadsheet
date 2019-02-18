a6af88ed5a3175b7eba8d833c3137404f57888ed
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2019-02-18 22:04:26 +1100

    Group implements HasJsonNode

--- src/test/java/walkingkooka/spreadsheet/security/GroupTest.java
+++ src/test/java/walkingkooka/spreadsheet/security/GroupTest.java
@@ -2,11 +2,14 @@ package walkingkooka.spreadsheet.security;
 
 import org.junit.jupiter.api.Test;
 import walkingkooka.net.email.EmailAddress;
+import walkingkooka.tree.json.HasJsonNodeTesting;
+import walkingkooka.tree.json.JsonNode;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public final class GroupTest implements IdentityTesting<Group, GroupId> {
+public final class GroupTest implements IdentityTesting<Group, GroupId>,
+        HasJsonNodeTesting<Group> {
 
     private final long ID_VALUE = 123;
 
@@ -35,6 +38,55 @@ public final class GroupTest implements IdentityTesting<Group, GroupId> {
         this.checkNotEquals(User.with(UserId.with(ID_VALUE), EmailAddress.parse("user@example.com")));
     }
 
+    // HasJsonNodeTesting.................................................................................................
+
+    @Test
+    public void testFromJsonNodeBooleanFails() {
+        this.fromJsonNodeFails(JsonNode.booleanNode(true));
+    }
+
+    @Test
+    public void testFromJsonNodeNullFails() {
+        this.fromJsonNodeFails(JsonNode.nullNode());
+    }
+
+    @Test
+    public void testFromJsonNodeNumberFails() {
+        this.fromJsonNodeFails(JsonNode.number(12));
+    }
+
+    @Test
+    public void testFromJsonNodeArrayFails() {
+        this.fromJsonNodeFails(JsonNode.array());
+    }
+
+    @Test
+    public void testFromJsonNodeObjectFails() {
+        this.fromJsonNodeFails(JsonNode.object());
+    }
+
+    @Test
+    public void testFromJsonNode() {
+        this.fromJsonNodeAndCheck(this.jsonNode(),
+                Group.with(this.createId(), this.name()));
+    }
+
+    @Test
+    public void testToJsonNode() {
+        this.toJsonNodeAndCheck(this.createObject(), this.jsonNode());
+    }
+
+    private JsonNode jsonNode() {
+        return JsonNode.object()
+                .set(Group.ID_PROPERTY, createId().toJsonNode())
+                .set(Group.NAME_PROPERTY, this.name().toJsonNode());
+    }
+
+    @Test
+    public void testToJsonNodeRoundtripTwice() {
+        this.toJsonNodeRoundTripTwiceAndCheck(this.createObject());
+    }
+
     @Test
     public void testToString() {
         this.toStringAndCheck(Group.with(GroupId.with(ID_VALUE), name()), name().toString());
@@ -57,5 +109,12 @@ public final class GroupTest implements IdentityTesting<Group, GroupId> {
     @Override
     public Class<Group> type() {
         return Group.class;
+    }
+
+    // HasJsonNodeTesting..................................................................
+
+    @Override
+    public final Group fromJsonNode(final JsonNode from) {
+        return Group.fromJsonNode(from);
     }
 }
