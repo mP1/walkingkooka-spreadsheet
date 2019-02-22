dc304bfb2ed0289a1724388b91e5db46cfd7b732
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2019-02-22 14:07:46 +1100

    SpreadsheetEngineLoadCellHateosGetHandler (SpreadsheetEngine.loadCell)

---
+++ src/test/java/walkingkooka/spreadsheet/hateos/SpreadsheetHateosHandlerTestCase2.java
@@ -0,0 +1,37 @@
+package walkingkooka.spreadsheet.hateos;
+
+import org.junit.jupiter.api.Test;
+import walkingkooka.net.http.server.hateos.HateosContentType;
+import walkingkooka.net.http.server.hateos.HateosHandler;
+import walkingkooka.net.http.server.hateos.HateosHandlerTesting;
+import walkingkooka.test.TestSuiteNameTesting;
+import walkingkooka.tree.json.HasJsonNode;
+import walkingkooka.tree.json.JsonNode;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public abstract class SpreadsheetHateosHandlerTestCase2<H extends HateosHandler<I, JsonNode>, I extends Comparable<I>> extends SpreadsheetHateosHandlerTestCase<H>
+        implements HateosHandlerTesting<H, I, JsonNode> {
+
+    SpreadsheetHateosHandlerTestCase2() {
+        super();
+    }
+
+    @Test
+    public final void testWithNullContentTypeFails() {
+        assertThrows(NullPointerException.class, () -> {
+           this.createHandler(null);
+        });
+    }
+
+    @Override
+    public final H createHandler() {
+        return this.createHandler(this.contentType());
+    }
+
+    final HateosContentType<JsonNode, HasJsonNode> contentType() {
+        return HateosContentType.JSON;
+    }
+
+    abstract H createHandler(final HateosContentType<JsonNode, HasJsonNode> type);
+}
