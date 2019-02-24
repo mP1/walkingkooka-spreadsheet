3bb605d5015cc6eb156a7d74aab5001c8f6c3aed
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2019-02-24 17:45:40 +1100

    SpreadsheetEngineRowHateosDeleteHandler

---
+++ src/test/java/walkingkooka/spreadsheet/hateos/SpreadsheetEngineRowHateosHandlerTest.java
@@ -0,0 +1,16 @@
+package walkingkooka.spreadsheet.hateos;
+
+import walkingkooka.net.http.server.hateos.HateosHandler;
+
+public final class SpreadsheetEngineRowHateosHandlerTest extends SpreadsheetHateosHandlerTestCase<SpreadsheetEngineRowHateosHandler> {
+
+    @Override
+    public Class<SpreadsheetEngineRowHateosHandler> type() {
+        return SpreadsheetEngineRowHateosHandler.class;
+    }
+
+    @Override
+    public String typeNameSuffix() {
+        return HateosHandler.class.getSimpleName();
+    }
+}
