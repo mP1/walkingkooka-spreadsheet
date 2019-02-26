282a0a8d66a06014bb7a376b8eb7c5b40e1b77a7
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2019-02-26 14:10:56 +1100

    SpreadsheetEngineCopyCellsHateosHandler

--- src/main/java/walkingkooka/spreadsheet/hateos/SpreadsheetHateosHandler.java
+++ src/main/java/walkingkooka/spreadsheet/hateos/SpreadsheetHateosHandler.java
@@ -1,7 +1,15 @@
 package walkingkooka.spreadsheet.hateos;
 
+import walkingkooka.net.UrlParameterName;
+import walkingkooka.net.http.server.HttpRequestAttribute;
 import walkingkooka.net.http.server.hateos.HateosHandler;
 import walkingkooka.net.http.server.hateos.HateosResource;
+import walkingkooka.text.CharSequences;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
 
 /**
  * Base class for all handlers.
@@ -13,5 +21,29 @@ abstract class SpreadsheetHateosHandler<I extends Comparable<I>, R extends Hateo
      */
     SpreadsheetHateosHandler() {
         super();
+    }
+
+    /**
+     * Fetches a required single parameter value and converts or fails.
+     */
+    final <T> T parameterValueOrFail(final Map<HttpRequestAttribute<?>, Object> parameters,
+                                     final UrlParameterName parameter,
+                                     final Function<String, T> converter) {
+        final Optional<List<String>> maybeValues = parameter.parameterValue(parameters);
+        if (!maybeValues.isPresent()) {
+            throw new IllegalArgumentException("Required parameter " + parameter + " missing");
+        }
+        final List<String> values = maybeValues.get();
+        if (values.size() != 1) {
+            throw new IllegalArgumentException("Required parameter " + parameter + " incorrect=" + values);
+        }
+        final String value = values.get(0);
+        try {
+            return converter.apply(value);
+        } catch (final NullPointerException | IllegalArgumentException cause) {
+            throw cause;
+        } catch (final Exception cause) {
+            throw new IllegalArgumentException("Invalid parameter " + parameter + " value " + CharSequences.quoteIfChars(value));
+        }
     }
 }
