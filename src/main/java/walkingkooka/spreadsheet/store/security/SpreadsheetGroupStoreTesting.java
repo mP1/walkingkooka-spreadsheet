4c871482d6310d667402ffdd3ed27bbab05eeffb
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2019-03-13 20:21:54 +1100

    TreeMapSpreadsheetGroupStore
    - GroupStore.addUser, removeUser, loadUserGroups

--- src/main/java/walkingkooka/spreadsheet/store/security/SpreadsheetGroupStoreTesting.java
+++ src/main/java/walkingkooka/spreadsheet/store/security/SpreadsheetGroupStoreTesting.java
@@ -1,8 +1,21 @@
 package walkingkooka.spreadsheet.store.security;
 
-import walkingkooka.spreadsheet.security.User;
+import walkingkooka.spreadsheet.security.Group;
+import walkingkooka.spreadsheet.security.GroupId;
 import walkingkooka.spreadsheet.security.UserId;
 import walkingkooka.spreadsheet.store.StoreTesting;
 
-public interface SpreadsheetGroupStoreTesting<S extends SpreadsheetUserStore> extends StoreTesting<S, UserId, User> {
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public interface SpreadsheetGroupStoreTesting<S extends SpreadsheetGroupStore> extends StoreTesting<S, GroupId, Group> {
+
+    default void loadUserGroupsAndCheck(final SpreadsheetGroupStore store,
+                                        final UserId userId,
+                                        final Set<Group> groups) {
+        assertEquals(groups,
+                store.loadUserGroups(userId),
+                "store loadUserGroups " + userId + " incorrect result");
+    }
 }
