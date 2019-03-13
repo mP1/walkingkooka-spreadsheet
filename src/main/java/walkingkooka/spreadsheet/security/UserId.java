ff341a4a5712ac0e49fb30ec81d0b6863286c88e
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2019-03-13 16:05:54 +1100

    UserId implements Comparable

--- src/main/java/walkingkooka/spreadsheet/security/UserId.java
+++ src/main/java/walkingkooka/spreadsheet/security/UserId.java
@@ -6,7 +6,8 @@ import walkingkooka.tree.json.JsonNode;
 /**
  * The primary key for a {@link User}.
  */
-public final class UserId extends IdentityId {
+public final class UserId extends IdentityId
+        implements Comparable<UserId> {
 
     public static UserId fromJsonNode(final JsonNode node) {
         return with(node.fromJsonNode(Long.class));
@@ -31,5 +32,12 @@ public final class UserId extends IdentityId {
         HasJsonNode.register("user-id",
                 UserId::fromJsonNode,
                 UserId.class);
+    }
+
+    // Comparable..............................................................................................
+
+    @Override
+    public int compareTo(final UserId other) {
+        return this.compareTo0(other);
     }
 }
