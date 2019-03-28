f8481b396a07c427b1b06ce6fcfc36c3ae546617
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2019-03-28 23:59:10 +1100

    Store default methods, firstId, firstValue, all

--- src/main/java/walkingkooka/spreadsheet/store/Store.java
+++ src/main/java/walkingkooka/spreadsheet/store/Store.java
@@ -1,5 +1,7 @@
 package walkingkooka.spreadsheet.store;
 
+import walkingkooka.collect.list.Lists;
+
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
@@ -47,9 +49,44 @@ public interface Store<K, V> {
     Set<K> ids(final int from, final int count);
 
     /**
+     * Returns the first id or an {@link Optional#empty()}.
+     */
+    default Optional<K> firstId() {
+        final Set<K> first = this.ids(0, 1);
+        return first.stream().findFirst();
+    }
+
+    /**
      * Returns a view of all values between the range of ids.
      */
     List<V> values(final K from, final int count);
+
+    /**
+     * Fetches the first value if one is present.
+     */
+    default Optional<V> firstValue() {
+        V value = null;
+
+        final Optional<K> id = this.firstId();
+        if (id.isPresent()) {
+
+            final List<V> values = this.values(id.get(), 1);
+            if (values.size() > 0) {
+                value = values.iterator().next();
+            }
+        }
+
+        return Optional.ofNullable(value);
+    }
+
+    /**
+     * Returns all values in this store.
+     */
+    default List<V> all() {
+        return this.firstId()
+                .map((i) -> this.values(i, Integer.MAX_VALUE))
+                .orElse(Lists.empty());
+    }
 
     /**
      * Useful parameter checking for both {@link #ids}
