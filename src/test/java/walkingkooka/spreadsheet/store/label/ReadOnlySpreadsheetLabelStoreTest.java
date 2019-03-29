05e6423ecfc9038374bc0637720a90b0b6d8575a
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2019-03-29 23:17:32 +1100

    ReadOnlySpreadsheetLabelStore

---
+++ src/test/java/walkingkooka/spreadsheet/store/label/ReadOnlySpreadsheetLabelStoreTest.java
@@ -0,0 +1,96 @@
+package walkingkooka.spreadsheet.store.label;
+
+import org.junit.jupiter.api.Test;
+import walkingkooka.spreadsheet.SpreadsheetLabelMapping;
+import walkingkooka.text.cursor.parser.spreadsheet.SpreadsheetCellReference;
+import walkingkooka.text.cursor.parser.spreadsheet.SpreadsheetLabelName;
+
+import static org.junit.jupiter.api.Assertions.assertSame;
+
+public final class ReadOnlySpreadsheetLabelStoreTest extends SpreadsheetLabelStoreTestCase<ReadOnlySpreadsheetLabelStore> {
+
+    @Test
+    public void testSaveAndLoad() {
+        final SpreadsheetLabelStore store = SpreadsheetLabelStores.treeMap();
+
+        final SpreadsheetLabelMapping mapping = SpreadsheetLabelMapping.with(LABEL, REFERENCE);
+        store.save(mapping);
+
+        assertSame(mapping, ReadOnlySpreadsheetLabelStore.with(store).loadOrFail(LABEL));
+    }
+
+    @Override
+    public void testSaveDeleteLoad() {
+    }
+
+    @Test
+    public void testCount() {
+        this.countAndCheck(this.createStore2(), 1);
+    }
+
+    @Test
+    public void testIds() {
+        final SpreadsheetLabelStore store = SpreadsheetLabelStores.treeMap();
+
+        final SpreadsheetLabelMapping a = this.mapping("a", 1, 2);
+        final SpreadsheetLabelMapping b = this.mapping("b", 4, 8);
+        final SpreadsheetLabelMapping c = this.mapping("c", 88, 99);
+
+        store.save(a);
+        store.save(b);
+        store.save(c);
+
+        this.idsAndCheck(ReadOnlySpreadsheetLabelStore.with(store), 0, 3, a.id(), b.id(), c.id());
+    }
+
+    @Override
+    public void testIdsWindow() {
+    }
+
+    @Test
+    public void testValues() {
+        final SpreadsheetLabelStore store = SpreadsheetLabelStores.treeMap();
+
+        final SpreadsheetLabelMapping a = this.mapping("a", 1, 2);
+        final SpreadsheetLabelMapping b = this.mapping("b", 4, 8);
+        final SpreadsheetLabelMapping c = this.mapping("c", 88, 99);
+
+        store.save(a);
+        store.save(b);
+        store.save(c);
+
+        this.valuesAndCheck(ReadOnlySpreadsheetLabelStore.with(store), a.id(), 3, a, b, c);
+    }
+
+    @Override
+    public void testValuesWindow() {
+    }
+
+    @Override
+    public ReadOnlySpreadsheetLabelStore createStore() {
+        return ReadOnlySpreadsheetLabelStore.with(SpreadsheetLabelStores.treeMap());
+    }
+
+    private ReadOnlySpreadsheetLabelStore createStore2() {
+        final SpreadsheetLabelStore store = SpreadsheetLabelStores.treeMap();
+        store.save(this.labelMapping());
+        return ReadOnlySpreadsheetLabelStore.with(store);
+    }
+
+    private SpreadsheetLabelMapping labelMapping() {
+        return SpreadsheetLabelMapping.with(this.labelName(), this.reference());
+    }
+
+    private SpreadsheetLabelName labelName() {
+        return SpreadsheetLabelName.with("elephant");
+    }
+
+    private SpreadsheetCellReference reference() {
+        return SpreadsheetCellReference.parse("A1");
+    }
+
+    @Override
+    public Class<ReadOnlySpreadsheetLabelStore> type() {
+        return ReadOnlySpreadsheetLabelStore.class;
+    }
+}
