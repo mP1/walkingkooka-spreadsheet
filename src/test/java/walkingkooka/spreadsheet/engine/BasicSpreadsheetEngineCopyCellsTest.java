aab5222de8ff0f9b208b8c21f68446cf5d63cca1
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2019-03-30 14:31:16 +1100

    BasicSpreadsheetEngine.copyCells extracted
    - Allow re-eval control in BasicSpreadsheetEngine save cells.

---
+++ src/test/java/walkingkooka/spreadsheet/engine/BasicSpreadsheetEngineCopyCellsTest.java
@@ -0,0 +1,14 @@
+package walkingkooka.spreadsheet.engine;
+
+public final class BasicSpreadsheetEngineCopyCellsTest extends BasicSpreadsheetEngineTestCase<BasicSpreadsheetEngineCopyCells>{
+
+    @Override
+    public Class<BasicSpreadsheetEngineCopyCells> type() {
+        return BasicSpreadsheetEngineCopyCells.class;
+    }
+
+    @Override
+    public String typeNameSuffix() {
+        return "CopyCells";
+    }
+}
