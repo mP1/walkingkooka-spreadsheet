78df9b21ac91d01c1dd8b827e5899ce0b9e46b46
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2019-03-30 11:16:52 +1100

    BasicSpreadsheetEngineTestCase introduced.

--- src/test/java/walkingkooka/spreadsheet/engine/BasicSpreadsheetEngineDeleteOrInsertColumnOrRowColumnOrRowTest.java
+++ src/test/java/walkingkooka/spreadsheet/engine/BasicSpreadsheetEngineDeleteOrInsertColumnOrRowColumnOrRowTest.java
@@ -3,14 +3,17 @@ package walkingkooka.spreadsheet.engine;
 import walkingkooka.test.ClassTesting2;
 import walkingkooka.type.MemberVisibility;
 
-public final class BasicSpreadsheetEngineDeleteOrInsertColumnOrRowColumnOrRowTest implements ClassTesting2<BasicSpreadsheetEngineDeleteOrInsertColumnOrRowColumnOrRow> {
+public final class BasicSpreadsheetEngineDeleteOrInsertColumnOrRowColumnOrRowTest extends BasicSpreadsheetEngineTestCase<BasicSpreadsheetEngineDeleteOrInsertColumnOrRowColumnOrRow> {
+
     @Override
     public Class<BasicSpreadsheetEngineDeleteOrInsertColumnOrRowColumnOrRow> type() {
         return BasicSpreadsheetEngineDeleteOrInsertColumnOrRowColumnOrRow.class;
     }
 
+    // TypeNameTesting..........................................................................
+
     @Override
-    public MemberVisibility typeVisibility() {
-        return MemberVisibility.PACKAGE_PRIVATE;
+    public String typeNameSuffix() {
+        return "ColumnOrRow";
     }
 }
