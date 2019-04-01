096c46946ec476fcd37c39735942b8d57eb9e4a4
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2019-04-01 17:32:41 +1100

    SpreadEngine all methods return Set<SpreadsheetCell>
    - Also updated matching HateosHandlers.

---
+++ src/main/java/walkingkooka/spreadsheet/hateos/SpreadsheetEngineDeleteColumnsHateosIdResourceCollectionResourceCollectionHandler.java
@@ -0,0 +1,54 @@
+package walkingkooka.spreadsheet.hateos;
+
+import walkingkooka.collect.list.Lists;
+import walkingkooka.net.http.server.HttpRequestAttribute;
+import walkingkooka.net.http.server.hateos.HateosIdResourceCollectionResourceCollectionHandler;
+import walkingkooka.spreadsheet.SpreadsheetCell;
+import walkingkooka.spreadsheet.SpreadsheetColumn;
+import walkingkooka.spreadsheet.engine.SpreadsheetEngine;
+import walkingkooka.spreadsheet.engine.SpreadsheetEngineContext;
+import walkingkooka.text.cursor.parser.spreadsheet.SpreadsheetColumnReference;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.Supplier;
+
+/**
+ * A {@link HateosIdResourceCollectionResourceCollectionHandler} that handles deleting a single or range of columns.
+ */
+final class SpreadsheetEngineDeleteColumnsHateosIdResourceCollectionResourceCollectionHandler extends SpreadsheetEngineColumnHateosHandler
+        implements HateosIdResourceCollectionResourceCollectionHandler<SpreadsheetColumnReference, SpreadsheetColumn, SpreadsheetCell> {
+
+    static SpreadsheetEngineDeleteColumnsHateosIdResourceCollectionResourceCollectionHandler with(final SpreadsheetEngine engine,
+                                                                                                  final Supplier<SpreadsheetEngineContext> context) {
+        check(engine, context);
+        return new SpreadsheetEngineDeleteColumnsHateosIdResourceCollectionResourceCollectionHandler(engine, context);
+    }
+
+    /**
+     * Private ctor
+     */
+    private SpreadsheetEngineDeleteColumnsHateosIdResourceCollectionResourceCollectionHandler(final SpreadsheetEngine engine,
+                                                                                              final Supplier<SpreadsheetEngineContext> context) {
+        super(engine, context);
+    }
+
+    @Override
+    public List<SpreadsheetCell> handle(final SpreadsheetColumnReference column,
+                                        final List<SpreadsheetColumn> resources,
+                                        final Map<HttpRequestAttribute<?>, Object> parameters) {
+        Objects.requireNonNull(column, "column");
+        checkResourcesEmpty(resources);
+        checkParameters(parameters);
+
+        final List<SpreadsheetCell> cells = Lists.array();
+        cells.addAll(this.engine.deleteColumns(column, 1, this.context.get()));
+        return Lists.readOnly(cells);
+    }
+
+    @Override
+    String operation() {
+        return "deleteColumns";
+    }
+}
