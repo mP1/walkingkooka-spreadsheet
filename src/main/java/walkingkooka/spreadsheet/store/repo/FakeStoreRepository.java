4e06f3802f2d093769dea9fafae599e5ea704f6e
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2019-06-24 18:08:55 +1000

    StoreRepository defined, BasicStoreRepository

---
+++ src/main/java/walkingkooka/spreadsheet/store/repo/FakeStoreRepository.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2019 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.store.repo;
+
+import walkingkooka.spreadsheet.SpreadsheetCellReference;
+import walkingkooka.spreadsheet.SpreadsheetLabelName;
+import walkingkooka.spreadsheet.store.cell.SpreadsheetCellStore;
+import walkingkooka.spreadsheet.store.label.SpreadsheetLabelStore;
+import walkingkooka.spreadsheet.store.range.SpreadsheetRangeStore;
+import walkingkooka.spreadsheet.store.reference.SpreadsheetReferenceStore;
+import walkingkooka.spreadsheet.store.security.SpreadsheetGroupStore;
+import walkingkooka.spreadsheet.store.security.SpreadsheetUserStore;
+import walkingkooka.test.Fake;
+
+public class FakeStoreRepository implements StoreRepository, Fake {
+
+    protected FakeStoreRepository() {
+        super();
+    }
+
+    @Override
+    public SpreadsheetCellStore cells() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public SpreadsheetReferenceStore<SpreadsheetCellReference> cellReferences() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public SpreadsheetGroupStore groups() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public SpreadsheetLabelStore labels() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public SpreadsheetReferenceStore<SpreadsheetLabelName> labelReferences() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public SpreadsheetRangeStore ranges() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public SpreadsheetUserStore users() {
+        throw new UnsupportedOperationException();
+    }
+}
---
+++ src/main/java/walkingkooka/spreadsheet/store/repo/StoreRepository.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2019 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.store.repo;
+
+import walkingkooka.spreadsheet.SpreadsheetCellReference;
+import walkingkooka.spreadsheet.SpreadsheetLabelName;
+import walkingkooka.spreadsheet.store.Store;
+import walkingkooka.spreadsheet.store.cell.SpreadsheetCellStore;
+import walkingkooka.spreadsheet.store.label.SpreadsheetLabelStore;
+import walkingkooka.spreadsheet.store.range.SpreadsheetRangeStore;
+import walkingkooka.spreadsheet.store.reference.SpreadsheetReferenceStore;
+import walkingkooka.spreadsheet.store.security.SpreadsheetGroupStore;
+import walkingkooka.spreadsheet.store.security.SpreadsheetUserStore;
+
+/**
+ * Holds all {@link Store stores} in the system
+ */
+public interface StoreRepository {
+
+    /**
+     * A {@link SpreadsheetCellStore} holding cells.
+     */
+    SpreadsheetCellStore cells();
+
+    /**
+     * A {@link SpreadsheetReferenceStore} holding cell references.
+     */
+    SpreadsheetReferenceStore<SpreadsheetCellReference> cellReferences();
+
+    /**
+     * A {@link SpreadsheetGroupStore} holding groups.
+     */
+    SpreadsheetGroupStore groups();
+
+    /**
+     * A {@link SpreadsheetLabelStore} holding labels.
+     */
+    SpreadsheetLabelStore labels();
+
+    /**
+     * A {@link SpreadsheetReferenceStore} holding label references.
+     */
+    SpreadsheetReferenceStore<SpreadsheetLabelName> labelReferences();
+
+    /**
+     * The {@lin SpreadsheetRangeStore}
+     */
+    SpreadsheetRangeStore ranges();
+
+    /**
+     * A {@link SpreadsheetUserStore} holding users.
+     */
+    SpreadsheetUserStore users();
+}
