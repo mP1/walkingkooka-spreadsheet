79958c844372f9db050be33885212f781da1dd8e
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2019-06-26 20:29:45 +1000

    MemorySpreadsheetContext

---
+++ src/test/java/walkingkooka/spreadsheet/MemorySpreadsheetContextTest.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2019 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet;
+
+import org.junit.jupiter.api.Test;
+import walkingkooka.spreadsheet.store.Store;
+import walkingkooka.spreadsheet.store.cell.SpreadsheetCellStore;
+import walkingkooka.spreadsheet.store.repo.StoreRepository;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
+
+public final class MemorySpreadsheetContextTest implements SpreadsheetContextTesting<MemorySpreadsheetContext> {
+
+    @Test
+    public void testStoreRepositoryUnknownSpreadsheetId() {
+        final MemorySpreadsheetContext context = this.createContext();
+        final SpreadsheetId id = SpreadsheetId.with(123);
+
+        final StoreRepository repository = context.storeRepository(id);
+        assertNotEquals(null, repository);
+
+        this.countAndCheck(repository.cells(), 0);
+        this.countAndCheck(repository.cellReferences(), 0);
+        this.countAndCheck(repository.groups(), 0);
+        this.countAndCheck(repository.labels(), 0);
+        this.countAndCheck(repository.labelReferences(), 0);
+        this.countAndCheck(repository.ranges(), 0);
+        this.countAndCheck(repository.users(), 0);
+
+        repository.cells().save(SpreadsheetCell.with(SpreadsheetExpressionReference.parseCellReference("A1"), SpreadsheetFormula.with("1+2")));
+        this.countAndCheck(repository.cells(), 1);
+    }
+
+    @Test
+    public void testStoreRepositoryDifferentSpreadsheetId() {
+        final MemorySpreadsheetContext context = this.createContext();
+
+        final SpreadsheetId id1 = SpreadsheetId.with(111);
+        final StoreRepository repository1 = context.storeRepository(id1);
+        assertNotEquals(null, repository1);
+
+        final SpreadsheetId id2 = SpreadsheetId.with(222);
+        final StoreRepository repository2 = context.storeRepository(id2);
+        assertNotEquals(null, repository2);
+    }
+
+    @Test
+    public void testStoreRepositorySameSpreadsheetId() {
+        final MemorySpreadsheetContext context = this.createContext();
+
+        final SpreadsheetId id1 = SpreadsheetId.with(111);
+        final StoreRepository repository1 = context.storeRepository(id1);
+        assertSame(repository1, context.storeRepository(id1));
+    }
+
+    private void countAndCheck(final Store<?, ?> store, final int count) {
+        assertEquals(count, store.count(), () -> "" + store.all());
+    }
+
+    @Test
+    public void testToString() {
+        final MemorySpreadsheetContext context = this.createContext();
+        context.storeRepository(SpreadsheetId.with(111));
+
+        this.toStringAndCheck(context, "{111=[] {} [] [] {} {} {}}");
+    }
+
+    @Override
+    public MemorySpreadsheetContext createContext() {
+        return MemorySpreadsheetContext.create();
+    }
+
+    @Override
+    public Class<MemorySpreadsheetContext> type() {
+        return MemorySpreadsheetContext.class;
+    }
+}
