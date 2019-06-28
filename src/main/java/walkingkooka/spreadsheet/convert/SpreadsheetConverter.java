d50f48f4f2b244e1c57286c9d37cb9177638eb9b
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2019-06-28 12:59:33 +1000

    SpreadsheetConverter

---
+++ src/main/java/walkingkooka/spreadsheet/convert/SpreadsheetConverter.java
@@ -0,0 +1,121 @@
+/*
+ * Copyright 2019 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.convert;
+
+import walkingkooka.collect.set.Sets;
+import walkingkooka.convert.Converter;
+import walkingkooka.convert.ConverterContext;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Set;
+
+/**
+ * A {@link Converter} that supports the 9 main types within a spreadsheet which are:
+ * <ul>
+ * <li>{@link Boolean}</li>
+ * <li>{@link java.time.LocalDate}</li>
+ * <li>{@link java.time.LocalDateTime}</li>
+ * <li>{@link java.time.LocalTime}</li>
+ * <li>{@link java.math.BigDecimal}</li>
+ * <li>{@link java.math.BigInteger}</li>
+ * <li>{@link Double}</li>
+ * <li>{@link Long}</li>
+ * <li>{@link String}</li>
+ * </ul>
+ * More types like {@link walkingkooka.net.Url} and {@link walkingkooka.color.ColorHslOrHsv} may be added.
+ * </pre>
+ */
+final class SpreadsheetConverter implements Converter {
+
+    /**
+     * Singleton. Has no state.
+     */
+    final static SpreadsheetConverter with(final long dateOffset,
+                                           final String bigDecimalFormat,
+                                           final String bigIntegerFormat,
+                                           final String doubleFormat,
+                                           final DateTimeFormatter date,
+                                           final DateTimeFormatter dateTime,
+                                           final DateTimeFormatter time,
+                                           final String longFormat) {
+        return new SpreadsheetConverter(dateOffset,
+                bigDecimalFormat,
+                bigIntegerFormat,
+                doubleFormat,
+                date,
+                dateTime,
+                time,
+                longFormat);
+    }
+
+    private SpreadsheetConverter(final long dateOffset,
+                                 final String bigDecimalFormat,
+                                 final String bigIntegerFormat,
+                                 final String doubleFormat,
+                                 final DateTimeFormatter date,
+                                 final DateTimeFormatter dateTime,
+                                 final DateTimeFormatter time,
+                                 final String longFormat) {
+        super();
+        this.mapping = SpreadsheetConverterMapping.with(dateOffset,
+                bigDecimalFormat,
+                bigIntegerFormat,
+                doubleFormat,
+                date,
+                dateTime,
+                time,
+                longFormat);
+    }
+
+    @Override
+    public boolean canConvert(final Object value,
+                              final Class<?> targetType,
+                              final ConverterContext context) {
+        return SUPPORTED_TYPES.contains(targetType);
+    }
+
+    private final static Set<Class<?>> SUPPORTED_TYPES = Sets.of(Boolean.class,
+            LocalDate.class,
+            LocalDateTime.class,
+            LocalTime.class,
+            BigDecimal.class,
+            BigInteger.class,
+            Double.class,
+            Long.class,
+            String.class);
+
+    @Override
+    public <T> T convert(final Object value,
+                         final Class<T> targetType,
+                         final ConverterContext context) {
+        return SpreadsheetConverterSpreadsheetValueVisitor.converter(value, targetType, this.mapping)
+                .convert(value, targetType, context);
+    }
+
+    private final SpreadsheetConverterMapping<SpreadsheetConverterMapping<Converter>> mapping;
+
+    @Override
+    public String toString() {
+        return this.getClass().getSimpleName();
+    }
+}
---
+++ src/main/java/walkingkooka/spreadsheet/convert/SpreadsheetConverterMapping.java
@@ -0,0 +1,317 @@
+/*
+ * Copyright 2019 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.convert;
+
+import walkingkooka.convert.Converter;
+import walkingkooka.convert.ConverterContext;
+import walkingkooka.convert.Converters;
+import walkingkooka.text.cursor.parser.Parser;
+import walkingkooka.text.cursor.parser.ParserContext;
+import walkingkooka.text.cursor.parser.ParserContexts;
+import walkingkooka.text.cursor.parser.Parsers;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * Holds individual values for each spreadsheet value type.
+ */
+final class SpreadsheetConverterMapping<T> {
+
+    private final static BigDecimal BIG_DECIMAL_TRUE = BigDecimal.ONE;
+    private final static BigDecimal BIG_DECIMAL_FALSE = BigDecimal.ZERO;
+
+    private final static BigInteger BIG_INTEGER_TRUE = BigInteger.ONE;
+    private final static BigInteger BIG_INTEGER_FALSE = BigInteger.ZERO;
+
+    private final static Double DOUBLE_TRUE = 1.0;
+    private final static Double DOUBLE_FALSE = 0.0;
+
+    private final static LocalTime LOCAL_TIME_TRUE = LocalTime.ofSecondOfDay(1);
+    private final static LocalTime LOCAL_TIME_FALSE = LocalTime.ofSecondOfDay(0);
+
+    private final static Long LONG_TRUE = 1L;
+    private final static Long LONG_FALSE = 0L;
+
+    private final static String STRING_TRUE = Boolean.TRUE.toString();
+    private final static String STRING_FALSE = Boolean.FALSE.toString();
+
+    /**
+     * Creates a new {@link SpreadsheetConverterMapping}
+     */
+    static SpreadsheetConverterMapping<SpreadsheetConverterMapping<Converter>> with(final long dateOffset,
+                                                                                    final String bigDecimalFormat,
+                                                                                    final String bigIntegerFormat,
+                                                                                    final String doubleFormat,
+                                                                                    final DateTimeFormatter date,
+                                                                                    final DateTimeFormatter dateTime,
+                                                                                    final DateTimeFormatter time,
+                                                                                    final String longFormat) {
+        return new SpreadsheetConverterMapping<>(bigDecimal(dateOffset, bigDecimalFormat),
+                bigInteger(dateOffset, bigIntegerFormat),
+                fromBoolean(dateOffset),
+                doubleMapping(dateOffset, doubleFormat),
+                localDate(dateOffset, date),
+                localDateTime(dateOffset, dateTime),
+                localTime(time),
+                longMapping(dateOffset, longFormat),
+                string(bigDecimalFormat, bigIntegerFormat, doubleFormat, date, dateTime, time, longFormat));
+    }
+
+    /**
+     * Holds all the {@link Converter converters} from a {@link BigDecimal}.
+     */
+    private static SpreadsheetConverterMapping<Converter> bigDecimal(final long dateOffset,
+                                                                     final String format) {
+        return new SpreadsheetConverterMapping<>(Converters.simple(), // bigDecimal -> bigDecimal
+                Converters.numberBigInteger(),
+                Converters.bigDecimalBoolean(),
+                Converters.numberDouble(),
+                Converters.numberLocalDate(dateOffset),
+                Converters.numberLocalDateTime(dateOffset),
+                Converters.numberLocalTime(),
+                Converters.numberLong(),
+                Converters.decimalFormatString(format));
+    }
+
+    /**
+     * Holds all the {@link Converter converters} from a {@link BigInteger}.
+     */
+    private static SpreadsheetConverterMapping<Converter> bigInteger(final long dateOffset,
+                                                                     final String format) {
+        return new SpreadsheetConverterMapping<>(Converters.numberBigDecimal(),
+                Converters.simple(), // bigDecimal -> bigInteger
+                toBoolean(BigInteger.class, BIG_INTEGER_FALSE),
+                Converters.numberDouble(),
+                Converters.numberLocalDate(dateOffset),
+                Converters.numberLocalDateTime(dateOffset),
+                Converters.numberLocalTime(),
+                Converters.numberLong(),
+                Converters.decimalFormatString(format));
+    }
+
+    /**
+     * Holds all the {@link Converter converters} from a {@link Boolean}.
+     */
+    private static SpreadsheetConverterMapping<Converter> fromBoolean(final long dateOffset) {
+        return new SpreadsheetConverterMapping<>(fromBoolean(BigDecimal.class, BIG_DECIMAL_TRUE, BIG_DECIMAL_FALSE),
+                fromBoolean(BigInteger.class, BIG_INTEGER_TRUE, BIG_INTEGER_FALSE),
+                Converters.simple(), // boolean -> boolean
+                fromBoolean(Double.class, DOUBLE_TRUE, DOUBLE_FALSE),
+                fromBoolean(LocalDate.class, localDateTrue(dateOffset), localDateFalse(dateOffset)),
+                fromBoolean(LocalDateTime.class, localDateTimeTrue(dateOffset), localDateTimeFalse(dateOffset)),
+                fromBoolean(LocalTime.class, LOCAL_TIME_TRUE, LOCAL_TIME_FALSE),
+                fromBoolean(Long.class, LONG_TRUE, LONG_FALSE),
+                fromBoolean(String.class, STRING_TRUE, STRING_FALSE));
+    }
+
+    /**
+     * Holds all the {@link Converter converters} from a {@link Double}.
+     */
+    private static SpreadsheetConverterMapping<Converter> doubleMapping(final long dateOffset,
+                                                                        final String format) {
+        return new SpreadsheetConverterMapping<>(Converters.numberBigDecimal(),
+                Converters.numberBigInteger(),
+                toBoolean(Double.class, DOUBLE_FALSE),
+                Converters.simple(), // double -> double
+                Converters.numberLocalDate(dateOffset),
+                Converters.numberLocalDateTime(dateOffset),
+                Converters.numberLocalTime(),
+                Converters.numberLong(),
+                Converters.decimalFormatString(format));
+    }
+
+    /**
+     * Holds all the {@link Converter converters} from a {@link LocalDate}.
+     */
+    private static SpreadsheetConverterMapping<Converter> localDate(final long dateOffset,
+                                                                    final DateTimeFormatter formatter) {
+        return new SpreadsheetConverterMapping<>(Converters.localDateBigDecimal(dateOffset),
+                Converters.localDateBigInteger(dateOffset),
+                toBoolean(LocalDate.class, localDateFalse(dateOffset)),
+                Converters.localDateDouble(dateOffset),
+                Converters.simple(), // date -> date
+                Converters.localDateLocalDateTime(),
+                Converters.fail(LocalDate.class, LocalTime.class),
+                Converters.localDateLong(dateOffset),
+                Converters.localDateString(formatter));
+    }
+
+    /**
+     * Holds all the {@link Converter converters} from a {@link LocalDateTime}.
+     */
+    private static SpreadsheetConverterMapping<Converter> localDateTime(final long dateOffset,
+                                                                        final DateTimeFormatter formatter) {
+        return new SpreadsheetConverterMapping<>(Converters.localDateTimeBigDecimal(dateOffset),
+                Converters.localDateTimeBigInteger(dateOffset),
+                toBoolean(LocalDateTime.class, LocalDateTime.of(localDateFalse(dateOffset), LOCAL_TIME_FALSE)),
+                Converters.localDateTimeDouble(dateOffset),
+                Converters.localDateTimeLocalDate(),
+                Converters.simple(), // datetime -> datetime
+                Converters.localDateTimeLocalTime(),
+                Converters.localDateTimeLong(dateOffset),
+                Converters.localDateTimeString(formatter));
+    }
+
+    /**
+     * Holds all the {@link Converter converters} from a {@link LocalTime}.
+     */
+    private static SpreadsheetConverterMapping<Converter> localTime(final DateTimeFormatter formatter) {
+        return new SpreadsheetConverterMapping<>(Converters.localTimeBigDecimal(),
+                Converters.localTimeBigInteger(),
+                toBoolean(LocalTime.class, LOCAL_TIME_FALSE),
+                Converters.localTimeDouble(),
+                Converters.fail(LocalTime.class, LocalDate.class), // time -> date fails
+                Converters.localTimeLocalDateTime(),
+                Converters.simple(), // time -> time
+                Converters.localTimeLong(),
+                Converters.localTimeString(formatter));
+    }
+
+    /**
+     * Holds all the {@link Converter converters} from a {@link Long}.
+     */
+    private static SpreadsheetConverterMapping<Converter> longMapping(final long dateOffset,
+                                                                      final String format) {
+        return new SpreadsheetConverterMapping<>(Converters.numberBigDecimal(),
+                Converters.numberBigInteger(),
+                toBoolean(Long.class, LONG_FALSE),
+                Converters.numberDouble(),
+                Converters.numberLocalDate(dateOffset),
+                Converters.numberLocalDateTime(dateOffset),
+                Converters.numberLocalTime(),
+                Converters.simple(), // long -> long
+                Converters.decimalFormatString(format));
+    }
+
+    private final static int RADIX = 10;
+
+    /**
+     * Holds all the {@link Converter converters} from a {@link Long}.
+     */
+    private static SpreadsheetConverterMapping<Converter> string(final String bigDecimalFormat,
+                                                                 final String bigIntegerFormat,
+                                                                 final String doubleFormat,
+                                                                 final DateTimeFormatter date,
+                                                                 final DateTimeFormatter dateTime,
+                                                                 final DateTimeFormatter time,
+                                                                 final String longFormat) {
+        return new SpreadsheetConverterMapping<>(parser(BigDecimal.class, Parsers.bigDecimal()),
+                parser(BigInteger.class, Parsers.bigInteger(RADIX)),
+                toBoolean(String.class, STRING_FALSE),
+                parser(Double.class, Parsers.doubleParser()),
+                Converters.stringLocalDate(date),
+                Converters.stringLocalDateTime(dateTime),
+                Converters.stringLocalTime(time),
+                parser(Long.class, Parsers.longParser(RADIX)),
+                Converters.simple()); // string -> string
+    }
+
+    private static <T> Converter parser(final Class<T> type,
+                                       final Parser parser) {
+        return Converters.parser(type, parser, SpreadsheetConverterMapping::parserContext);
+    }
+
+    private static ParserContext parserContext(final ConverterContext context) {
+        return ParserContexts.basic(context);
+    }
+
+    private static <T> Converter fromBoolean(final Class<T> type,
+                                             final T trueValue,
+                                             final T falseValue) {
+        return Converters.booleanConverter(Boolean.class,
+                Boolean.FALSE,
+                type,
+                trueValue,
+                falseValue);
+    }
+
+    private static <T> Converter toBoolean(final Class<T> from,
+                                           final T falseValue) {
+        return Converters.booleanConverter(from,
+                falseValue,
+                Boolean.class,
+                Boolean.TRUE,
+                Boolean.FALSE);
+    }
+
+    private static LocalDate localDateTrue(final long dateOffset) {
+        return localDateFalse(dateOffset + 1);
+    }
+
+    private static LocalDate localDateFalse(final long dateOffset) {
+        return LocalDate.ofEpochDay(dateOffset);
+    }
+
+    private static LocalDateTime localDateTimeTrue(final long dateOffset) {
+        return localDateTimeFalse(1 + dateOffset);
+    }
+
+    private static LocalDateTime localDateTimeFalse(final long dateOffset) {
+        return LocalDateTime.of(localDateFalse(dateOffset), LOCAL_TIME_FALSE);
+    }
+
+    private SpreadsheetConverterMapping(final T bigDecimal,
+                                        final T bigInteger,
+                                        final T booleanValue,
+                                        final T doubleValue,
+                                        final T localDate,
+                                        final T localDateTime,
+                                        final T localTime,
+                                        final T longValue,
+                                        final T string) {
+        super();
+
+        this.bigDecimal = bigDecimal;
+        this.bigInteger = bigInteger;
+        this.booleanValue = booleanValue;
+        this.doubleValue = doubleValue;
+        this.localDate = localDate;
+        this.localDateTime = localDateTime;
+        this.localTime = localTime;
+        this.longValue = longValue;
+        this.string = string;
+    }
+
+    final T bigDecimal;
+    final T bigInteger;
+    final T booleanValue;
+    final T doubleValue;
+    final T localDate;
+    final T localDateTime;
+    final T localTime;
+    final T longValue;
+    final T string;
+
+    @Override
+    public String toString() {
+        return this.bigDecimal + " " +
+                this.bigInteger + " " +
+                this.booleanValue + " " +
+                this.doubleValue + " " +
+                this.localDate + " " +
+                this.localDateTime + " " +
+                this.localTime + " " +
+                this.longValue + " " +
+                this.string;
+    }
+}
