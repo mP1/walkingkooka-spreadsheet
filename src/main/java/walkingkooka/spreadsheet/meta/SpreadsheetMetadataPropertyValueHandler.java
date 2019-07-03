614b2a6fb470208cf6caf66700a7830ca6950f50
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2019-07-03 17:31:55 +1000

    SpreadsheetMetadata

---
+++ src/main/java/walkingkooka/spreadsheet/meta/SpreadsheetMetadataPropertyValueHandler.java
@@ -0,0 +1,117 @@
+/*
+ * Copyright 2019 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.meta;
+
+import walkingkooka.Cast;
+import walkingkooka.text.CharSequences;
+import walkingkooka.tree.json.HasJsonNode;
+import walkingkooka.tree.json.JsonNode;
+
+/**
+ * Base converter that provides support for handling property values.
+ */
+abstract class SpreadsheetMetadataPropertyValueHandler<T> {
+
+    /**
+     * {@see EmailAddressSpreadsheetMetadataPropertyValueHandler}
+     */
+    static EmailAddressSpreadsheetMetadataPropertyValueHandler emailAddress() {
+        return EmailAddressSpreadsheetMetadataPropertyValueHandler.INSTANCE;
+    }
+
+    /**
+     * {@see LocalDateTimeSpreadsheetMetadataPropertyValueHandler}
+     */
+    static LocalDateTimeSpreadsheetMetadataPropertyValueHandler localDateTime() {
+        return LocalDateTimeSpreadsheetMetadataPropertyValueHandler.INSTANCE;
+    }
+
+    /**
+     * Package private to limit sub classing.
+     */
+    SpreadsheetMetadataPropertyValueHandler() {
+        super();
+    }
+
+    // checkValue...........................................................
+
+    final T check(final Object value, final SpreadsheetMetadataPropertyName<?> name) {
+        if (null == value) {
+            throw new SpreadsheetMetadataPropertyValueException("Property " + name.inQuotes + " missing value");
+        }
+
+        this.check0(value, name);
+        return Cast.to(value);
+    }
+
+    abstract void check0(final Object value, final SpreadsheetMetadataPropertyName<?> name);
+
+    /**
+     * Checks the type of the given value and throws a {@link SpreadsheetMetadataPropertyValueException} if this test fails.
+     */
+    final <U> U checkType(final Object value, final Class<U> type, final SpreadsheetMetadataPropertyName<?> name) {
+        if (!type.isInstance(value)) {
+            throw this.spreadsheetMetadataPropertyValueException(value, name);
+        }
+        return type.cast(value);
+    }
+
+    /**
+     * Creates a {@link SpreadsheetMetadataPropertyValueException} used to report an invalid value.
+     */
+    final SpreadsheetMetadataPropertyValueException spreadsheetMetadataPropertyValueException(final Object value,
+                                                                                              final SpreadsheetMetadataPropertyName<?> name) {
+        final Class<?> type = value.getClass();
+
+        String typeName = type.getName();
+        if (textStylePropertyType(typeName) || hasJsonType(type)) {
+            typeName = typeName.substring(1 + typeName.lastIndexOf('.'));
+        }
+
+        return new SpreadsheetMetadataPropertyValueException("Property " + name.inQuotes + " value " + CharSequences.quoteIfChars(value) + "(" + typeName + ") is not a " + this.expectedTypeName(type));
+    }
+
+    abstract String expectedTypeName(final Class<?> type);
+
+    final boolean textStylePropertyType(final String type) {
+        return type.startsWith(PACKAGE) && type.indexOf('.', 1 + PACKAGE.length()) == -1;
+    }
+
+    final boolean hasJsonType(final Class<?> type) {
+        return HasJsonNode.typeName(type).isPresent();
+    }
+
+    private final static String PACKAGE = "walkingkooka.spreadsheet.meta";
+
+    // fromJsonNode ....................................................................................................
+
+    /**
+     * Transforms a {@link JsonNode} into a value.
+     */
+    abstract T fromJsonNode(final JsonNode node, final SpreadsheetMetadataPropertyName<?> name);
+
+    /**
+     * Transforms a value into json, performing the inverse of {@link #fromJsonNode(JsonNode, SpreadsheetMetadataPropertyName)}
+     */
+    abstract JsonNode toJsonNode(final T value);
+
+    // Object .........................................................................................................
+
+    @Override
+    abstract public String toString();
+}
---
+++ src/test/java/walkingkooka/spreadsheet/meta/SpreadsheetMetadataMapTest.java
@@ -0,0 +1,134 @@
+/*
+ * Copyright 2019 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.meta;
+
+import org.junit.jupiter.api.Test;
+import walkingkooka.collect.list.Lists;
+import walkingkooka.collect.map.MapTesting2;
+import walkingkooka.collect.map.Maps;
+import walkingkooka.net.email.EmailAddress;
+import walkingkooka.tree.json.HasJsonNodeTesting;
+import walkingkooka.tree.json.JsonNode;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public final class SpreadsheetMetadataMapTest implements MapTesting2<SpreadsheetMetadataMap, SpreadsheetMetadataPropertyName<?>, Object> {
+
+    @Test
+    public void testWithInvalidPropertyFails() {
+        assertThrows(SpreadsheetMetadataPropertyValueException.class, () -> {
+            SpreadsheetMetadataMap.with(Maps.of(SpreadsheetMetadataPropertyName.CREATOR, null));
+        });
+    }
+
+    @Test
+    public void testWithMapCopied() {
+        final Map<SpreadsheetMetadataPropertyName<?>, Object> from = Maps.sorted();
+        from.put(this.property1(), this.value1());
+        from.put(this.property2(), this.value2());
+
+        final SpreadsheetMetadataMap map = SpreadsheetMetadataMap.with(from);
+
+        from.clear();
+        this.sizeAndCheck(map, 2);
+    }
+
+    @Test
+    public void testGet() {
+        this.getAndCheck(this.property1(), this.value1());
+    }
+
+    @Test
+    public void testGetUnknown() {
+        this.getAndCheckAbsent(SpreadsheetMetadataPropertyName.MODIFIED_BY);
+    }
+
+    @Test
+    public void testSize() {
+        this.sizeAndCheck(this.createMap(), 2);
+    }
+
+    @Test
+    public void testPutFails() {
+        assertThrows(UnsupportedOperationException.class, () -> {
+            this.createMap().put(this.property1(), this.value1());
+        });
+    }
+
+    @Test
+    public void testKeySet() {
+        final List<SpreadsheetMetadataPropertyName<?>> keys = Lists.array();
+
+        for (SpreadsheetMetadataPropertyName<?> key : this.createMap().keySet()) {
+            keys.add(key);
+        }
+
+        assertEquals(Lists.of(this.property2(), this.property1()), keys);
+    }
+
+    @Test
+    public void testFromEmptyJsonObject() {
+        assertSame(SpreadsheetMetadataMap.EMPTY, SpreadsheetMetadataMap.fromJson(JsonNode.object()));
+    }
+
+    @Test
+    public void testToString() {
+        final Map<SpreadsheetMetadataPropertyName<?>, Object> map = Maps.sorted();
+        map.put(this.property1(), this.value1());
+        map.put(this.property2(), this.value2());
+
+        this.toStringAndCheck(this.createMap(), map.toString());
+    }
+
+    @Override
+    public SpreadsheetMetadataMap createMap() {
+        final Map<SpreadsheetMetadataPropertyName<?>, Object> map = Maps.ordered();
+        map.put(this.property1(), this.value1());
+        map.put(this.property2(), this.value2());
+        return SpreadsheetMetadataMap.with(map);
+    }
+
+    private SpreadsheetMetadataPropertyName<?> property1() {
+        return SpreadsheetMetadataPropertyName.CREATOR;
+    }
+
+    private EmailAddress value1() {
+        return EmailAddress.parse("user@example.com");
+    }
+
+    private SpreadsheetMetadataPropertyName<?> property2() {
+        return SpreadsheetMetadataPropertyName.CREATE_DATE_TIME;
+    }
+
+    private LocalDateTime value2() {
+        return LocalDateTime.of(2000, 1, 2, 12, 58, 59);
+    }
+
+    // ClassTesting.....................................................................................................
+
+    @Override
+    public Class<SpreadsheetMetadataMap> type() {
+        return SpreadsheetMetadataMap.class;
+    }
+}
