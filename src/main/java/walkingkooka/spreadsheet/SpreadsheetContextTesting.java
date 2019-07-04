06fb39062ddad1e0d210c10103763ddad6f21598
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2019-07-04 10:38:40 +1000

    SpreadsheetContext.metadata

--- src/main/java/walkingkooka/spreadsheet/SpreadsheetContextTesting.java
+++ src/main/java/walkingkooka/spreadsheet/SpreadsheetContextTesting.java
@@ -69,6 +69,13 @@ public interface SpreadsheetContextTesting<C extends SpreadsheetContext> extends
     }
 
     @Test
+    default void testMetadataNullSpreadsheetIdFails() {
+        assertThrows(NullPointerException.class, () -> {
+            this.createContext().metadata(null);
+        });
+    }
+
+    @Test
     default void testNameToColorNullSpreadsheetIdFails() {
         assertThrows(NullPointerException.class, () -> {
             this.createContext().nameToColor(null);
