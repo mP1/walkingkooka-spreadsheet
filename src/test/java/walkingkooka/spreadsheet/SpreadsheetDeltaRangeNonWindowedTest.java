0f746ec81e17f8ff43603a6a550b49d1f4f8ee39
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2019-07-08 13:27:41 +1000

    SpreadsheetDelta introduce ID type parameter, accepts Optional and Range.

---
+++ src/test/java/walkingkooka/spreadsheet/SpreadsheetDeltaRangeNonWindowedTest.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright 2019 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet;
+
+import org.junit.jupiter.api.Test;
+import walkingkooka.Cast;
+import walkingkooka.compare.Range;
+import walkingkooka.tree.json.HasJsonNode;
+import walkingkooka.tree.json.JsonNode;
+
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertSame;
+
+public final class SpreadsheetDeltaRangeNonWindowedTest extends SpreadsheetDeltaNonWindowedTestCase<SpreadsheetDeltaRangeNonWindowed<SpreadsheetId>, Range<SpreadsheetId>> {
+
+    @Test
+    public void testSetRangeSame() {
+        final SpreadsheetDeltaRangeNonWindowed<SpreadsheetId> delta = this.createSpreadsheetDelta();
+        assertSame(delta, delta.setRange(this.id()));
+    }
+
+    @Test
+    public void testEqualsSpreadsheetDeltaWindowed() {
+        this.checkNotEquals(SpreadsheetDeltaRangeWindowed.with(this.id(), this.cells(), this.window0("A1:Z99")));
+    }
+
+    // HasJson..........................................................................................................
+
+    @Test
+    public void testFromJson() {
+        this.fromJsonNodeAndCheck(JsonNode.object()
+                        .set(SpreadsheetDelta.RANGE_PROPERTY, this.id().toJsonNode())
+                        .set(SpreadsheetDelta.CELLS_PROPERTY, HasJsonNode.toJsonNodeSet(this.cells())),
+                this.createHasJsonNode());
+    }
+
+    @Test
+    public void testToJsonNode() {
+        this.toJsonNodeAndCheck(this.createHasJsonNode(),
+                JsonNode.object()
+                        .set(SpreadsheetDelta.RANGE_PROPERTY, this.id().toJsonNode())
+                        .set(SpreadsheetDelta.CELLS_PROPERTY, HasJsonNode.toJsonNodeSet(this.cells())));
+    }
+
+    // HasHateosLink....................................................................................................
+
+    @Test
+    public final void testHateosLinkId() {
+        this.hateosLinkIdAndCheck("1a-1f");
+    }
+
+    // toString..........................................................................................................
+
+    @Test
+    public void testToString() {
+        this.toStringAndCheck(this.createSpreadsheetDelta(),
+                "1a:1f cells: A1=1, B2=2, C3=3");
+    }
+
+    @Override
+    SpreadsheetDeltaRangeNonWindowed<SpreadsheetId> createSpreadsheetDelta(final Range<SpreadsheetId> id,
+                                                                           final Set<SpreadsheetCell> cells) {
+        return SpreadsheetDeltaRangeNonWindowed.with(id, cells);
+    }
+
+    @Override
+    Range<SpreadsheetId> id() {
+        return range(0x1A, 0x1F);
+    }
+
+    @Override
+    Range<SpreadsheetId> differentId() {
+        return range(0x22, 0x3F);
+    }
+
+    @Override
+    public Class<SpreadsheetDeltaRangeNonWindowed<SpreadsheetId>> type() {
+        return Cast.to(SpreadsheetDeltaRangeNonWindowed.class);
+    }
+}
