737d4f7f45d8f6730ebd8c0be8153868c040bf82
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2019-07-11 19:52:50 +1000

    SpreadsheetEngineCopyCellsHateosHandler

---
+++ src/main/java/walkingkooka/spreadsheet/hateos/SpreadsheetEngineCopyCellsHateosHandler.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2019 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.hateos;
+
+import walkingkooka.compare.Range;
+import walkingkooka.net.UrlParameterName;
+import walkingkooka.net.http.server.HttpRequestAttribute;
+import walkingkooka.net.http.server.hateos.HateosHandler;
+import walkingkooka.spreadsheet.SpreadsheetCell;
+import walkingkooka.spreadsheet.SpreadsheetCellReference;
+import walkingkooka.spreadsheet.SpreadsheetDelta;
+import walkingkooka.spreadsheet.SpreadsheetExpressionReference;
+import walkingkooka.spreadsheet.engine.SpreadsheetEngine;
+import walkingkooka.spreadsheet.engine.SpreadsheetEngineContext;
+
+import java.util.Map;
+import java.util.Optional;
+
+/**
+ * A {@link HateosHandler} that calls {@link SpreadsheetEngine#saveCell(SpreadsheetCell, SpreadsheetEngineContext)}.
+ */
+final class SpreadsheetEngineCopyCellsHateosHandler extends SpreadsheetEngineHateosHandler<SpreadsheetCellReference> {
+
+    static SpreadsheetEngineCopyCellsHateosHandler with(final SpreadsheetEngine engine,
+                                                        final SpreadsheetEngineContext context) {
+        check(engine, context);
+        return new SpreadsheetEngineCopyCellsHateosHandler(engine, context);
+    }
+
+    private SpreadsheetEngineCopyCellsHateosHandler(final SpreadsheetEngine engine,
+                                                    final SpreadsheetEngineContext context) {
+        super(engine, context);
+    }
+
+    @Override
+    public Optional<SpreadsheetDelta<Optional<SpreadsheetCellReference>>> handle(final Optional<SpreadsheetCellReference> id,
+                                                                                 final Optional<SpreadsheetDelta<Optional<SpreadsheetCellReference>>> resource,
+                                                                                 final Map<HttpRequestAttribute<?>, Object> parameters) {
+        this.checkIdNotNull(id);
+        this.checkResource(resource);
+        this.checkParameters(parameters);
+
+        throw new UnsupportedOperationException();
+    }
+
+
+    @Override
+    public Optional<SpreadsheetDelta<Range<SpreadsheetCellReference>>> handleCollection(final Range<SpreadsheetCellReference> ids,
+                                                                                        final Optional<SpreadsheetDelta<Range<SpreadsheetCellReference>>> resource,
+                                                                                        final Map<HttpRequestAttribute<?>, Object> parameters) {
+        this.checkRangeNotNull(ids);
+        final SpreadsheetDelta<Range<SpreadsheetCellReference>> delta = this.checkResourceNotEmpty(resource);
+        this.checkParameters(parameters);
+
+        return Optional.of(this.engine.copyCells(delta.cells(),
+                this.parameterValueOrFail(parameters, TO, SpreadsheetExpressionReference::parseRange),
+                this.context));
+    }
+
+    final static UrlParameterName TO = UrlParameterName.with("to");
+
+    @Override
+    String operation() {
+        return "copyCells"; // SpreadsheetEngine#copyCells
+    }
+}
