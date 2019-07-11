55b1dc1c9cd4299f6fafa8721962e9dab88360b5
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2019-07-11 18:10:34 +1000

    SpreadsheetEngineSaveCellHateosHandler

---
+++ src/test/java/walkingkooka/spreadsheet/hateos/SpreadsheetEngineHateosHandlerTestCase.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2019 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.hateos;
+
+import walkingkooka.spreadsheet.SpreadsheetCell;
+import walkingkooka.spreadsheet.SpreadsheetExpressionReference;
+import walkingkooka.spreadsheet.SpreadsheetFormula;
+import walkingkooka.spreadsheet.engine.SpreadsheetEngine;
+
+public abstract class SpreadsheetEngineHateosHandlerTestCase<H> extends SpreadsheetHateosHandlerTestCase<H> {
+
+    SpreadsheetEngineHateosHandlerTestCase() {
+        super();
+    }
+
+    final SpreadsheetCell cell() {
+        return SpreadsheetCell.with(SpreadsheetExpressionReference.parseCellReference("A99"), SpreadsheetFormula.with("1+2"));
+    }
+
+    @Override
+    public final String typeNamePrefix() {
+        return SpreadsheetEngine.class.getSimpleName();
+    }
+}
