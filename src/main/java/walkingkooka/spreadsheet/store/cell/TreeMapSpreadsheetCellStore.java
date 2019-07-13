9d5fdf2a4d149e293d07857e0c51c80760823123
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2019-07-13 12:57:33 +1000

    TreeMapSpreadsheetCellStore wrap SpreadsheetStores.treeMap

--- src/main/java/walkingkooka/spreadsheet/store/cell/TreeMapSpreadsheetCellStore.java
+++ src/main/java/walkingkooka/spreadsheet/store/cell/TreeMapSpreadsheetCellStore.java
@@ -17,16 +17,17 @@
 
 package walkingkooka.spreadsheet.store.cell;
 
-import walkingkooka.collect.list.Lists;
 import walkingkooka.collect.map.Maps;
 import walkingkooka.collect.set.Sets;
 import walkingkooka.spreadsheet.SpreadsheetCell;
 import walkingkooka.spreadsheet.SpreadsheetCellReference;
 import walkingkooka.spreadsheet.SpreadsheetColumnReference;
 import walkingkooka.spreadsheet.SpreadsheetRowReference;
+import walkingkooka.spreadsheet.security.UserId;
 import walkingkooka.spreadsheet.store.SpreadsheetStore;
-import walkingkooka.spreadsheet.store.Watchers;
+import walkingkooka.spreadsheet.store.SpreadsheetStores;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
@@ -54,101 +55,69 @@ final class TreeMapSpreadsheetCellStore implements SpreadsheetCellStore {
      */
     private TreeMapSpreadsheetCellStore() {
         super();
+        this.store = SpreadsheetStores.treeMap(Comparator.naturalOrder(), TreeMapSpreadsheetCellStore::idSetter);
+    }
+
+    private static SpreadsheetCell idSetter(final SpreadsheetCellReference id, final SpreadsheetCell spreadsheetCell) {
+        throw new UnsupportedOperationException();
     }
 
     @Override
-    public final Optional<SpreadsheetCell> load(final SpreadsheetCellReference reference) {
-        Objects.requireNonNull(reference, "references");
-
-        return Optional.ofNullable(this.cells.get(reference));
+    public Optional<SpreadsheetCell> load(final SpreadsheetCellReference id) {
+        return this.store.load(id);
     }
 
-    /**
-     * Accepts a potentially updated cell.
-     */
     @Override
-    public final SpreadsheetCell save(final SpreadsheetCell cell) {
-        Objects.requireNonNull(cell, "cell");
-
-        final SpreadsheetCellReference key = cell.reference();
-        if (false == cell.equals(this.cells.put(key, cell))) {
-            this.saveWatchers.accept(cell);
-        }
-
-        return cell;
+    public SpreadsheetCell save(final SpreadsheetCell spreadsheetCell) {
+        return this.store.save(spreadsheetCell);
     }
 
     @Override
     public Runnable addSaveWatcher(final Consumer<SpreadsheetCell> saved) {
-        return this.saveWatchers.addWatcher(saved);
+        return this.store.addSaveWatcher(saved);
     }
 
-    private final Watchers<SpreadsheetCell> saveWatchers = Watchers.create();
-
-    /**
-     * Deletes a single cell, ignoring invalid requests.
-     */
     @Override
-    public final void delete(final SpreadsheetCellReference reference) {
-        Objects.requireNonNull(reference, "reference");
-
-        if (null != this.cells.remove(reference)) {
-            this.deleteWatchers.accept(reference);
-        }
+    public void delete(final SpreadsheetCellReference id) {
+        this.store.delete(id);
     }
 
     @Override
     public Runnable addDeleteWatcher(final Consumer<SpreadsheetCellReference> deleted) {
-        return this.deleteWatchers.addWatcher(deleted);
+        return this.store.addDeleteWatcher(deleted);
     }
-
-    private final Watchers<SpreadsheetCellReference> deleteWatchers = Watchers.create();
-
 
     @Override
     public int count() {
-        return this.cells.size();
+        return this.store.count();
     }
 
     @Override
     public Set<SpreadsheetCellReference> ids(final int from,
                                              final int count) {
-        SpreadsheetStore.checkFromAndTo(from, count);
-
-        return this.cells.keySet()
-                .stream()
-                .skip(from)
-                .limit(count)
-                .collect(Collectors.toCollection(Sets::ordered));
+        return this.store.ids(from, count);
     }
 
     @Override
     public List<SpreadsheetCell> values(final SpreadsheetCellReference from,
                                         final int count) {
-        SpreadsheetStore.checkFromAndToIds(from, count);
-
-        return this.cells.entrySet()
-                .stream()
-                .filter(e -> e.getKey().compareTo(from) >= 0)
-                .map(e -> e.getValue())
-                .limit(count)
-                .collect(Collectors.toCollection(Lists::array));
+        return this.store.values(from, count);
     }
 
     @Override
     public int rows() {
-        return this.max(c -> c.row().value());
+        return this.max(c -> c.reference().row().value());
     }
 
     @Override
     public int columns() {
-        return this.max(c -> c.column().value());
+        return this.max(c -> c.reference().column().value());
     }
 
-    private int max(final ToIntFunction<SpreadsheetCellReference> value) {
-        return this.cells.keySet()
+    private int max(final ToIntFunction<SpreadsheetCell> value) {
+        return this.all()
                 .stream()
-                .mapToInt(value)
+                .mapToInt(value::applyAsInt)
                 .max()
                 .orElse(0);
     }
@@ -168,19 +137,17 @@ final class TreeMapSpreadsheetCellStore implements SpreadsheetCellStore {
     }
 
     private Set<SpreadsheetCell> filter(final Predicate<SpreadsheetCell> filter) {
-        return this.cells.values()
+        return this.all()
                 .stream()
                 .filter(filter)
                 .collect(Collectors.toCollection(Sets::sorted));
     }
 
-    /**
-     * All cells present in this spreadsheet
-     */
-    private final Map<SpreadsheetCellReference, SpreadsheetCell> cells = Maps.sorted();
+    // VisibleForTesting
+    final SpreadsheetStore<SpreadsheetCellReference, SpreadsheetCell> store;
 
     @Override
     public String toString() {
-        return this.cells.values().toString();
+        return this.store.toString();
     }
 }
