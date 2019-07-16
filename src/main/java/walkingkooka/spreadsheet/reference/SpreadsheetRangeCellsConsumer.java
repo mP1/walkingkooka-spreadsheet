bd70d1a06ac468822b94991aadef42e2738629a7
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2019-07-16 18:46:36 +1000

    SpreadsheetRange.cells(Collection<SpreadsheetCell>, Consumer<SpreadsheetCell> present, Consumer<SpreadsheetCellReference> absent)

---
+++ src/main/java/walkingkooka/spreadsheet/reference/SpreadsheetRangeCellsConsumer.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2019 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.reference;
+
+import walkingkooka.collect.map.Maps;
+import walkingkooka.spreadsheet.SpreadsheetCell;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.Consumer;
+
+/**
+ * A {@link Consumer} used by {@link SpreadsheetRange#cells(List, Consumer, Consumer)}.
+ */
+final class SpreadsheetRangeCellsConsumer implements Consumer<SpreadsheetCellReference> {
+
+    static SpreadsheetRangeCellsConsumer with(final Collection<SpreadsheetCell> cells,
+                                              final Consumer<? super SpreadsheetCell> present,
+                                              final Consumer<? super SpreadsheetCellReference> absent) {
+        Objects.requireNonNull(cells, "cells");
+        Objects.requireNonNull(present, "present");
+        Objects.requireNonNull(absent, "absent");
+
+        return new SpreadsheetRangeCellsConsumer(cells, present, absent);
+    }
+
+
+    SpreadsheetRangeCellsConsumer(final Collection<SpreadsheetCell> cells,
+                                  final Consumer<? super SpreadsheetCell> present,
+                                  final Consumer<? super SpreadsheetCellReference> absent) {
+        Objects.requireNonNull(cells, "cells");
+        Objects.requireNonNull(present, "present");
+        Objects.requireNonNull(absent, "absent");
+
+        cells.stream()
+                .forEach(this::addCell);
+
+        this.present = present;
+        this.absent = absent;
+    }
+
+    private void addCell(final SpreadsheetCell cell) {
+        this.referenceToCells.put(cell.reference(), cell);
+    }
+
+    @Override
+    public void accept(final SpreadsheetCellReference spreadsheetCellReference) {
+        final SpreadsheetCell cell = this.referenceToCells.get(spreadsheetCellReference);
+        if (null != cell) {
+            this.present.accept(cell);
+        } else {
+            this.absent.accept(spreadsheetCellReference);
+        }
+    }
+
+    final Consumer<? super SpreadsheetCell> present;
+    final Consumer<? super SpreadsheetCellReference> absent;
+    final Map<SpreadsheetCellReference, SpreadsheetCell> referenceToCells = Maps.sorted(SpreadsheetCellReference.COMPARATOR);
+
+    @Override
+    public String toString() {
+        return this.present + " " + this.absent;
+    }
+}
---
+++ src/test/java/walkingkooka/spreadsheet/reference/SpreadsheetRangeCellsConsumerTest.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2019 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.reference;
+
+import org.junit.jupiter.api.Test;
+import walkingkooka.collect.list.Lists;
+import walkingkooka.spreadsheet.SpreadsheetCell;
+import walkingkooka.test.ClassTesting2;
+import walkingkooka.test.ToStringTesting;
+import walkingkooka.type.JavaVisibility;
+
+import java.util.function.Consumer;
+
+public final class SpreadsheetRangeCellsConsumerTest implements ClassTesting2<SpreadsheetRangeCellsConsumer>, ToStringTesting<SpreadsheetRangeCellsConsumer> {
+
+    @Test
+    public void testToString() {
+        final Consumer<SpreadsheetCell> present = this::present;
+        final Consumer<SpreadsheetCellReference> absent = this::absent;
+
+        this.toStringAndCheck(new SpreadsheetRangeCellsConsumer(Lists.empty(), present, absent), present + " " + absent);
+    }
+
+    private void present(final SpreadsheetCell cells) {
+    }
+
+    private void absent(final SpreadsheetCellReference reference) {
+    }
+
+    @Override
+    public Class<SpreadsheetRangeCellsConsumer> type() {
+        return SpreadsheetRangeCellsConsumer.class;
+    }
+
+    @Override
+    public JavaVisibility typeVisibility() {
+        return JavaVisibility.PACKAGE_PRIVATE;
+    }
+}
