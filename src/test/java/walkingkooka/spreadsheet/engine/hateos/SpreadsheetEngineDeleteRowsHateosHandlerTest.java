4506a165b663e8cd9b2547b5c558c1879f64adea
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2019-07-18 10:25:45 +1000

    SpreadsheetEngineDeleteOrInsertColumnsOrRowsHateosHandler window filter support
    - added check to check and fail if incoming SpreadsheetDelta if present has ANY cells.

--- src/test/java/walkingkooka/spreadsheet/engine/hateos/SpreadsheetEngineDeleteRowsHateosHandlerTest.java
+++ src/test/java/walkingkooka/spreadsheet/engine/hateos/SpreadsheetEngineDeleteRowsHateosHandlerTest.java
@@ -18,27 +18,34 @@
 package walkingkooka.spreadsheet.engine.hateos;
 
 import org.junit.jupiter.api.Test;
+import walkingkooka.collect.set.Sets;
 import walkingkooka.compare.Range;
 import walkingkooka.net.http.server.HttpRequestAttribute;
 import walkingkooka.net.http.server.hateos.HateosHandler;
+import walkingkooka.spreadsheet.SpreadsheetCell;
 import walkingkooka.spreadsheet.engine.FakeSpreadsheetEngine;
 import walkingkooka.spreadsheet.engine.SpreadsheetDelta;
 import walkingkooka.spreadsheet.engine.SpreadsheetEngine;
 import walkingkooka.spreadsheet.engine.SpreadsheetEngineContext;
+import walkingkooka.spreadsheet.reference.SpreadsheetRange;
 import walkingkooka.spreadsheet.reference.SpreadsheetRowReference;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public final class SpreadsheetEngineDeleteRowsHateosHandlerTest extends SpreadsheetEngineHateosHandlerTestCase2<SpreadsheetEngineDeleteRowsHateosHandler,
+public final class SpreadsheetEngineDeleteRowsHateosHandlerTest extends SpreadsheetEngineDeleteOrInsertColumnsOrRowsHateosHandlerTestCase2<SpreadsheetEngineDeleteRowsHateosHandler,
         SpreadsheetRowReference> {
 
     @Test
     public void testDeleteRow() {
         final Optional<SpreadsheetRowReference> row = this.id();
         final Optional<SpreadsheetDelta<Optional<SpreadsheetRowReference>>> resource = this.resource();
+
+        final Set<SpreadsheetCell> cells = Sets.of(this.cell());
 
         this.handleAndCheck(this.createHandler(new FakeSpreadsheetEngine() {
 
@@ -48,13 +55,13 @@ public final class SpreadsheetEngineDeleteRowsHateosHandlerTest extends Spreadsh
                                                                                        final SpreadsheetEngineContext context) {
                         assertEquals(row.get(), r, "row");
                         assertEquals(1, count, "count");
-                        return delta(Range.singleton(row.get()));
+                        return SpreadsheetDelta.withRange(Range.singleton(row.get()), cells);
                     }
                 }),
                 row,
                 resource,
                 HateosHandler.NO_PARAMETERS,
-                Optional.of(SpreadsheetDelta.withId(row, SpreadsheetDelta.NO_CELLS)));
+                Optional.of(SpreadsheetDelta.withId(row, cells)));
     }
 
     @Test
@@ -62,7 +69,9 @@ public final class SpreadsheetEngineDeleteRowsHateosHandlerTest extends Spreadsh
         final Optional<SpreadsheetDelta<Range<SpreadsheetRowReference>>> resource = this.collectionResource();
 
         final Range<SpreadsheetRowReference> range = SpreadsheetRowReference.parseRange("2:4");
-        final SpreadsheetDelta<Range<SpreadsheetRowReference>> delta = delta(range);
+        final Set<SpreadsheetCell> cells = this.cells();
+
+        final SpreadsheetDelta<Range<SpreadsheetRowReference>> delta = SpreadsheetDelta.withRange(range, cells);
 
         this.handleCollectionAndCheck(this.createHandler(new FakeSpreadsheetEngine() {
 
@@ -79,6 +88,30 @@ public final class SpreadsheetEngineDeleteRowsHateosHandlerTest extends Spreadsh
                 resource,
                 HateosHandler.NO_PARAMETERS,
                 Optional.of(delta));
+    }
+
+    @Test
+    public void testDeleteRowFiltered() {
+        final Optional<SpreadsheetRowReference> row = this.id();
+
+        final Set<SpreadsheetCell> cells = this.cells();
+        final List<SpreadsheetRange> window = this.window();
+
+        this.handleAndCheck(this.createHandler(new FakeSpreadsheetEngine() {
+
+                    @Override
+                    public SpreadsheetDelta<Range<SpreadsheetRowReference>> deleteRows(final SpreadsheetRowReference c,
+                                                                                             final int count,
+                                                                                             final SpreadsheetEngineContext context) {
+                        assertEquals(row.get(), c, "row");
+                        assertEquals(1, count, "count");
+                        return SpreadsheetDelta.withRange(Range.singleton(row.get()), cells);
+                    }
+                }),
+                row,
+                Optional.of(SpreadsheetDelta.withId(row, SpreadsheetDelta.NO_CELLS).setWindow(window)),
+                HateosHandler.NO_PARAMETERS,
+                Optional.of(SpreadsheetDelta.withId(row, this.cellsWithinWindow()).setWindow(window)));
     }
 
     @Test
