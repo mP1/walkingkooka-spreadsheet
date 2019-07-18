5a0fe3b0d0dc3da23e1f8e75dc87493c20389199
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2019-07-18 14:55:13 +1000

    SpreadsheetEngineLoadCellHateosHandler apply window filter

--- src/test/java/walkingkooka/spreadsheet/engine/hateos/SpreadsheetEngineDeleteOrInsertColumnsOrRowsHateosHandlerTestCase2.java
+++ src/test/java/walkingkooka/spreadsheet/engine/hateos/SpreadsheetEngineDeleteOrInsertColumnsOrRowsHateosHandlerTestCase2.java
@@ -18,25 +18,17 @@
 package walkingkooka.spreadsheet.engine.hateos;
 
 import org.junit.jupiter.api.Test;
-import walkingkooka.collect.list.Lists;
 import walkingkooka.collect.set.Sets;
 import walkingkooka.compare.Range;
 import walkingkooka.net.http.server.hateos.HasHateosLinkId;
-import walkingkooka.predicate.PredicateTesting;
-import walkingkooka.spreadsheet.SpreadsheetCell;
 import walkingkooka.spreadsheet.engine.SpreadsheetDelta;
 import walkingkooka.spreadsheet.reference.SpreadsheetColumnOrRowReference;
-import walkingkooka.spreadsheet.reference.SpreadsheetExpressionReference;
-import walkingkooka.spreadsheet.reference.SpreadsheetRange;
 
-import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 
 public abstract class SpreadsheetEngineDeleteOrInsertColumnsOrRowsHateosHandlerTestCase2<H extends SpreadsheetEngineDeleteOrInsertColumnsOrRowsHateosHandler<I>,
         I extends SpreadsheetColumnOrRowReference<I> & HasHateosLinkId>
-        extends SpreadsheetEngineHateosHandlerTestCase2<H, I>
-        implements PredicateTesting {
+        extends SpreadsheetEngineHateosHandlerTestCase2<H, I> {
 
     SpreadsheetEngineDeleteOrInsertColumnsOrRowsHateosHandlerTestCase2() {
         super();
@@ -60,27 +52,5 @@ public abstract class SpreadsheetEngineDeleteOrInsertColumnsOrRowsHateosHandlerT
                 resource,
                 this.parameters(),
                 IllegalArgumentException.class);
-    }
-
-    final Set<SpreadsheetCell> cells() {
-        return Sets.of(this.cell(), this.outsideWindowCell());
-    }
-
-    final Set<SpreadsheetCell> cellsWithinWindow() {
-        return Sets.of(this.cell());
-    }
-
-    final List<SpreadsheetRange> window() {
-        final SpreadsheetRange window = SpreadsheetExpressionReference.parseRange("A1:B99");
-
-        this.testTrue(window, this.cell().reference());
-
-        this.testFalse(window, outsideWindowCell().reference());
-
-        return Lists.of(window);
-    }
-
-    private SpreadsheetCell outsideWindowCell() {
-        return this.cell("Z99", "99");
     }
 }
