1531b00cfa924fed3ad8db6f541dae89d9c564e4
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2020-07-20 19:48:09 +1000

    Sample improvements using SpreadsheetMetadata

--- src/test/java/walkingkooka/spreadsheet/sample/Sample.java
+++ src/test/java/walkingkooka/spreadsheet/sample/Sample.java
@@ -19,17 +19,11 @@ package walkingkooka.spreadsheet.sample;
 
 import walkingkooka.Cast;
 import walkingkooka.Either;
-import walkingkooka.collect.list.Lists;
 import walkingkooka.collect.set.Sets;
 import walkingkooka.color.Color;
-import walkingkooka.convert.Converter;
-import walkingkooka.convert.ConverterContext;
-import walkingkooka.convert.ConverterContexts;
 import walkingkooka.convert.Converters;
-import walkingkooka.datetime.DateTimeContext;
 import walkingkooka.datetime.DateTimeContexts;
-import walkingkooka.math.DecimalNumberContext;
-import walkingkooka.math.DecimalNumberContexts;
+import walkingkooka.net.email.EmailAddress;
 import walkingkooka.spreadsheet.SpreadsheetCell;
 import walkingkooka.spreadsheet.SpreadsheetFormula;
 import walkingkooka.spreadsheet.SpreadsheetId;
@@ -41,12 +35,14 @@ import walkingkooka.spreadsheet.engine.SpreadsheetEngines;
 import walkingkooka.spreadsheet.format.SpreadsheetFormatException;
 import walkingkooka.spreadsheet.format.SpreadsheetFormatter;
 import walkingkooka.spreadsheet.format.SpreadsheetFormatterContext;
-import walkingkooka.spreadsheet.format.SpreadsheetFormatterContexts;
 import walkingkooka.spreadsheet.format.SpreadsheetFormatters;
 import walkingkooka.spreadsheet.format.SpreadsheetText;
 import walkingkooka.spreadsheet.format.parser.SpreadsheetFormatExpressionParserToken;
 import walkingkooka.spreadsheet.format.parser.SpreadsheetFormatParserContexts;
 import walkingkooka.spreadsheet.format.parser.SpreadsheetFormatParsers;
+import walkingkooka.spreadsheet.format.pattern.SpreadsheetPattern;
+import walkingkooka.spreadsheet.meta.SpreadsheetMetadata;
+import walkingkooka.spreadsheet.meta.SpreadsheetMetadataPropertyName;
 import walkingkooka.spreadsheet.parser.SpreadsheetParserContexts;
 import walkingkooka.spreadsheet.parser.SpreadsheetParserToken;
 import walkingkooka.spreadsheet.parser.SpreadsheetParsers;
@@ -64,8 +60,10 @@ import walkingkooka.tree.expression.ExpressionEvaluationContexts;
 import walkingkooka.tree.expression.ExpressionReference;
 import walkingkooka.tree.expression.FunctionExpressionName;
 
-import java.math.MathContext;
+import java.math.RoundingMode;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Locale;
 import java.util.Optional;
 import java.util.Set;
 import java.util.function.BiFunction;
@@ -103,27 +101,47 @@ public final class Sample {
         return SpreadsheetCellStores.treeMap();
     }
 
-    private static Converter converter() {
-        return Converters.collection(Lists.of(Converters.simple(), Converters.numberNumber()));
+    private static SpreadsheetMetadata metadata() {
+        if (null == Sample.metadata) {
+            SpreadsheetMetadata metadata = SpreadsheetMetadata.EMPTY
+                    .set(SpreadsheetMetadataPropertyName.CREATE_DATE_TIME, LocalDateTime.of(2000, 12, 31, 12, 58, 59))
+                    .set(SpreadsheetMetadataPropertyName.CREATOR, EmailAddress.parse("creator@example.com"))
+                    .set(SpreadsheetMetadataPropertyName.CURRENCY_SYMBOL, "$AUD")
+                    .set(SpreadsheetMetadataPropertyName.DATE_FORMAT_PATTERN, SpreadsheetPattern.parseDateFormatPattern("DD/MM/YYYY"))
+                    .set(SpreadsheetMetadataPropertyName.DATE_PARSE_PATTERNS, SpreadsheetPattern.parseDateParsePatterns("DD/MM/YYYYDDMMYYYY"))
+                    .set(SpreadsheetMetadataPropertyName.DATETIME_OFFSET, Converters.JAVA_EPOCH_OFFSET)
+                    .set(SpreadsheetMetadataPropertyName.DATETIME_FORMAT_PATTERN, SpreadsheetPattern.parseDateTimeFormatPattern("DD/MM/YYYY hh:mm"))
+                    .set(SpreadsheetMetadataPropertyName.DATETIME_PARSE_PATTERNS, SpreadsheetPattern.parseDateTimeParsePatterns("DD/MM/YYYY hh:mmDDMMYYYYHHMMDDMMYYYY HHMM"))
+                    .set(SpreadsheetMetadataPropertyName.DECIMAL_SEPARATOR, 'D')
+                    .set(SpreadsheetMetadataPropertyName.EXPONENT_SYMBOL, 'E')
+                    .set(SpreadsheetMetadataPropertyName.GROUPING_SEPARATOR, 'G')
+                    .set(SpreadsheetMetadataPropertyName.LOCALE, Locale.ENGLISH)
+                    .set(SpreadsheetMetadataPropertyName.MODIFIED_BY, EmailAddress.parse("modified@example.com"))
+                    .set(SpreadsheetMetadataPropertyName.MODIFIED_DATE_TIME, LocalDateTime.of(1999, 12, 31, 12, 58, 59))
+                    .set(SpreadsheetMetadataPropertyName.NEGATIVE_SIGN, 'N')
+                    .set(SpreadsheetMetadataPropertyName.NUMBER_FORMAT_PATTERN, SpreadsheetPattern.parseNumberFormatPattern("#0.0"))
+                    .set(SpreadsheetMetadataPropertyName.NUMBER_PARSE_PATTERNS, SpreadsheetPattern.parseNumberParsePatterns("#0.0$#0.00"))
+                    .set(SpreadsheetMetadataPropertyName.PERCENTAGE_SYMBOL, 'P')
+                    .set(SpreadsheetMetadataPropertyName.POSITIVE_SIGN, 'O')
+                    .set(SpreadsheetMetadataPropertyName.PRECISION, 123)
+                    .set(SpreadsheetMetadataPropertyName.ROUNDING_MODE, RoundingMode.FLOOR)
+                    .set(SpreadsheetMetadataPropertyName.SPREADSHEET_ID, SpreadsheetId.with(123))
+                    .set(SpreadsheetMetadataPropertyName.TEXT_FORMAT_PATTERN, SpreadsheetPattern.parseTextFormatPattern("@@"))
+                    .set(SpreadsheetMetadataPropertyName.TIME_FORMAT_PATTERN, SpreadsheetPattern.parseTimeFormatPattern("hh:mm"))
+                    .set(SpreadsheetMetadataPropertyName.TIME_PARSE_PATTERNS, SpreadsheetPattern.parseTimeParsePatterns("hh:mmhh:mm:ss.000"))
+                    .set(SpreadsheetMetadataPropertyName.TWO_DIGIT_YEAR, 31)
+                    .set(SpreadsheetMetadataPropertyName.WIDTH, 10);
+
+            for (int i = 0; i < SpreadsheetMetadata.MAX_NUMBER_COLOR + 2; i++) {
+                metadata = metadata.set(SpreadsheetMetadataPropertyName.numberedColor(i), Color.fromRgb(i));
+            }
+
+            Sample.metadata = metadata;
+        }
+        return Sample.metadata;
     }
 
-    private static ConverterContext converterContext() {
-        return ConverterContexts.basic(dateTimeContext(), decimalNumberContext());
-    }
-
-    private static DateTimeContext dateTimeContext() {
-        return DateTimeContexts.locale(decimalNumberContext().locale(), 50);
-    }
-
-    private static DecimalNumberContext decimalNumberContext() {
-        return DecimalNumberContexts.american(MathContext.DECIMAL32);
-    }
-
-    private static BiFunction<FunctionExpressionName, List<Object>, Object> functions() {
-        return (n, p) -> {
-            throw new UnsupportedOperationException("unsupported function " + n + " params:" + p);
-        };
-    }
+    private static SpreadsheetMetadata metadata;
 
     private static SpreadsheetEngine engine(final SpreadsheetCellStore cellStore,
                                             final SpreadsheetLabelStore labelStore) {
@@ -139,23 +157,32 @@ public final class Sample {
 
     private static SpreadsheetEngineContext engineContext(final SpreadsheetEngine engine,
                                                           final SpreadsheetLabelStore labelStore) {
+        final SpreadsheetMetadata metadata = metadata();
         return new FakeSpreadsheetEngineContext() {
 
             @Override
             public SpreadsheetParserToken parseFormula(final String formula) {
                 return Cast.to(SpreadsheetParsers.expression()
                         .orFailIfCursorNotEmpty(ParserReporters.basic())
-                        .parse(TextCursors.charSequence(formula), SpreadsheetParserContexts.basic(DateTimeContexts.fake(), converterContext()))
+                        .parse(TextCursors.charSequence(formula), SpreadsheetParserContexts.basic(DateTimeContexts.fake(), metadata.converterContext()))
                         .get());
             }
 
             @Override
             public Object evaluate(final Expression node) {
-                return node.toValue(ExpressionEvaluationContexts.basic(functions(), references(), converter(), converterContext()));
+                return node.toValue(ExpressionEvaluationContexts.basic(functions(),
+                        references(),
+                        metadata.converter(),
+                        metadata.converterContext()));
+            }
+
+            private BiFunction<FunctionExpressionName, List<Object>, Object> functions() {
+                return (n, p) -> {
+                    throw new UnsupportedOperationException("unsupported function " + n + " params:" + p);
+                };
             }
 
             private Function<ExpressionReference, Optional<Expression>> references() {
-                //return (r) -> {throw new UnsupportedOperationException(r.toString());};
                 return SpreadsheetEngines.expressionEvaluationContextExpressionReferenceExpressionFunction(engine, labelStore, this);
             }
 
@@ -192,6 +219,13 @@ public final class Sample {
     }
 
     /**
+     * A {@lnk SpreadsheetFormatterContext} that is fully functional except for translating colour numbers and colour names to a {@link Color}.
+     */
+    private static SpreadsheetFormatterContext formatterContext() {
+        return metadata().formatterContext(defaultSpreadsheetFormatter());
+    }
+
+    /**
      * A {@link SpreadsheetFormatter} that accepts all values and creates a {@link SpreadsheetText} with {@link Object#toString()} and no colour.
      */
     private static SpreadsheetFormatter defaultSpreadsheetFormatter() {
@@ -208,21 +242,5 @@ public final class Sample {
                 return Optional.of(SpreadsheetText.with(SpreadsheetText.WITHOUT_COLOR, String.valueOf(value)));
             }
         };
-    }
-
-    /**
-     * A {@lnk SpreadsheetFormatterContext} that is fully functional except for translating colour numbers and colour names to a {@link Color}.
-     */
-    private static SpreadsheetFormatterContext formatterContext() {
-        return SpreadsheetFormatterContexts.basic(
-                (n) -> Optional.empty(), // always nothing
-                (n) -> {
-                    throw new UnsupportedOperationException();
-                },
-                80,
-                converter(),
-                defaultSpreadsheetFormatter(),
-                converterContext()
-        );
     }
 }
