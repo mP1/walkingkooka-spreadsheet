4c460b10e66515274da386fb12e11270a548ff07
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2021-07-06 19:18:25 +1000

    SpreadsheetSelection.parseXXX pulled from SpreadsheetExpressionReference

--- src/main/java/walkingkooka/spreadsheet/reference/SpreadsheetSelection.java
+++ src/main/java/walkingkooka/spreadsheet/reference/SpreadsheetSelection.java
@@ -17,10 +17,191 @@
 
 package walkingkooka.spreadsheet.reference;
 
+import walkingkooka.collect.Range;
+import walkingkooka.spreadsheet.parser.SpreadsheetParsers;
+import walkingkooka.text.CharSequences;
+
+import java.util.Objects;
+
 /**
  * Base class for all selection types, including columns, rows, cells, labels and ranges.
  */
 public abstract class SpreadsheetSelection {
+
+    // modes used by isTextCellReference
+    private final static int MODE_COLUMN_FIRST = 0;
+    private final static int MODE_COLUMN = MODE_COLUMN_FIRST + 1;
+    private final static int MODE_ROW_FIRST = MODE_COLUMN + 1;
+    private final static int MODE_ROW = MODE_ROW_FIRST + 1;
+    private final static int MODE_FAIL = MODE_ROW + 1;
+
+    /**
+     * Tests if the {@link String name} is a valid cell reference.
+     */
+    public static boolean isCellReferenceText(final String text) {
+        Objects.requireNonNull(text, "text");
+
+        int mode = MODE_COLUMN_FIRST; // -1 too long or contains invalid char
+        int columnLength = 0;
+        int column = 0;
+        int row = 0;
+
+        // AB11 max row, max column
+        final int length = text.length();
+        for (int i = 0; i < length; i++) {
+            final char c = text.charAt(i);
+
+            if (MODE_COLUMN_FIRST == mode) {
+                mode = MODE_COLUMN;
+                if (SpreadsheetReferenceKind.ABSOLUTE_PREFIX == c) {
+                    continue;
+                }
+                // fall-thru might be column letter
+            }
+
+            // try and consume column letters
+            if (MODE_COLUMN == mode) {
+                final int digit = SpreadsheetParsers.valueFromDigit(c);
+                if (-1 != digit) {
+                    column = column * SpreadsheetColumnReference.RADIX + digit;
+                    if (column > 1 + SpreadsheetColumnReference.MAX_VALUE) {
+                        mode = MODE_FAIL;
+                        break; // column is too big cant be a cell reference.
+                    }
+                    columnLength++;
+                    continue;
+                }
+                if (0 == columnLength) {
+                    mode = MODE_FAIL;
+                    break;
+                }
+                mode = MODE_ROW_FIRST;
+            }
+
+            if (MODE_ROW_FIRST == mode) {
+                mode = MODE_ROW;
+                if (SpreadsheetReferenceKind.ABSOLUTE_PREFIX == c) {
+                    continue;
+                }
+                // fall-thru might be row letter
+            }
+
+
+            if (MODE_ROW == mode) {
+                final int digit = Character.digit(c, SpreadsheetRowReference.RADIX);
+                if (-1 != digit) {
+                    row = SpreadsheetRowReference.RADIX * row + digit;
+                    if (row > 1 + SpreadsheetRowReference.MAX_VALUE) {
+                        mode = MODE_FAIL;
+                        break; // row is too big cant be a cell reference.
+                    }
+                    continue;
+                }
+                mode = MODE_FAIL;
+                break;
+            }
+        }
+
+        // ran out of characters still checking row must be a valid cell reference.
+        return MODE_ROW == mode;
+    }
+
+    // sub class factories..............................................................................................
+
+    /**
+     * {@see SpreadsheetCellReference}
+     */
+    public static SpreadsheetCellReference cellReference(final SpreadsheetColumnReference column,
+                                                         final SpreadsheetRowReference row) {
+        return SpreadsheetCellReference.with(column, row);
+    }
+
+    /**
+     * {@see SpreadsheetLabelName}
+     */
+    public static SpreadsheetLabelName labelName(final String name) {
+        return SpreadsheetLabelName.with(name);
+    }
+
+    // parse............................................................................................................
+
+    /**
+     * Parsers the given text into of the sub classes of {@link SpreadsheetExpressionReference}.
+     */
+    public static SpreadsheetExpressionReference parseExpressionReference(final String text) {
+        Objects.requireNonNull(text, "text");
+
+        final SpreadsheetExpressionReference reference;
+
+        switch (text.split(":").length) {
+            case 1:
+                reference = isCellReferenceText(text) ?
+                        parseCellReference(text) :
+                        labelName(text);
+                break;
+            case 2:
+                reference = parseRange(text);
+                break;
+            default:
+                throw new IllegalArgumentException("Expected cell, label or range got " + CharSequences.quote(text));
+        }
+
+        return reference;
+    }
+
+    /**
+     * Parsers a range of cell referencs.
+     */
+    public static Range<SpreadsheetCellReference> parseCellReferenceRange(final String text) {
+        return SpreadsheetCellReference.parseCellReferenceRange0(text);
+    }
+
+    /**
+     * Parsers the text expecting a valid {@link SpreadsheetCellReference} or fails.
+     */
+    public static SpreadsheetCellReference parseCellReference(final String text) {
+        return SpreadsheetCellReference.parseCellReference0(text);
+    }
+
+    /**
+     * Parses text expecting either a {@link SpreadsheetCellReference} or {@link SpreadsheetLabelName}
+     */
+    public static SpreadsheetCellReferenceOrLabelName parseCellReferenceOrLabelName(final String text) {
+        Objects.requireNonNull(text, "text");
+
+        return isCellReferenceText(text) ?
+                parseCellReference(text) :
+                labelName(text);
+    }
+
+    /**
+     * {@see #parse}
+     */
+    public static SpreadsheetLabelMappingExpressionReference parseSpreadsheetLabelMappingExpressionReference(final String text) {
+        Objects.requireNonNull(text, "text");
+
+        final SpreadsheetLabelMappingExpressionReference reference;
+
+        switch (text.split(":").length) {
+            case 1:
+                reference = isCellReferenceText(text) ?
+                        parseCellReference(text) :
+                        labelName(text);
+                break;
+            default:
+                reference = parseRange(text);
+                break;
+        }
+
+        return reference;
+    }
+
+    /**
+     * Parsers the text expecting a valid {@link SpreadsheetRange} or fails.
+     */
+    public static SpreadsheetRange parseRange(final String text) {
+        return SpreadsheetRange.parseRange0(text);
+    }
 
     SpreadsheetSelection() {
         super();
--- src/test/java/walkingkooka/spreadsheet/reference/SpreadsheetExpressionReferenceTest.java
+++ src/test/java/walkingkooka/spreadsheet/reference/SpreadsheetExpressionReferenceTest.java
@@ -18,144 +18,16 @@
 package walkingkooka.spreadsheet.reference;
 
 import org.junit.jupiter.api.Test;
-import walkingkooka.InvalidCharacterException;
 import walkingkooka.reflect.ClassTesting2;
 import walkingkooka.reflect.JavaVisibility;
-import walkingkooka.test.ParseStringTesting;
-import walkingkooka.text.CharSequences;
 import walkingkooka.tree.json.JsonNode;
 import walkingkooka.tree.json.marshall.JsonNodeMarshallingTesting;
 import walkingkooka.tree.json.marshall.JsonNodeUnmarshallContext;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public final class SpreadsheetExpressionReferenceTest implements ClassTesting2<SpreadsheetExpressionReference>,
-        JsonNodeMarshallingTesting<SpreadsheetExpressionReference>,
-        ParseStringTesting<SpreadsheetExpressionReference> {
-
-    @Test
-    public void testIsCellReferenceTextNullFails() {
-        assertThrows(NullPointerException.class, () -> SpreadsheetExpressionReference.isCellReferenceText(null));
-    }
-
-    @Test
-    public void testIsCellReferenceTextEmptyFalse() {
-        this.isCellReferenceTextAndCheck("", false);
-    }
-
-    @Test
-    public void testIsCellReferenceTextInvalidCharacterFalse() {
-        this.isCellReferenceTextAndCheck("!", false);
-    }
-
-    @Test
-    public void testIsCellReferenceTextAbsolutePrefixOnlyFalse() {
-        this.isCellReferenceTextAndCheck("$", false);
-    }
-
-    @Test
-    public void testIsCellReferenceTextAbsoluteColumnOnlyFalse() {
-        this.isCellReferenceTextAndCheck("$A", false);
-    }
-
-    @Test
-    public void testIsCellReferenceTextColumnOnlyFalse() {
-        this.isCellReferenceTextAndCheck("A", false);
-    }
-
-    @Test
-    public void testIsCellReferenceTextAbsoluteRowOnlyFalse() {
-        this.isCellReferenceTextAndCheck("$9", false);
-    }
-
-    @Test
-    public void testIsCellReferenceTextRowOnlyFalse() {
-        this.isCellReferenceTextAndCheck("9", false);
-    }
-
-    @Test
-    public void testIsCellReferenceTextRowOnlyFalse2() {
-        this.isCellReferenceTextAndCheck("98", false);
-    }
-
-    @Test
-    public void testIsCellReferenceTextWithCellReferenceUppercase() {
-        this.isCellReferenceTextAndCheck("A1", true);
-    }
-
-    @Test
-    public void testIsCellReferenceTextWithCellReferenceUppercaseAbsolute() {
-        this.isCellReferenceTextAndCheck("$A1", true);
-    }
-
-    @Test
-    public void testIsCellReferenceTextWithCellReferenceLowercase() {
-        this.isCellReferenceTextAndCheck("a1", true);
-    }
-
-    @Test
-    public void testIsCellReferenceTextWithCellReferenceLowercaseExtra() {
-        this.isCellReferenceTextAndCheck("a1!", false);
-    }
-
-    @Test
-    public void testIsCellReferenceTextWithCellReferenceLowercaseAbsolute() {
-        this.isCellReferenceTextAndCheck("$a1", true);
-    }
-
-    @Test
-    public void testIsCellReferenceTextAbsoluteColumnAbsoluteRow() {
-        this.isCellReferenceTextAndCheck("$a$1", true);
-    }
-
-    @Test
-    public void testIsCellReferenceTextAbsoluteColumnAbsoluteRowExtra() {
-        this.isCellReferenceTextAndCheck("$a$1!", false);
-    }
-
-    @Test
-    public void testIsCellReferenceTextLastColumn() {
-        this.isCellReferenceTextAndCheck("XFD1", true);
-    }
-
-    @Test
-    public void testIsCellReferenceTextLastColumnPlus1() {
-        this.isCellReferenceTextAndCheck("XFE1", false);
-    }
-
-    @Test
-    public void testIsCellReferenceTextLastRow() {
-        this.isCellReferenceTextAndCheck("A1048576", true);
-    }
-
-    @Test
-    public void testIsCellReferenceTextLastRowPlus1() {
-        this.isCellReferenceTextAndCheck("A1048577", false);
-    }
-
-    @Test
-    public void testIsCellReferenceTextWithLabel() {
-        this.isCellReferenceTextAndCheck("LABEL123", false);
-    }
-
-    private void isCellReferenceTextAndCheck(final String text, final boolean expected) {
-        assertEquals(expected,
-                SpreadsheetExpressionReference.isCellReferenceText(text),
-                () -> "isCellReferenceText " + CharSequences.quoteAndEscape(text));
-        if (expected) {
-            SpreadsheetExpressionReference.parseExpressionReference(text);
-        }
-    }
-
-    // cellReference....................................................................................................
-
-    @Test
-    public void testCellReference() {
-        assertEquals(SpreadsheetExpressionReference.parseCellReference("A1"),
-                SpreadsheetExpressionReference.cellReference(SpreadsheetColumnOrRowReference.parseColumn("A"),
-                        SpreadsheetColumnOrRowReference.parseRow("1")));
-    }
+        JsonNodeMarshallingTesting<SpreadsheetExpressionReference> {
 
     // unmarshall.....................................................................................................
 
@@ -188,109 +60,6 @@ public final class SpreadsheetExpressionReferenceTest implements ClassTesting2<S
         this.marshallRoundTripTwiceAndCheck(SpreadsheetExpressionReference.parseRange("B2:C3"));
     }
 
-    // parse............................................................................................................
-
-    @Test
-    public void testParseCellReferenceUpperCaseRelativeRelative() {
-        final String reference = "A2";
-        this.parseStringAndCheck(reference, SpreadsheetReferenceKind.RELATIVE.column(0).setRow(SpreadsheetReferenceKind.RELATIVE.row(1)));
-    }
-
-    @Test
-    public void testParseCellReferenceUpperCaseRelativeAbsolute() {
-        final String reference = "C$4";
-        this.parseStringAndCheck(reference, SpreadsheetReferenceKind.RELATIVE.column(2).setRow(SpreadsheetReferenceKind.ABSOLUTE.row(3)));
-    }
-
-    @Test
-    public void testParseCellReferenceUpperCaseAbsoluteRelative() {
-        final String reference = "$E6";
-        this.parseStringAndCheck(reference, SpreadsheetReferenceKind.ABSOLUTE.column(4).setRow(SpreadsheetReferenceKind.RELATIVE.row(5)));
-    }
-
-    @Test
-    public void testParseCellReferenceUpperCaseAbsoluteAbsolute() {
-        final String reference = "$G$8";
-        this.parseStringAndCheck(reference, SpreadsheetReferenceKind.ABSOLUTE.column(6).setRow(SpreadsheetReferenceKind.ABSOLUTE.row(7)));
-    }
-
-    @Test
-    public void testParseCellReferenceLowercaseRelativeRelative() {
-        final String reference = "i10";
-        this.parseStringAndCheck(reference, SpreadsheetReferenceKind.RELATIVE.column(8).setRow(SpreadsheetReferenceKind.RELATIVE.row(9)));
-    }
-
-    @Test
-    public void testParseCellReferenceLowercaseAbsolute() {
-        final String reference = "$k12";
-        this.parseStringAndCheck(reference, SpreadsheetReferenceKind.ABSOLUTE.column(10).setRow(SpreadsheetReferenceKind.RELATIVE.row(11)));
-    }
-
-    @Test
-    public void testParseLabel() {
-        final String label = "label123";
-        this.parseStringAndCheck(label, SpreadsheetExpressionReference.labelName(label));
-    }
-
-    @Test
-    public void testParseRange() {
-        final String range = "A2:B2";
-        this.parseStringAndCheck(range, SpreadsheetExpressionReference.parseRange(range));
-    }
-
-    // parseCellReferenceOrLabelName....................................................................................
-
-    @Test
-    public void testParseCellReferenceOrLabelNameNullFails() {
-        parseCellReferenceOrLabelNameFails(
-                null,
-                NullPointerException.class
-        );
-    }
-
-    @Test
-    public void testParseCellReferenceOrLabelNameEmptyFails() {
-        parseCellReferenceOrLabelNameFails(
-                "",
-                IllegalArgumentException.class
-        );
-    }
-
-    @Test
-    public void testParseCellReferenceOrLabelNameRangeFails() {
-        parseCellReferenceOrLabelNameFails(
-                SpreadsheetExpressionReference.parseRange("A1:B2").toString(),
-                InvalidCharacterException.class
-        );
-    }
-
-    private void parseCellReferenceOrLabelNameFails(final String text,
-                                                    final Class<? extends RuntimeException> thrown) {
-        assertThrows(thrown, () -> SpreadsheetCellReferenceOrLabelName.parseCellReferenceOrLabelName(text));
-    }
-
-    @Test
-    public void testParseCellReferenceOrLabelNameCell() {
-        final String text = "A1";
-        this.parseCellReferenceOrLabelNameAndCheck(text, SpreadsheetCellReference.parseCellReference(text));
-    }
-
-    @Test
-    public void testParseCellReferenceOrLabelNameLabel() {
-        final String text = "Label123";
-        this.parseCellReferenceOrLabelNameAndCheck(text, SpreadsheetCellReference.labelName(text));
-    }
-
-    private void parseCellReferenceOrLabelNameAndCheck(final String text,
-                                                       final SpreadsheetCellReferenceOrLabelName expected) {
-        final SpreadsheetCellReferenceOrLabelName parsed = SpreadsheetCellReferenceOrLabelName.parseCellReferenceOrLabelName(text);
-        assertEquals(
-                expected,
-                parsed,
-                () -> "Parsing of " + CharSequences.quoteAndEscape(text) + " failed"
-        );
-    }
-
     // ClassTesting.....................................................................................................
 
     @Override
@@ -314,22 +83,5 @@ public final class SpreadsheetExpressionReferenceTest implements ClassTesting2<S
     @Override
     public SpreadsheetExpressionReference createJsonNodeMappingValue() {
         return SpreadsheetExpressionReference.parseExpressionReference("A1");
-    }
-
-    // ParseStringTesting...............................................................................................
-
-    @Override
-    public SpreadsheetExpressionReference parseString(final String text) {
-        return SpreadsheetExpressionReference.parseExpressionReference(text);
-    }
-
-    @Override
-    public Class<? extends RuntimeException> parseStringFailedExpected(Class<? extends RuntimeException> throwing) {
-        return throwing;
-    }
-
-    @Override
-    public RuntimeException parseStringFailedExpected(final RuntimeException expected) {
-        return expected;
     }
 }
