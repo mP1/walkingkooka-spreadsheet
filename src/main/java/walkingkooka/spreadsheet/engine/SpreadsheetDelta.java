9ce90cf4a5da7e61c4a2264ae4308dd988dbdcf8
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2022-02-24 00:11:01 +1100

    SpreadsheetDelta.patch deletedColumns & deletedRows fails

--- src/main/java/walkingkooka/spreadsheet/engine/SpreadsheetDelta.java
+++ src/main/java/walkingkooka/spreadsheet/engine/SpreadsheetDelta.java
@@ -810,6 +810,8 @@ public abstract class SpreadsheetDelta implements Patchable<SpreadsheetDelta>,
                 case LABELS_PROPERTY_STRING:
                 case ROWS_PROPERTY_STRING:
                 case DELETED_CELLS_PROPERTY_STRING:
+                case DELETED_COLUMNS_PROPERTY_STRING:
+                case DELETED_ROWS_PROPERTY_STRING:
                 case COLUMN_WIDTHS_PROPERTY_STRING:
                 case ROW_HEIGHTS_PROPERTY_STRING:
                     Patchable.invalidPropertyPresent(propertyName, propertyAndValue);
