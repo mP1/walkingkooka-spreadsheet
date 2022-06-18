954247f973fdc2674ca7a10d53963983856de9ce
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2022-06-18 13:33:58 +1000

    SpreadsheetErrorKind.translateMessage more ClassCastException extracting

--- src/main/java/walkingkooka/spreadsheet/SpreadsheetErrorKindClassCastExceptionMessage.java
+++ src/main/java/walkingkooka/spreadsheet/SpreadsheetErrorKindClassCastExceptionMessage.java
@@ -26,32 +26,109 @@ final class SpreadsheetErrorKindClassCastExceptionMessage {
 
     // "walkingkooka.spreadsheet.SpreadsheetErrorKindTest cannot be cast to java.base/java.lang.Void"
     // ->
+    // class walkingkooka.spreadsheet.SpreadsheetError cannot be cast to class walkingkooka.tree.expression.ExpressionNumber
+    // (walkingkooka.spreadsheet.SpreadsheetError and walkingkooka.tree.expression.ExpressionNumber are in unnamed module of loader 'app')
+    //
     // Failed to convert $FROM to $TO
     static String extractClassCastExceptionMessage(final String message) {
-        final String[] tokens = message.split(" ");
+        String result = message;
 
-        return tokens.length < 2 ?
-                message :
-                extractClassCastExceptionMessage0(tokens);
+        final String[] tokens = message.split(" ");
+        if (tokens.length > 7) {
+            // class walkingkooka.spreadsheet.SpreadsheetError cannot be cast to class walkingkooka.tree.expression.ExpressionNumber
+            //
+            // 0=class
+            // 1=walkingkooka.spreadsheet.SpreadsheetError
+            // 2=cannot
+            // 3=be
+            // 4=cast
+            // 5=to
+            // 6=class
+            // 7=walkingkooka.tree.expression.ExpressionNumber
+            if (tokens[0].equals("class") &&
+                    tokens[2].equals("cannot") &&
+                    tokens[3].equals("be") &&
+                    tokens[4].equals("cast") &&
+                    tokens[5].equals("to") &&
+                    tokens[6].equals("class")) {
+                result = classClassNameCannotBeCastToClass(tokens);
+            }
+        }
+        if (tokens.length > 2) {
+            // "walkingkooka.spreadsheet.SpreadsheetErrorKindTest cannot be cast to java.base/java.lang.Void"
+            //
+            // 0="walkingkooka.spreadsheet.SpreadsheetErrorKindTest
+            // 1=cannot
+            // 2=be
+            // 3=cast
+            // 4=to
+            // 5=java.base/java.lang.Void"
+            if (tokens[1].equals("cannot") &&
+                    tokens[2].equals("be") &&
+                    tokens[3].equals("cast") &&
+                    tokens[4].equals("to")) {
+                result = classCannotBeCastTo(tokens);
+            }
+        }
+        return result;
     }
 
-    private static String extractClassCastExceptionMessage0(final String[] tokens) {
-        final String first = tokens[0];
+    // class walkingkooka.spreadsheet.SpreadsheetError cannot be cast to class walkingkooka.tree.expression.ExpressionNumber
+    // (walkingkooka.spreadsheet.SpreadsheetError and walkingkooka.tree.expression.ExpressionNumber are in unnamed module of loader 'app')
+    //
+    // 0=class
+    // 1=walkingkooka.spreadsheet.SpreadsheetError
+    // 2=cannot
+    // 3=be
+    // 4=cast
+    // 5=to
+    // 6=class
+    // 7=walkingkooka.tree.expression.ExpressionNumber
+    //
+    // from = 1
+    // to = 7
+    private static String classClassNameCannotBeCastToClass(final String[] tokens) {
+        return failedToConvert(
+                tokens[1],
+                tokens[7]
+        );
+    }
 
-        String last = tokens[tokens.length - 1];
-        final int slash = last.indexOf('/');
-        if (-1 != slash) {
-            last = last.substring(slash + 1);
-        }
+    // "walkingkooka.spreadsheet.SpreadsheetErrorKindTest cannot be cast to java.base/java.lang.Void"
 
-        return "Failed to convert " + extractSimpleClassName(first) + " to " + extractSimpleClassName(last);
+    // from = first
+    // to = last
+    private static String classCannotBeCastTo(final String[] tokens) {
+        return failedToConvert(
+                tokens[0],
+                tokens[tokens.length - 1]
+        );
+    }
+
+    private static String failedToConvert(final String from,
+                                          final String to) {
+        return "Failed to convert " + extractSimpleClassName(from) + " to " + extractSimpleClassName(to);
     }
 
     private static String extractSimpleClassName(final String typeName) {
-        final int lastDot = typeName.lastIndexOf('.');
+        String simple = typeName;
 
-        return -1 == lastDot ?
-                typeName :
-                typeName.substring(lastDot + 1);
+        // there could be a mention in message in parens
+        final int leftParens = simple.indexOf('(');
+        if (-1 != leftParens) {
+            simple = simple.substring(0, leftParens);
+        }
+
+        final int slash = simple.indexOf('/');
+        if (-1 != slash) {
+            simple = simple.substring(slash + 1);
+        }
+
+        final int lastDot = simple.lastIndexOf('.');
+        if (-1 != lastDot) {
+            simple = simple.substring(lastDot + 1);
+        }
+
+        return simple;
     }
 }
