d9b87fa85c62b93e59292d7e6d955e9f2e8b40d4
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2022-11-08 20:29:20 +1100

    Unnecessary semi-colon removed

--- src/main/java/walkingkooka/spreadsheet/format/pattern/SpreadsheetFormatPatternCreateFormatterSpreadsheetFormatParserTokenVisitorComponent.java
+++ src/main/java/walkingkooka/spreadsheet/format/pattern/SpreadsheetFormatPatternCreateFormatterSpreadsheetFormatParserTokenVisitorComponent.java
@@ -31,7 +31,6 @@ final class SpreadsheetFormatPatternCreateFormatterSpreadsheetFormatParserTokenV
 
     SpreadsheetFormatPatternCreateFormatterSpreadsheetFormatParserTokenVisitorComponent() {
         super();
-        ;
     }
 
     SpreadsheetFormatConditionParserToken condition;
