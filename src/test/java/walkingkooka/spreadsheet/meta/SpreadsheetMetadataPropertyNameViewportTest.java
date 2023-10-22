06c92c91b428757ce91a732e49377b48ce383090
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2023-10-22 17:32:43 +1100

    SpreadsheetViewport.home & AnchoredSpreadsheetSelection selection change 2/2

---
+++ src/test/java/walkingkooka/spreadsheet/meta/SpreadsheetMetadataPropertyNameViewportTest.java
@@ -0,0 +1,144 @@
+/*
+ * Copyright 2019 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.meta;
+
+import org.junit.jupiter.api.Test;
+import walkingkooka.spreadsheet.reference.SpreadsheetLabelName;
+import walkingkooka.spreadsheet.reference.SpreadsheetSelection;
+import walkingkooka.spreadsheet.reference.SpreadsheetViewport;
+import walkingkooka.spreadsheet.reference.SpreadsheetViewportAnchor;
+
+import java.util.Locale;
+import java.util.Optional;
+
+
+public final class SpreadsheetMetadataPropertyNameViewportTest extends SpreadsheetMetadataPropertyNameTestCase<
+        SpreadsheetMetadataPropertyNameViewport,
+        SpreadsheetViewport> {
+
+    @Test
+    public void testCheckCellRange() {
+        this.checkValue(
+                SpreadsheetSelection.A1.viewportRectangle(100, 40)
+                        .viewport()
+                        .setSelection(
+                                Optional.of(
+                                        SpreadsheetSelection.parseCellRange("A1:B2")
+                                                .setAnchor(SpreadsheetViewportAnchor.BOTTOM_RIGHT)
+                                )
+                        )
+        );
+    }
+
+    @Test
+    public void testCheckColumn() {
+        this.checkValue(
+                SpreadsheetSelection.A1.viewportRectangle(100, 40)
+                        .viewport()
+                        .setSelection(
+                                Optional.of(
+                                        SpreadsheetSelection.parseColumn("AB")
+                                                .setAnchor(SpreadsheetViewportAnchor.NONE)
+                                )
+                        )
+        );
+    }
+
+    @Test
+    public void testCheckLabel() {
+        this.checkValue(
+                SpreadsheetSelection.A1.viewportRectangle(100, 40)
+                        .viewport()
+                        .setSelection(
+                                Optional.of(
+                                        SpreadsheetLabelName.labelName("Label123")
+                                                .setAnchor(SpreadsheetViewportAnchor.NONE)
+                                )
+                        )
+        );
+    }
+
+    @Test
+    public void testCheckRow() {
+        this.checkValue(
+                SpreadsheetSelection.A1.viewportRectangle(100, 40)
+                        .viewport()
+                        .setSelection(
+                                Optional.of(
+                                        SpreadsheetSelection.parseRow("1234")
+                                                .setAnchor(SpreadsheetViewportAnchor.NONE)
+                                )
+                        )
+        );
+    }
+
+    @Test
+    public void testInvalidValueFails() {
+        this.checkValueFails(
+                "invalid",
+                "Expected SpreadsheetViewport, but got \"invalid\" for \"viewport\""
+        );
+    }
+
+    @Test
+    public void testExtractLocaleValue() {
+        this.extractLocaleValueAndCheck(Locale.ENGLISH, null);
+    }
+
+    @Test
+    public void testToString() {
+        this.toStringAndCheck(
+                SpreadsheetMetadataPropertyNameViewport.instance(),
+                "viewport"
+        );
+    }
+
+    @Override
+    SpreadsheetMetadataPropertyNameViewport createName() {
+        return SpreadsheetMetadataPropertyNameViewport.instance();
+    }
+
+    @Override
+    SpreadsheetViewport propertyValue() {
+        return SpreadsheetSelection.A1.viewportRectangle(100, 40)
+                .viewport()
+                .setSelection(
+                        Optional.of(
+                                SpreadsheetSelection.parseCell("B99")
+                                        .setDefaultAnchor()
+                        )
+                );
+    }
+
+    @Override
+    String propertyValueType() {
+        return SpreadsheetViewport.class.getSimpleName();
+    }
+
+    @Override
+    String urlFragment() {
+        return "/viewport";
+    }
+
+    // ClassTesting.....................................................................................................
+
+    @Override
+    public Class<SpreadsheetMetadataPropertyNameViewport> type() {
+        return SpreadsheetMetadataPropertyNameViewport.class;
+    }
+}
---
+++ src/test/java/walkingkooka/spreadsheet/reference/SpreadsheetViewportNavigationColumnOrRowTestCase.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2019 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.reference;
+
+import org.junit.jupiter.api.Test;
+import walkingkooka.Cast;
+import walkingkooka.collect.list.Lists;
+import walkingkooka.predicate.Predicates;
+import walkingkooka.test.ParseStringTesting;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public abstract class SpreadsheetViewportNavigationColumnOrRowTestCase<T extends SpreadsheetViewportNavigationColumnOrRow> extends
+        SpreadsheetViewportNavigationTestCase2<T>  {
+
+    SpreadsheetViewportNavigationColumnOrRowTestCase() {
+        super();
+    }
+
+    // update...........................................................................................................
+
+    final static SpreadsheetCellReference HOME = SpreadsheetCellReference.A1;
+
+    final void updateAndCheck(final SpreadsheetSelection selection) {
+        this.updateAndCheck(
+                selection,
+                selection
+        );
+    }
+    final void updateAndCheck(final SpreadsheetSelection selection,
+                              final SpreadsheetSelection expected) {
+        this.updateAndCheck(
+                selection,
+                expected.setDefaultAnchor()
+        );
+    }
+
+    final void updateAndCheck(final SpreadsheetSelection selection,
+                              final AnchoredSpreadsheetSelection expected) {
+        this.updateAndCheck(
+                selection.setAnchor(SpreadsheetViewportAnchor.NONE),
+                expected
+        );
+    }
+
+    final void updateAndCheck(final AnchoredSpreadsheetSelection selection,
+                              final AnchoredSpreadsheetSelection expected) {
+        this.updateAndCheck(
+                this.createSpreadsheetViewportNavigation(),
+                HOME.viewportRectangle(VIEWPORT_WIDTH, VIEWPORT_HEIGHT)
+                        .viewport()
+                        .setSelection(
+                                Optional.of(
+                                        selection
+                                )
+                        ),
+                HOME.viewportRectangle(VIEWPORT_WIDTH, VIEWPORT_HEIGHT)
+                        .viewport()
+                        .setSelection(
+                                Optional.of(
+                                        expected
+                                )
+                        )
+        );
+    }
+}
