0f66cf5eac1ab295fe46692d2f1f1426da5e1c7a
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2024-01-16 22:31:42 +1100

    SpreadsheetFormula.replaceReferences(Function<SpreadsheetCellReference, Optional<SpreadsheetCellReference>);

---
+++ src/main/java/walkingkooka/spreadsheet/SpreadsheetFormulaReplaceReferencesFunctionParserToken.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2019 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet;
+
+import walkingkooka.spreadsheet.parser.SpreadsheetCellReferenceParserToken;
+import walkingkooka.spreadsheet.reference.SpreadsheetCellReference;
+import walkingkooka.text.cursor.parser.ParserToken;
+
+import java.util.Optional;
+import java.util.function.Function;
+
+final class SpreadsheetFormulaReplaceReferencesFunctionParserToken extends SpreadsheetFormulaReplaceReferencesFunction<ParserToken> {
+
+    static SpreadsheetFormulaReplaceReferencesFunctionParserToken with(final Function<SpreadsheetCellReference, Optional<SpreadsheetCellReference>> mapper) {
+        return new SpreadsheetFormulaReplaceReferencesFunctionParserToken(mapper);
+    }
+
+    private SpreadsheetFormulaReplaceReferencesFunctionParserToken(final Function<SpreadsheetCellReference, Optional<SpreadsheetCellReference>> mapper) {
+        super(mapper);
+    }
+
+    @Override
+    public ParserToken apply(final ParserToken token) {
+        return this.mapper.apply(
+                        token.cast(SpreadsheetCellReferenceParserToken.class).cell()
+                ).map(t -> (ParserToken) t.toParserToken())
+                .orElse(token);
+    }
+}
