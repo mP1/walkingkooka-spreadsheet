befcccab09b51fb65016e069eb53d0d102dd898c
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2024-03-19 19:30:37 +1100

    SpreadsheetEngineSpreadsheetExpressionEvaluationContext.parseFormula parses expressions not formulas
    - Previously it was parsing formula expressions and values which meant it could not parse a simple addition like 1+2 because it expected a leading equals-sign for its expressions like =1+2.

--- src/test/java/walkingkooka/spreadsheet/expression/SpreadsheetEngineSpreadsheetExpressionEvaluationContextTest.java
+++ src/test/java/walkingkooka/spreadsheet/expression/SpreadsheetEngineSpreadsheetExpressionEvaluationContextTest.java
@@ -19,36 +19,84 @@ package walkingkooka.spreadsheet.expression;
 
 import org.junit.jupiter.api.Test;
 import walkingkooka.ToStringTesting;
+import walkingkooka.collect.list.Lists;
 import walkingkooka.net.AbsoluteUrl;
 import walkingkooka.net.Url;
 import walkingkooka.reflect.ClassTesting;
 import walkingkooka.reflect.JavaVisibility;
 import walkingkooka.spreadsheet.SpreadsheetCell;
 import walkingkooka.spreadsheet.SpreadsheetFormula;
+import walkingkooka.spreadsheet.engine.FakeSpreadsheetEngineContext;
 import walkingkooka.spreadsheet.engine.SpreadsheetEngineContext;
 import walkingkooka.spreadsheet.engine.SpreadsheetEngineContexts;
+import walkingkooka.spreadsheet.format.pattern.SpreadsheetFormatPattern;
+import walkingkooka.spreadsheet.meta.SpreadsheetMetadata;
+import walkingkooka.spreadsheet.meta.SpreadsheetMetadataPropertyName;
+import walkingkooka.spreadsheet.parser.SpreadsheetParserToken;
 import walkingkooka.spreadsheet.reference.SpreadsheetSelection;
 import walkingkooka.tree.expression.ExpressionEvaluationContext;
+import walkingkooka.tree.expression.ExpressionNumberKind;
 import walkingkooka.tree.expression.ExpressionReference;
 import walkingkooka.tree.expression.FunctionExpressionName;
 import walkingkooka.tree.expression.function.ExpressionFunction;
+import walkingkooka.tree.expression.function.UnknownExpressionFunctionException;
 
+import java.math.MathContext;
+import java.util.Locale;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.function.Function;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public final class SpreadsheetEngineSpreadsheetExpressionEvaluationContextTest implements ClassTesting<SpreadsheetEngineSpreadsheetExpressionEvaluationContext>,
+        SpreadsheetExpressionEvaluationContextTesting<SpreadsheetEngineSpreadsheetExpressionEvaluationContext>,
         ToStringTesting<SpreadsheetEngineSpreadsheetExpressionEvaluationContext> {
 
     private final static AbsoluteUrl SERVER_URL = Url.parseAbsolute("https://example.com");
 
-    private final static Function<ExpressionReference, Optional<Optional<Object>>> REFERENCES = (r) -> {
+    private final static String CURRENCY = "CURR";
+
+    private final static char DECIMAL = '.';
+
+    private final static String EXPONENT = "e";
+
+    private final static ExpressionNumberKind EXPRESSION_NUMBER_KIND = ExpressionNumberKind.DOUBLE;
+
+    private final static char GROUP_SEPARATOR = ',';
+
+    private final static char MINUS = '!';
+
+    private final static char PERCENT = '#';
+
+    private final static char PLUS = '@';
+
+    private final static char VALUE_SEPARATOR = ',';
+
+    private final static SpreadsheetMetadata METADATA = SpreadsheetMetadata.NON_LOCALE_DEFAULTS
+            .set(SpreadsheetMetadataPropertyName.LOCALE, Locale.forLanguageTag("EN-AU"))
+            .loadFromLocale()
+            .set(SpreadsheetMetadataPropertyName.DATETIME_PARSE_PATTERN, SpreadsheetFormatPattern.parseDateTimeParsePattern("dd/mm/yyyy hh:mm"))
+            .set(SpreadsheetMetadataPropertyName.TEXT_FORMAT_PATTERN, SpreadsheetFormatPattern.parseTextFormatPattern("@"))
+            .set(SpreadsheetMetadataPropertyName.CURRENCY_SYMBOL, CURRENCY)
+            .set(SpreadsheetMetadataPropertyName.DECIMAL_SEPARATOR, DECIMAL)
+            .set(SpreadsheetMetadataPropertyName.EXPONENT_SYMBOL, EXPONENT)
+            .set(SpreadsheetMetadataPropertyName.GROUP_SEPARATOR, GROUP_SEPARATOR)
+            .set(SpreadsheetMetadataPropertyName.NEGATIVE_SIGN, MINUS)
+            .set(SpreadsheetMetadataPropertyName.POSITIVE_SIGN, PLUS)
+            .set(SpreadsheetMetadataPropertyName.PERCENTAGE_SYMBOL, PERCENT)
+            .set(SpreadsheetMetadataPropertyName.CELL_CHARACTER_WIDTH, 1)
+            .set(SpreadsheetMetadataPropertyName.EXPRESSION_NUMBER_KIND, EXPRESSION_NUMBER_KIND)
+            .set(SpreadsheetMetadataPropertyName.VALUE_SEPARATOR, VALUE_SEPARATOR);
+
+    private final static Function<ExpressionReference, Optional<Optional<Object>>> REFERENCES = (reference) -> {
+        Objects.requireNonNull(reference, "reference");
         throw new UnsupportedOperationException();
     };
 
-    private final static Function<FunctionExpressionName, ExpressionFunction<?, ExpressionEvaluationContext>> FUNCTIONS = (n) -> {
-        throw new UnsupportedOperationException();
+    private final static Function<FunctionExpressionName, ExpressionFunction<?, ExpressionEvaluationContext>> FUNCTIONS = (name) -> {
+        Objects.requireNonNull(name, "name");
+        throw new UnknownExpressionFunctionException(name);
     };
 
     @Test
@@ -121,6 +169,64 @@ public final class SpreadsheetEngineSpreadsheetExpressionEvaluationContextTest i
         );
     }
 
+    // parseFormula.....................................................................................................
+
+    @Test
+    public void testParseFormulaNumber() {
+        final String text = "123";
+        this.parseFormulaAndCheck(
+                text,
+                SpreadsheetParserToken.number(
+                        Lists.of(
+                                SpreadsheetParserToken.digits(text, text)
+                        ),
+                        text
+                )
+        );
+    }
+
+    @Test
+    public void testParseFormulaNumber2() {
+        final String text = "1" + DECIMAL + "5";
+        this.parseFormulaAndCheck(
+                text,
+                SpreadsheetParserToken.number(
+                        Lists.of(
+                                SpreadsheetParserToken.digits("1", "1"),
+                                SpreadsheetParserToken.decimalSeparatorSymbol("" + DECIMAL, "" + DECIMAL),
+                                SpreadsheetParserToken.digits("5", "5")
+                        ),
+                        text
+                )
+        );
+    }
+
+    @Test
+    public void testParseFormulaExpression() {
+        final String text = "1+2";
+        this.parseFormulaAndCheck(
+                text,
+                SpreadsheetParserToken.addition(
+                        Lists.of(
+                                SpreadsheetParserToken.number(
+                                        Lists.of(
+                                                SpreadsheetParserToken.digits("1", "1")
+                                        ),
+                                        "1"
+                                ),
+                                SpreadsheetParserToken.plusSymbol("+", "+"),
+                                SpreadsheetParserToken.number(
+                                        Lists.of(
+                                                SpreadsheetParserToken.digits("2", "2")
+                                        ),
+                                        "2"
+                                )
+                        ),
+                        text
+                )
+        );
+    }
+
     // toString.........................................................................................................
 
     @Test
@@ -155,6 +261,71 @@ public final class SpreadsheetEngineSpreadsheetExpressionEvaluationContextTest i
                 ),
                 cell + " " + SERVER_URL + " " + context
         );
+    }
+
+    // SpreadsheetExpressionEvaluationContext...........................................................................
+
+    @Override
+    public SpreadsheetEngineSpreadsheetExpressionEvaluationContext createContext() {
+        return SpreadsheetEngineSpreadsheetExpressionEvaluationContext.with(
+                Optional.empty(), // cell
+                SERVER_URL,
+                REFERENCES, // references
+                FUNCTIONS, // functions
+                new FakeSpreadsheetEngineContext() {
+
+                    @Override
+                    public boolean isPure(final FunctionExpressionName name) {
+                        Objects.requireNonNull(name, "name");
+                        throw new UnknownExpressionFunctionException(name);
+                    }
+
+                    @Override
+                    public SpreadsheetMetadata spreadsheetMetadata() {
+                        return METADATA;
+                    }
+                }
+        );
+    }
+
+    @Override
+    public String currencySymbol() {
+        return CURRENCY;
+    }
+
+    @Override
+    public char decimalSeparator() {
+        return DECIMAL;
+    }
+
+    @Override
+    public String exponentSymbol() {
+        return EXPONENT;
+    }
+
+    @Override
+    public char groupSeparator() {
+        return GROUP_SEPARATOR;
+    }
+
+    @Override
+    public MathContext mathContext() {
+        return METADATA.mathContext();
+    }
+
+    @Override
+    public char negativeSign() {
+        return MINUS;
+    }
+
+    @Override
+    public char percentageSymbol() {
+        return PERCENT;
+    }
+
+    @Override
+    public char positiveSign() {
+        return PLUS;
     }
 
     // ClassTesting......................................................................................................
