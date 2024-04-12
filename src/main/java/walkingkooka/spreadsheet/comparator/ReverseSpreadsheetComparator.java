b419904770807a8f5474f9961c64dbc3d6a57751
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2024-04-12 19:37:26 +1000

    ReverseSpreadsheetComparator

---
+++ src/main/java/walkingkooka/spreadsheet/comparator/ReverseSpreadsheetComparator.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2019 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.comparator;
+
+import walkingkooka.Cast;
+import walkingkooka.compare.Comparators;
+
+import java.util.Objects;
+
+/**
+ * Wraps another {@link SpreadsheetComparator} and reverses the comparator result.
+ */
+final class ReverseSpreadsheetComparator<T> implements SpreadsheetComparator<T> {
+
+    static <T> SpreadsheetComparator<T> with(final SpreadsheetComparator<T> comparator) {
+        return comparator instanceof ReverseSpreadsheetComparator ?
+                ((ReverseSpreadsheetComparator) comparator).comparator :
+                new ReverseSpreadsheetComparator<>(
+                        Objects.requireNonNull(comparator, "comparator")
+                );
+    }
+
+    private ReverseSpreadsheetComparator(final SpreadsheetComparator<T> comparator) {
+        this.comparator = comparator;
+    }
+
+    @Override
+    public Class<T> type() {
+        return this.comparator.type();
+    }
+
+    @Override
+    public int compare(final T left,
+                       final T right) {
+        return -
+                Comparators.normalize(
+                        this.comparator.compare(
+                                left,
+                                right
+                        )
+                );
+    }
+
+
+    private final SpreadsheetComparator<T> comparator;
+
+    // Object...........................................................................................................
+
+    @Override
+    public int hashCode() {
+        return this.comparator.hashCode();
+    }
+
+    @Override
+    public boolean equals(final Object other) {
+        return this == other ||
+                other instanceof ReverseSpreadsheetComparator && this.equals0(Cast.to(other));
+    }
+
+    private boolean equals0(final ReverseSpreadsheetComparator<?> other) {
+        return this.comparator.equals(other.comparator);
+    }
+
+    @Override
+    public String toString() {
+        return this.comparator + " Reverse";
+    }
+}
---
+++ src/test/java/walkingkooka/spreadsheet/comparator/ReverseSpreadsheetComparatorTest.java
@@ -0,0 +1,139 @@
+
+/*
+ * Copyright 2019 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.comparator;
+
+import org.junit.jupiter.api.Test;
+import walkingkooka.Cast;
+import walkingkooka.HashCodeEqualsDefinedTesting2;
+import walkingkooka.ToStringTesting;
+
+import java.time.LocalTime;
+
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public final class ReverseSpreadsheetComparatorTest implements SpreadsheetComparatorTesting<ReverseSpreadsheetComparator<String>, String>,
+        HashCodeEqualsDefinedTesting2<ReverseSpreadsheetComparator<String>>,
+        ToStringTesting<ReverseSpreadsheetComparator<String>> {
+
+    @Test
+    public void testWithNullComparatorFails() {
+        assertThrows(
+                NullPointerException.class,
+                () -> ReverseSpreadsheetComparator.with(
+                        null
+                )
+        );
+    }
+
+    @Test
+    public void testWith() {
+        ReverseSpreadsheetComparator.with(
+                SpreadsheetComparators.string()
+
+        );
+    }
+
+    @Test
+    public void testWithUnwrapsReverseSpreadsheetConverter() {
+        final SpreadsheetComparator<String> comparator = SpreadsheetComparators.string();
+        assertSame(
+                comparator,
+                ReverseSpreadsheetComparator.with(
+                        ReverseSpreadsheetComparator.with(
+                                comparator
+                        )
+                )
+        );
+    }
+
+    @Test
+    public void testType() {
+        final SpreadsheetComparator<LocalTime> comparator = SpreadsheetComparators.hourOfDay();
+
+        this.typeAndCheck(
+                SpreadsheetComparators.reverse(
+                        comparator
+                ),
+                LocalTime.class
+        );
+    }
+
+    @Test
+    public void testCompareMore() {
+        this.compareAndCheckMore(
+                "apple",
+                "Banana"
+        );
+    }
+
+    @Test
+    public void testCompareEqual() {
+        this.compareAndCheckEquals(
+                "apple",
+                "APPLE"
+        );
+    }
+
+    @Test
+    public void testEqualsDifferentComparator() {
+        this.checkNotEquals(
+                ReverseSpreadsheetComparator.with(
+                        SpreadsheetComparators.hourOfDay()
+                ),
+                ReverseSpreadsheetComparator.with(
+                        SpreadsheetComparators.string()
+                )
+        );
+    }
+
+    @Override
+    public ReverseSpreadsheetComparator<String> createObject() {
+        return this.createComparator();
+    }
+
+    @Test
+    public void tesToString() {
+        final SpreadsheetComparator<String> comparator = SpreadsheetComparators.stringCaseInsensitive();
+
+        this.toStringAndCheck(
+                ReverseSpreadsheetComparator.with(
+                        comparator
+                ),
+                comparator + " Reverse"
+        );
+    }
+
+    // Comparator.......................................................................................................
+    @Override
+    public ReverseSpreadsheetComparator<String> createComparator() {
+        return Cast.to(
+                ReverseSpreadsheetComparator.with(
+                        SpreadsheetComparators.stringCaseInsensitive()
+                )
+        );
+    }
+
+    @Override
+    public Class<ReverseSpreadsheetComparator<String>> type() {
+        return Cast.to(
+                ReverseSpreadsheetComparator.class
+        );
+    }
+}
