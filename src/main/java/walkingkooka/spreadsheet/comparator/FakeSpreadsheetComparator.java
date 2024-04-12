eccffead4ecd365f7d2f7b709f214a2c3202f78d
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2024-04-12 19:44:58 +1000

    FakeSpreadsheetComparator

---
+++ src/main/java/walkingkooka/spreadsheet/comparator/FakeSpreadsheetComparator.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2019 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.comparator;
+
+class FakeSpreadsheetComparator<T> implements SpreadsheetComparator<T> {
+
+    FakeSpreadsheetComparator() {
+        super();
+    }
+
+    @Override
+    public Class<T> type() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int compare(final T left,
+                       final T right) {
+        throw new UnsupportedOperationException();
+    }
+}
