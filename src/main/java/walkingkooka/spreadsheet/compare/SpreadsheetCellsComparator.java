7caa1e7747a909c832a607549e43c259d1cbf139
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2024-04-15 18:28:17 +1000

    SpreadsheetCellsComparator

---
+++ src/main/java/walkingkooka/spreadsheet/compare/SpreadsheetCellsComparator.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright 2019 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.compare;
+
+import walkingkooka.ToStringBuilder;
+import walkingkooka.collect.list.Lists;
+import walkingkooka.compare.Comparators;
+import walkingkooka.spreadsheet.SpreadsheetCell;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * A {@link Comparator} that may be used to compare all sorted columns or rows within a large range being sorted.
+ * After the sorted columns or rows are sorted, the remaining columns and rows will need to be moved around to match the
+ * sorted columns/rows and then have their references fixed.If a cell is missing the list should contain a NULL<br>
+ * Note the given lists for left and right must match the number of {@link SpreadsheetCellSpreadsheetComparators}.
+ */
+public final class SpreadsheetCellsComparator implements Comparator<List<SpreadsheetCell>> {
+
+    static SpreadsheetCellsComparator with(final List<SpreadsheetCellSpreadsheetComparators> comparators,
+                                           final SpreadsheetComparatorContext context) {
+        final List<SpreadsheetCellSpreadsheetComparators> copy = Lists.immutable(
+                Objects.requireNonNull(comparators, "comparators")
+        );
+        if (copy.isEmpty()) {
+            throw new IllegalArgumentException("Comparators must not be empty");
+        }
+
+        return new SpreadsheetCellsComparator(
+                copy,
+                Objects.requireNonNull(context, "context")
+        );
+    }
+
+    private SpreadsheetCellsComparator(final List<SpreadsheetCellSpreadsheetComparators> comparators,
+                                       final SpreadsheetComparatorContext context) {
+        this.comparators = comparators;
+        this.context = context;
+    }
+
+    // Comparator.......................................................................................................
+
+    @Override
+    public int compare(final List<SpreadsheetCell> left,
+                       final List<SpreadsheetCell> right) {
+        Objects.requireNonNull(left, "left");
+        Objects.requireNonNull(right, "right");
+
+        final List<SpreadsheetCellSpreadsheetComparators> comparators = this.comparators;
+        final SpreadsheetComparatorContext context = this.context;
+
+        // assumes size of left = right = comparators
+
+        int result = Comparators.EQUAL;
+
+        int i = 0;
+        for (final SpreadsheetCell cellLeft : left) {
+            result = comparators.get(i)
+                    .compare(
+                            cellLeft,
+                            right.get(i),
+                            context
+                    );
+            if (Comparators.EQUAL != result) {
+                break;
+            }
+            i++;
+        }
+
+        return result;
+    }
+
+    private final List<SpreadsheetCellSpreadsheetComparators> comparators;
+
+    // TODO should cache converted values key=value+target-type value=converted-value
+    private final SpreadsheetComparatorContext context;
+
+    // Object...........................................................................................................
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.empty()
+                .value(this.comparators)
+                .value(this.context)
+                .build();
+    }
+}
---
+++ src/test/java/walkingkooka/spreadsheet/compare/SpreadsheetCellsComparatorTest.java
@@ -0,0 +1,391 @@
+/*
+ * Copyright 2019 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.compare;
+
+import org.junit.jupiter.api.Test;
+import walkingkooka.Either;
+import walkingkooka.collect.list.Lists;
+import walkingkooka.compare.ComparatorTesting2;
+import walkingkooka.convert.Converter;
+import walkingkooka.convert.ConverterContexts;
+import walkingkooka.convert.Converters;
+import walkingkooka.convert.FakeConverter;
+import walkingkooka.datetime.DateTimeContexts;
+import walkingkooka.math.DecimalNumberContexts;
+import walkingkooka.spreadsheet.SpreadsheetCell;
+import walkingkooka.spreadsheet.SpreadsheetFormula;
+import walkingkooka.spreadsheet.convert.SpreadsheetConverterContext;
+import walkingkooka.spreadsheet.convert.SpreadsheetConverterContexts;
+import walkingkooka.spreadsheet.reference.SpreadsheetSelection;
+import walkingkooka.text.printer.TreePrintableTesting;
+import walkingkooka.tree.expression.ExpressionNumberConverterContexts;
+import walkingkooka.tree.expression.ExpressionNumberKind;
+
+import java.math.MathContext;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Locale;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public final class SpreadsheetCellsComparatorTest implements ComparatorTesting2<SpreadsheetCellsComparator, List<SpreadsheetCell>>,
+        TreePrintableTesting {
+
+    @Test
+    public void testWithNullComparatorsFails() {
+        assertThrows(
+                NullPointerException.class,
+                () -> SpreadsheetCellsComparator.with(
+                        null, // spreadsheetComparators
+                        SpreadsheetComparatorContexts.fake()
+                )
+        );
+    }
+
+    @Test
+    public void testWithEmptyComparatorsFails() {
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> SpreadsheetCellsComparator.with(
+                        Lists.empty(), // spreadsheetComparators
+                        SpreadsheetComparatorContexts.fake()
+                )
+        );
+    }
+
+    @Test
+    public void testWithNullContextFails() {
+        assertThrows(
+                NullPointerException.class,
+                () -> SpreadsheetCellsComparator.with(
+                        SpreadsheetCellSpreadsheetComparators.parse(
+                                "A=string",
+                                SpreadsheetComparators.nameToSpreadsheetComparator()
+                        ), // spreadsheetComparators
+                        null // context
+                )
+        );
+    }
+
+    // Comparator.......................................................................................................
+
+    @Test
+    public void testCompareSingleColumn() {
+        final SpreadsheetCell a1 = this.cell(
+                "A1",
+                LocalDate.of(1999, 12, 1)
+        );
+        final SpreadsheetCell a2 = this.cell(
+                "A2",
+                LocalDate.of(1999, 12, 2)
+        );
+        final SpreadsheetCell a3 = this.cell(
+                "A3",
+                LocalDate.of(1999, 12, 3)
+        );
+        this.comparatorArraySortAndCheck(
+                list(a1),
+                list(a2),
+                list(a3),
+                list(a1), // expected
+                list(a2),
+                list(a3)
+        );
+    }
+
+    @Test
+    public void testCompareSingleColumnSomeEqual() {
+        final SpreadsheetCell a1 = this.cell(
+                "A1",
+                LocalDate.of(1999, 12, 1)
+        );
+        final SpreadsheetCell a2 = this.cell(
+                "A2",
+                LocalDate.of(1999, 12, 2)
+        );
+        final SpreadsheetCell a3 = this.cell(
+                "A3",
+                LocalDate.of(1999, 12, 1)
+        );
+        this.comparatorArraySortAndCheck(
+                list(a1),
+                list(a2),
+                list(a3),
+                list(a1), // expected
+                list(a3),
+                list(a2)
+        );
+    }
+
+    @Test
+    public void testCompareSingleColumnSomeMissing() {
+        final SpreadsheetCell a1 = this.cell(
+                "A1",
+                LocalDate.of(1999, 12, 1)
+        );
+        final SpreadsheetCell a2 = this.cell(
+                "A2",
+                LocalDate.of(1999, 12, 2)
+        );
+        final SpreadsheetCell a3 = this.cell(
+                "A3",
+                LocalDate.of(1999, 12, 1)
+        );
+        final SpreadsheetCell a4 = null;
+
+        this.comparatorArraySortAndCheck(
+                list(a1),
+                list(a2),
+                list(a3),
+                list(),
+                list(a1), // expected
+                list(a3),
+                list(a2),
+                list()
+        );
+    }
+
+    @Test
+    public void testCompareMultipleColumn() {
+        final SpreadsheetCell a1 = this.cell(
+                "A1",
+                LocalDate.of(1999, 12, 1)
+        );
+        final SpreadsheetCell a2 = this.cell(
+                "A2",
+                LocalDate.of(1999, 12, 1)
+        );
+
+        final SpreadsheetCell b1 = this.cell(
+                "b1",
+                "Second"
+        );
+        final SpreadsheetCell b2 = this.cell(
+                "b2",
+                "First"
+        );
+
+        this.comparatorArraySortAndCheck(
+                list(a1, b1),
+                list(a2, b2),
+                list(a2, b2), // expected
+                list(a1, b1)
+        );
+    }
+
+    @Test
+    public void testCompareMultipleColumnSomeMissing() {
+        final SpreadsheetCell a1 = this.cell(
+                "A1",
+                LocalDate.of(1999, 12, 1)
+        );
+        final SpreadsheetCell a2 = this.cell(
+                "A2",
+                LocalDate.of(1999, 12, 1)
+        );
+
+        final SpreadsheetCell b1 = this.cell(
+                "b1",
+                "First"
+        );
+        final SpreadsheetCell b2 = null;
+
+        this.comparatorArraySortAndCheck(
+                list(a1, b1),
+                list(a2, b2),
+                list(a1, b1), // expected
+                list(a2, b2) // null is after
+        );
+    }
+
+    @Test
+    public void testCompareMultipleColumn2() {
+        final SpreadsheetCell a1 = this.cell(
+                "A1",
+                LocalDate.of(1999, 12, 1)
+        );
+        final SpreadsheetCell a2 = this.cell(
+                "A2",
+                LocalDate.of(1999, 12, 1)
+        );
+        final SpreadsheetCell a3 = this.cell(
+                "A3",
+                LocalDate.of(1999, 12, 3)
+        );
+        final SpreadsheetCell a4 = this.cell(
+                "A4",
+                LocalDate.of(1999, 12, 3)
+        );
+        final SpreadsheetCell a5 = this.cell(
+                "A5",
+                LocalDate.of(1999, 12, 5)
+        );
+
+        final SpreadsheetCell b1 = this.cell(
+                "b1",
+                "Second"
+        );
+        final SpreadsheetCell b2 = this.cell(
+                "b2",
+                "First"
+        );
+        final SpreadsheetCell b3 = this.cell(
+                "b3",
+                "Third"
+        );
+        final SpreadsheetCell b4 = this.cell(
+                "b4",
+                "TT Fourth"
+        );
+        final SpreadsheetCell b5 = this.cell(
+                "b5",
+                "Fifth"
+        );
+
+        this.comparatorArraySortAndCheck(
+                list(a1, b1),
+                list(a2, b2),
+                list(a3, b3),
+                list(a4, b4),
+                list(a5, b5),
+                list(a2, b2), // expected
+                list(a1, b1),
+                list(a3, b3),
+                list(a4, b4),
+                list(a5, b5)
+        );
+    }
+
+    private SpreadsheetCell cell(final String reference,
+                                 final Object value) {
+        return SpreadsheetSelection.parseCell(reference)
+                .setFormula(
+                        SpreadsheetFormula.EMPTY.setValue(
+                                Optional.ofNullable(value)
+                        )
+                );
+    }
+
+    private static List<SpreadsheetCell> list(final SpreadsheetCell... cells) {
+        return Lists.of(cells);
+    }
+
+    // Object...........................................................................................................
+
+    @Test
+    public void testToString() {
+        final SpreadsheetComparatorContext context = SpreadsheetComparatorContexts.fake();
+
+        this.toStringAndCheck(
+                SpreadsheetCellsComparator.with(
+                        SpreadsheetCellSpreadsheetComparators.parse(
+                                "B=day-of-month",
+                                SpreadsheetComparators.nameToSpreadsheetComparator()
+                        ),
+                        context
+                ),
+                "B DayOfMonth " + context
+        );
+    }
+
+    @Override
+    public SpreadsheetCellsComparator createComparator() {
+        return this.createComparator(
+                "B=day-of-month;C=string-case-insensitive;D=string-case-insensitive",
+                new FakeConverter<>() {
+
+                    @Override
+                    public <T> Either<T, String> convert(final Object value,
+                                                         final Class<T> target,
+                                                         final SpreadsheetConverterContext context) {
+                        if (value instanceof String && LocalDate.class == target) {
+                            try {
+                                return this.successfulConversion(
+                                        LocalDate.parse((String) value),
+                                        target
+                                );
+                            } catch (final Exception ignore) {
+                                // eventually becomes a failConversion
+                            }
+                        }
+                        if (value instanceof LocalDate && LocalDate.class == target) {
+                            return this.successfulConversion(
+                                    value,
+                                    target
+                            );
+                        }
+                        if (value instanceof String && String.class == target) {
+                            return this.successfulConversion(
+                                    value,
+                                    target
+                            );
+                        }
+                        return this.failConversion(
+                                value,
+                                target
+                        );
+                    }
+
+                    @Override
+                    public String toString() {
+                        return "TestConverter";
+                    }
+                }
+        );
+    }
+
+    private SpreadsheetCellsComparator createComparator(final String comparators,
+                                                        final Converter<SpreadsheetConverterContext> converter) {
+        return SpreadsheetCellsComparator.with(
+                SpreadsheetCellSpreadsheetComparators.parse(
+                        comparators,
+                        SpreadsheetComparators.nameToSpreadsheetComparator()
+                ),
+                SpreadsheetComparatorContexts.basic(
+                        SpreadsheetComparatorMissingValues.AFTER,
+                        SpreadsheetConverterContexts.basic(
+                                converter,
+                                (label) -> {
+                                    throw new UnsupportedOperationException();
+                                },
+                                ExpressionNumberConverterContexts.basic(
+                                        Converters.fake(),
+                                        ConverterContexts.basic(
+                                                Converters.fake(),
+                                                DateTimeContexts.locale(
+                                                        Locale.forLanguageTag("EN-AU"),
+                                                        1950, // defaultYear
+                                                        50, // twoDigitYears
+                                                        LocalDateTime::now
+                                                ),
+                                                DecimalNumberContexts.american(MathContext.DECIMAL32)
+                                        ),
+                                        ExpressionNumberKind.BIG_DECIMAL
+                                )
+                        )
+                )
+        );
+    }
+
+    @Override
+    public Class<SpreadsheetCellsComparator> type() {
+        return SpreadsheetCellsComparator.class;
+    }
+}
