3377ef826c33f77c41201369a2c46253494e7c97
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2024-04-17 19:43:28 +1000

    SpreadsheetSelectionReplaceReferencesMapperFunction skip absolute cell components FIX
    - Closes https://github.com/mP1/walkingkooka-spreadsheet/issues/3813
    - SpreadsheetSelectionReplaceReferencesMapperFunction should skip absolute references.

--- src/test/java/walkingkooka/spreadsheet/reference/SpreadsheetSelectionReplaceReferencesMapperFunctionTest.java
+++ src/test/java/walkingkooka/spreadsheet/reference/SpreadsheetSelectionReplaceReferencesMapperFunctionTest.java
@@ -17,10 +17,106 @@
 
 package walkingkooka.spreadsheet.reference;
 
+import org.junit.jupiter.api.Test;
 import walkingkooka.reflect.ClassTesting;
 import walkingkooka.reflect.JavaVisibility;
+import walkingkooka.util.FunctionTesting;
 
-public final class SpreadsheetSelectionReplaceReferencesMapperFunctionTest implements ClassTesting<SpreadsheetSelectionReplaceReferencesMapperFunction> {
+import java.util.Optional;
+
+public final class SpreadsheetSelectionReplaceReferencesMapperFunctionTest implements ClassTesting<SpreadsheetSelectionReplaceReferencesMapperFunction>,
+        FunctionTesting<SpreadsheetSelectionReplaceReferencesMapperFunction, SpreadsheetCellReference, Optional<SpreadsheetCellReference>> {
+
+    @Test
+    public void testRelativeCell() {
+        this.applyAndCheck(
+                "A1",
+                "B2"
+        );
+    }
+
+    @Test
+    public void testRelativeColumnAbsoluteRow() {
+        this.applyAndCheck(
+                "A$1",
+                "B$1"
+        );
+    }
+
+    @Test
+    public void testAbsoluteColumnRelativeRow() {
+        this.applyAndCheck(
+                "$A1",
+                "$A2"
+        );
+    }
+
+    @Test
+    public void testAbsoluteColumnAbsoluteRow() {
+        this.applyAndCheck(
+                "$A$1",
+                "$A$1"
+        );
+    }
+
+    @Test
+    public void testUnderflowColumnAndRow() {
+        this.applyAndCheck(
+                SpreadsheetSelectionReplaceReferencesMapperFunction.with(
+                        -1,
+                        -1
+                ),
+                SpreadsheetSelection.A1,
+                Optional.empty()
+        );
+    }
+
+    @Test
+    public void testUnderflowColumn() {
+        this.applyAndCheck(
+                SpreadsheetSelectionReplaceReferencesMapperFunction.with(
+                        -1,
+                        0
+                ),
+                SpreadsheetSelection.A1,
+                Optional.empty()
+        );
+    }
+
+    @Test
+    public void testUnderflowRow() {
+        this.applyAndCheck(
+                SpreadsheetSelectionReplaceReferencesMapperFunction.with(
+                        0,
+                        -1
+                ),
+                SpreadsheetSelection.A1,
+                Optional.empty()
+        );
+    }
+
+    private void applyAndCheck(final String cellBefore,
+                               final String cellAfter) {
+        this.applyAndCheck(
+                this.createFunction(),
+                SpreadsheetSelection.parseCell(cellBefore),
+                Optional.of(
+                        SpreadsheetSelection.parseCell(cellAfter)
+                )
+        );
+    }
+
+    // FunctionTesting..................................................................................................
+
+    @Override
+    public SpreadsheetSelectionReplaceReferencesMapperFunction createFunction() {
+        return SpreadsheetSelectionReplaceReferencesMapperFunction.with(
+                1,
+                1
+        );
+    }
+
+    // ClassTesting.....................................................................................................
 
     @Override
     public Class<SpreadsheetSelectionReplaceReferencesMapperFunction> type() {
