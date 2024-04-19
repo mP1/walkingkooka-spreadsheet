635545cb975c871aed0b0c8809ef0dd154467498
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2024-04-19 21:03:40 +1000

    SpreadsheetComparatorNameMapper.toSpreadsheetCellSpreadsheetComparators

--- src/main/java/walkingkooka/spreadsheet/compare/SpreadsheetComparatorNameMapper.java
+++ src/main/java/walkingkooka/spreadsheet/compare/SpreadsheetComparatorNameMapper.java
@@ -17,6 +17,10 @@
 
 package walkingkooka.spreadsheet.compare;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * A mapper function that can turn a {@link SpreadsheetComparatorName} into a {@link SpreadsheetComparator}.
  */
@@ -26,4 +30,19 @@ public interface SpreadsheetComparatorNameMapper {
      * Resolves the given {@link SpreadsheetComparatorName} to a {@link SpreadsheetComparatorName}.
      */
     SpreadsheetComparator<?> spreadsheetComparator(final SpreadsheetComparatorName name);
+
+    /**
+     * Helper that maps a {@link List} of {@link SpreadsheetCellSpreadsheetComparatorNames} into a {@link List} of
+     * {@link SpreadsheetCellSpreadsheetComparators} including the name to comparator lookups.
+     */
+    default List<SpreadsheetCellSpreadsheetComparators> toSpreadsheetCellSpreadsheetComparators(final Collection<SpreadsheetCellSpreadsheetComparatorNames> names) {
+        return names.stream()
+                .map(n -> SpreadsheetCellSpreadsheetComparators.with(
+                        n.columnOrRow(),
+                        n.comparatorNameAndDirections()
+                                .stream()
+                                .map(nad -> nad.direction().apply(this.spreadsheetComparator(nad.name())))
+                                .collect(Collectors.toList())
+                )).collect(Collectors.toList());
+    }
 }
