14929ae45719a1bcdc45dd2759d4bf12088122f2
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2024-05-03 13:17:23 +1000

    SpreadsheetViewportNavigationSelectionCell
    - Closes https://github.com/mP1/walkingkooka-spreadsheet/issues/3962
    - SpreadsheetViewportNavigation.cell
    
    - Closes https://github.com/mP1/walkingkooka-spreadsheet/issues/3966
    - SpreadsheetViewportSelectionNavigation.cell(SpreadsheetCellReference)

---
+++ src/main/java/walkingkooka/spreadsheet/reference/SpreadsheetViewportNavigationSelectionCell.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2019 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.reference;
+
+/**
+ * Represents a cell being CLICKED in the viewport.
+ * <br>
+ * <pre>
+ * select cell A1
+ * </pre>
+ */
+final class SpreadsheetViewportNavigationSelectionCell extends SpreadsheetViewportNavigationSelection<SpreadsheetCellReference> {
+
+    static SpreadsheetViewportNavigationSelectionCell with(final SpreadsheetCellReference selection) {
+        return new SpreadsheetViewportNavigationSelectionCell(selection);
+    }
+
+    public SpreadsheetViewportNavigationSelectionCell(final SpreadsheetCellReference selection) {
+        super(selection);
+    }
+}
