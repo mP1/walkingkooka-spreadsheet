d7508f1ee6932b703e670b46c60eeffdddc35e60
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2024-05-30 21:05:07 +1000

    SpreadsheetFormatterName

---
+++ src/main/java/walkingkooka/spreadsheet/format/SpreadsheetFormatterName.java
@@ -0,0 +1,126 @@
+/*
+ * Copyright 2019 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.format;
+
+import walkingkooka.Cast;
+import walkingkooka.naming.Name;
+import walkingkooka.spreadsheet.SpreadsheetComponentName;
+import walkingkooka.text.CaseSensitivity;
+import walkingkooka.tree.json.JsonNode;
+import walkingkooka.tree.json.marshall.JsonNodeContext;
+import walkingkooka.tree.json.marshall.JsonNodeMarshallContext;
+import walkingkooka.tree.json.marshall.JsonNodeUnmarshallContext;
+
+/**
+ * The {@link Name} of a {@link SpreadsheetFormatter}. Note formatter names are case-sensitive.
+ */
+final public class SpreadsheetFormatterName implements Name, Comparable<SpreadsheetFormatterName> {
+
+    public static boolean isChar(final int pos,
+                                 final char c) {
+        return SpreadsheetComponentName.isChar(pos, c);
+    }
+
+    /**
+     * The maximum valid length
+     */
+    public final static int MAX_LENGTH = SpreadsheetComponentName.MAX_LENGTH;
+
+    /**
+     * Factory that creates a {@link SpreadsheetFormatterName}
+     */
+    public static SpreadsheetFormatterName with(final String name) {
+        return new SpreadsheetFormatterName(
+                SpreadsheetComponentName.with(name)
+        );
+    }
+
+    /**
+     * Private constructor
+     */
+    private SpreadsheetFormatterName(final SpreadsheetComponentName name) {
+        super();
+        this.name = name;
+    }
+
+    @Override
+    public String value() {
+        return this.name.value();
+    }
+
+    private final SpreadsheetComponentName name;
+
+    // Object...........................................................................................................
+
+    @Override
+    public int hashCode() {
+        return this.name.hashCode();
+    }
+
+    @Override
+    public boolean equals(final Object other) {
+        return this == other ||
+                other instanceof SpreadsheetFormatterName &&
+                        this.equals0(Cast.to(other));
+    }
+
+    private boolean equals0(final SpreadsheetFormatterName other) {
+        return this.compareTo(other) == 0;
+    }
+
+    @Override
+    public String toString() {
+        return this.name.toString();
+    }
+
+    // Comparable ...................................................................................................
+
+    @Override
+    public int compareTo(final SpreadsheetFormatterName other) {
+        return this.name.compareTo(other.name);
+    }
+
+    // HasCaseSensitivity................................................................................................
+
+    @Override
+    public CaseSensitivity caseSensitivity() {
+        return CASE_SENSITIVITY;
+    }
+
+    private final static CaseSensitivity CASE_SENSITIVITY = CaseSensitivity.SENSITIVE;
+
+    // Json.............................................................................................................
+
+    static SpreadsheetFormatterName unmarshall(final JsonNode node,
+                                               final JsonNodeUnmarshallContext context) {
+        return with(node.stringOrFail());
+    }
+
+    private JsonNode marshall(final JsonNodeMarshallContext context) {
+        return JsonNode.string(this.toString());
+    }
+
+    static {
+        JsonNodeContext.register(
+                JsonNodeContext.computeTypeName(SpreadsheetFormatterName.class),
+                SpreadsheetFormatterName::unmarshall,
+                SpreadsheetFormatterName::marshall,
+                SpreadsheetFormatterName.class
+        );
+    }
+}
