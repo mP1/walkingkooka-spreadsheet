b05459ef770969ce9e98f1dd1c9b26a1cca03288
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2024-05-31 18:08:12 +1000

    SpreadsheetFormatterInfo

---
+++ src/test/java/walkingkooka/spreadsheet/format/SpreadsheetFormatterInfoTest.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2019 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.format;
+
+import walkingkooka.net.AbsoluteUrl;
+import walkingkooka.spreadsheet.component.SpreadsheetComponentInfoLikeTesting;
+import walkingkooka.tree.json.JsonNode;
+import walkingkooka.tree.json.marshall.JsonNodeUnmarshallContext;
+
+public final class SpreadsheetFormatterInfoTest implements SpreadsheetComponentInfoLikeTesting<SpreadsheetFormatterInfo, SpreadsheetFormatterName> {
+
+    // ClassTesting.....................................................................................................
+
+    @Override
+    public Class<SpreadsheetFormatterInfo> type() {
+        return SpreadsheetFormatterInfo.class;
+    }
+
+    // SpreadsheetComponentInfoLikeTesting..............................................................................
+
+    @Override
+    public SpreadsheetFormatterName createName(final String value) {
+        return SpreadsheetFormatterName.with(value);
+    }
+
+    @Override
+    public SpreadsheetFormatterInfo createSpreadsheetComponentInfo(final AbsoluteUrl url,
+                                                                   final SpreadsheetFormatterName name) {
+        return SpreadsheetFormatterInfo.with(
+                url,
+                name
+        );
+    }
+
+    // json.............................................................................................................
+
+    @Override
+    public SpreadsheetFormatterInfo unmarshall(final JsonNode json,
+                                               final JsonNodeUnmarshallContext context) {
+        return SpreadsheetFormatterInfo.unmarshall(
+                json,
+                context
+        );
+    }
+}
