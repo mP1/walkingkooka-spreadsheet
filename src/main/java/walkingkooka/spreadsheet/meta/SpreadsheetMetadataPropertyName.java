9b4ccaf8390b1fcda3fc5d8335aaeb1bdf06307f
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2024-06-05 21:30:40 +1000

    SpreadsheetMetadata force SpreadsheetFormatterSelector json register

--- src/main/java/walkingkooka/spreadsheet/meta/SpreadsheetMetadataPropertyName.java
+++ src/main/java/walkingkooka/spreadsheet/meta/SpreadsheetMetadataPropertyName.java
@@ -27,6 +27,7 @@ import walkingkooka.net.email.EmailAddress;
 import walkingkooka.spreadsheet.SpreadsheetId;
 import walkingkooka.spreadsheet.SpreadsheetName;
 import walkingkooka.spreadsheet.format.SpreadsheetColorName;
+import walkingkooka.spreadsheet.format.SpreadsheetFormatterName;
 import walkingkooka.spreadsheet.format.SpreadsheetFormatterSelector;
 import walkingkooka.spreadsheet.format.pattern.HasSpreadsheetPatternKind;
 import walkingkooka.spreadsheet.format.pattern.SpreadsheetDateParsePattern;
@@ -639,6 +640,10 @@ public abstract class SpreadsheetMetadataPropertyName<T> implements Name,
         ExpressionNumberKind.DEFAULT.name();
         FontFamily.with("MS Sans Serif");
         FontSize.with(1);
+        SpreadsheetFormatterSelector.with(
+                SpreadsheetFormatterName.DATE_FORMAT,
+                "dd/mm/yyyy"
+        );
         SpreadsheetPattern.parseNumberFormatPattern(" ");
         //noinspection ResultOfMethodCallIgnored
         SpreadsheetId.with(0);
