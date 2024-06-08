a84dad48061f93370520c349c1212a7fe0e34dfc
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2024-06-08 13:54:21 +1000

    https://github.com/mP1/walkingkooka-plugin/pull/53 PluginInfoLike.toString
    - https://github.com/mP1/walkingkooka-plugin/pull/53
    - PluginInfoLike.toString

--- src/main/java/walkingkooka/spreadsheet/format/SpreadsheetFormatterInfo.java
+++ src/main/java/walkingkooka/spreadsheet/format/SpreadsheetFormatterInfo.java
@@ -96,7 +96,7 @@ public final class SpreadsheetFormatterInfo implements PluginInfoLike<Spreadshee
 
     @Override
     public String toString() {
-        return this.url + " " + this.name;
+        return PluginInfoLike.toString(this);
     }
 
     // Json.............................................................................................................
