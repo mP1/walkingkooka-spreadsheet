1589cfb6b667f05a68d4e1f079f4d33177ed7448
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2024-06-15 10:41:22 +1000

    SpreadsheetParserProviderTesting

---
+++ src/main/java/walkingkooka/spreadsheet/format/SpreadsheetParserProviderTesting.java
@@ -0,0 +1,168 @@
+/*
+ * Copyright 2019 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.format;
+
+import org.junit.jupiter.api.Test;
+import walkingkooka.collect.set.Sets;
+import walkingkooka.reflect.ClassTesting2;
+import walkingkooka.spreadsheet.parser.SpreadsheetParserContext;
+import walkingkooka.text.cursor.parser.Parser;
+import walkingkooka.text.printer.TreePrintableTesting;
+
+import java.util.Optional;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public interface SpreadsheetParserProviderTesting<T extends SpreadsheetParserProvider> extends ClassTesting2<T>,
+        TreePrintableTesting {
+
+    @Test
+    default void testSpreadsheetParserWithNullFails() {
+        assertThrows(
+                NullPointerException.class,
+                () -> this.createSpreadsheetParserProvider().spreadsheetParser(null)
+        );
+    }
+
+    T createSpreadsheetParserProvider();
+
+    default void spreadsheetParserAndCheck(final String selector) {
+        this.spreadsheetParserAndCheck(
+                this.createSpreadsheetParserProvider(),
+                SpreadsheetParserSelector.parse(selector)
+        );
+    }
+
+    default void spreadsheetParserAndCheck(final SpreadsheetParserProvider provider,
+                                           final String selector) {
+        this.spreadsheetParserAndCheck(
+                provider,
+                SpreadsheetParserSelector.parse(selector)
+        );
+    }
+
+    default void spreadsheetParserAndCheck(final SpreadsheetParserSelector selector) {
+        this.spreadsheetParserAndCheck(
+                this.createSpreadsheetParserProvider(),
+                selector,
+                Optional.empty()
+        );
+    }
+
+    default void spreadsheetParserAndCheck(final SpreadsheetParserProvider provider,
+                                           final SpreadsheetParserSelector selector) {
+        this.spreadsheetParserAndCheck(
+                provider,
+                selector,
+                Optional.empty()
+        );
+    }
+
+    default void spreadsheetParserAndCheck(final String selector,
+                                           final Parser<SpreadsheetParserContext> expected) {
+        this.spreadsheetParserAndCheck(
+                this.createSpreadsheetParserProvider(),
+                SpreadsheetParserSelector.parse(selector),
+                expected
+        );
+    }
+
+    default void spreadsheetParserAndCheck(final SpreadsheetParserProvider provider,
+                                           final String selector,
+                                           final Parser<SpreadsheetParserContext> expected) {
+        this.spreadsheetParserAndCheck(
+                provider,
+                SpreadsheetParserSelector.parse(selector),
+                expected
+        );
+    }
+
+    default void spreadsheetParserAndCheck(final SpreadsheetParserSelector selector,
+                                           final Parser<SpreadsheetParserContext> expected) {
+        this.spreadsheetParserAndCheck(
+                this.createSpreadsheetParserProvider(),
+                selector,
+                Optional.of(expected)
+        );
+    }
+
+    default void spreadsheetParserAndCheck(final SpreadsheetParserProvider provider,
+                                           final SpreadsheetParserSelector selector,
+                                           final Parser<SpreadsheetParserContext> expected) {
+        this.spreadsheetParserAndCheck(
+                provider,
+                selector,
+                Optional.of(expected)
+        );
+    }
+
+    default void spreadsheetParserAndCheck(final SpreadsheetParserSelector selector,
+                                           final Optional<Parser<SpreadsheetParserContext>> expected) {
+        this.spreadsheetParserAndCheck(
+                this.createSpreadsheetParserProvider(),
+                selector,
+                expected
+        );
+    }
+
+    default void spreadsheetParserAndCheck(final SpreadsheetParserProvider provider,
+                                           final SpreadsheetParserSelector selector,
+                                           final Optional<Parser<SpreadsheetParserContext>> expected) {
+        this.checkEquals(
+                expected,
+                provider.spreadsheetParser(selector),
+                () -> selector.toString()
+        );
+    }
+
+    default void spreadsheetParserInfosAndCheck(final SpreadsheetParserInfo... expected) {
+        this.spreadsheetParserInfosAndCheck(
+                this.createSpreadsheetParserProvider(),
+                Sets.of(
+                        expected
+                )
+        );
+    }
+
+    default void spreadsheetParserInfosAndCheck(final SpreadsheetParserProvider provider,
+                                                final SpreadsheetParserInfo... expected) {
+        this.spreadsheetParserInfosAndCheck(
+                provider,
+                Sets.of(
+                        expected
+                )
+        );
+    }
+
+    default void spreadsheetParserInfosAndCheck(final Set<SpreadsheetParserInfo> expected) {
+        this.spreadsheetParserInfosAndCheck(
+                this.createSpreadsheetParserProvider(),
+                expected
+        );
+    }
+
+    default void spreadsheetParserInfosAndCheck(final SpreadsheetParserProvider provider,
+                                                final Set<SpreadsheetParserInfo> expected) {
+        this.checkEquals(
+                expected,
+                provider.spreadsheetParserInfos(),
+                () -> provider.toString()
+        );
+    }
+}
---
+++ src/test/java/walkingkooka/spreadsheet/format/SpreadsheetParserProviderTestingTest.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright 2019 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.format;
+
+import org.junit.jupiter.api.Test;
+import walkingkooka.collect.set.Sets;
+import walkingkooka.net.Url;
+import walkingkooka.reflect.JavaVisibility;
+import walkingkooka.spreadsheet.parser.SpreadsheetParserContext;
+import walkingkooka.text.cursor.parser.Parser;
+import walkingkooka.text.cursor.parser.Parsers;
+
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
+
+public final class SpreadsheetParserProviderTestingTest implements SpreadsheetParserProviderTesting<SpreadsheetParserProviderTestingTest.TestSpreadsheetParserProvider> {
+
+    private final static String SELECTOR = "date-parse-pattern dd/mm/yyyy";
+
+    private final static Parser<SpreadsheetParserContext> PARSER = Parsers.fake();
+
+    private final static SpreadsheetParserInfo INFO = SpreadsheetParserInfo.with(
+            Url.parseAbsolute("https://example.com/123"),
+            SpreadsheetParserName.DATE_PARSE_PATTERN
+    );
+
+    @Test
+    public void testSpreadsheetParserAndCheck() {
+        this.spreadsheetParserAndCheck(
+                SELECTOR,
+                PARSER
+        );
+    }
+
+    @Test
+    public void testSpreadsheetParserInfosAndCheck() {
+        this.spreadsheetParserInfosAndCheck(
+                new TestSpreadsheetParserProvider(),
+                INFO
+        );
+    }
+
+    @Override
+    public TestSpreadsheetParserProvider createSpreadsheetParserProvider() {
+        return new TestSpreadsheetParserProvider();
+    }
+
+    class TestSpreadsheetParserProvider implements SpreadsheetParserProvider {
+        @Override
+        public Optional<Parser<SpreadsheetParserContext>> spreadsheetParser(final SpreadsheetParserSelector selector) {
+            Objects.requireNonNull(selector, "selector");
+
+            checkEquals("date-parse-pattern", selector.name().value());
+            return Optional.of(PARSER);
+        }
+
+        @Override
+        public Set<SpreadsheetParserInfo> spreadsheetParserInfos() {
+            return Sets.of(INFO);
+        }
+    }
+
+    @Override
+    public void testTestNaming() {
+        throw new UnsupportedOperationException();
+    }
+
+    // ClassTesting.....................................................................................................
+
+    @Override
+    public JavaVisibility typeVisibility() {
+        return JavaVisibility.PACKAGE_PRIVATE;
+    }
+
+    @Override
+    public Class<TestSpreadsheetParserProvider> type() {
+        return TestSpreadsheetParserProvider.class;
+    }
+}
