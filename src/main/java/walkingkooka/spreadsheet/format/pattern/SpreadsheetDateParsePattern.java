8062a5c3b2e6e350bef2c6c35741bc29df98690e
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2024-06-21 15:41:00 +1000

    SpreadsheetXXXParserPattern.parse calls Parser.parseText
    - Closes https://github.com/mP1/walkingkooka-spreadsheet/issues/4350
    - SpreadsheetTimeParsePattern.parse should be able to use parser.parseTextOrFail
    
    - Closes https://github.com/mP1/walkingkooka-spreadsheet/issues/4349
    - SpreadsheetDateParsePattern.parse should be able to use parser.parseTextOrFail
    
    - Closes https://github.com/mP1/walkingkooka-spreadsheet/issues/4348
    - SpreadsheetDateTimeParsePattern.parse should be able to use parser.parseTextOrFail

--- src/main/java/walkingkooka/spreadsheet/format/pattern/SpreadsheetDateParsePattern.java
+++ src/main/java/walkingkooka/spreadsheet/format/pattern/SpreadsheetDateParsePattern.java
@@ -22,8 +22,6 @@ import walkingkooka.convert.Converter;
 import walkingkooka.spreadsheet.convert.SpreadsheetConverters;
 import walkingkooka.spreadsheet.parser.SpreadsheetDateParserToken;
 import walkingkooka.spreadsheet.parser.SpreadsheetParserContext;
-import walkingkooka.text.cursor.TextCursors;
-import walkingkooka.text.cursor.parser.ParserReporters;
 import walkingkooka.text.cursor.parser.ParserToken;
 import walkingkooka.tree.expression.ExpressionNumberConverterContext;
 
@@ -74,10 +72,10 @@ public final class SpreadsheetDateParsePattern extends SpreadsheetNonNumberParse
     public LocalDate parse(final String text,
                            final SpreadsheetParserContext context) {
         return this.parser()
-                .orFailIfCursorNotEmpty(ParserReporters.basic())
-                .parse(TextCursors.charSequence(text), context)
-                .get()
-                .cast(SpreadsheetDateParserToken.class)
+                .parseText(
+                        text,
+                        context
+                ).cast(SpreadsheetDateParserToken.class)
                 .toLocalDate(context);
     }
 
