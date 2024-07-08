198a783daeaa687cb0913e8f7a31623b1339c46c
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2024-07-08 12:17:19 +1000

    SpreadsheetErrorThrowingConverterTest.testConvertErrorToThrows typo FIX

--- src/test/java/walkingkooka/spreadsheet/convert/SpreadsheetErrorThrowingConverterTest.java
+++ src/test/java/walkingkooka/spreadsheet/convert/SpreadsheetErrorThrowingConverterTest.java
@@ -39,7 +39,7 @@ public final class SpreadsheetErrorThrowingConverterTest implements ConverterTes
     }
 
     @Test
-    public void testConvetErrorToThrows() {
+    public void testConvertErrorToThrows() {
         assertThrows(
                 SpreadsheetErrorException.class,
                 () -> SpreadsheetErrorThrowingConverter.INSTANCE.convert(
