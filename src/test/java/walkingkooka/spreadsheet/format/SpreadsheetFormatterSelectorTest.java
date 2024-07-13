1bfb2cd2f2e3f714dd77905caf9e56f54e9df877
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2024-07-13 18:38:20 +1000

    SpreadsheetFormatterSelectorTest implements PluginSelectorLikeTesting

--- src/test/java/walkingkooka/spreadsheet/format/SpreadsheetFormatterSelectorTest.java
+++ src/test/java/walkingkooka/spreadsheet/format/SpreadsheetFormatterSelectorTest.java
@@ -18,157 +18,30 @@
 package walkingkooka.spreadsheet.format;
 
 import org.junit.jupiter.api.Test;
-import walkingkooka.HashCodeEqualsDefinedTesting2;
-import walkingkooka.InvalidCharacterException;
-import walkingkooka.ToStringTesting;
-import walkingkooka.reflect.ClassTesting2;
+import walkingkooka.plugin.PluginSelectorLikeTesting;
 import walkingkooka.reflect.JavaVisibility;
 import walkingkooka.spreadsheet.format.pattern.SpreadsheetFormatPattern;
 import walkingkooka.spreadsheet.format.pattern.SpreadsheetPattern;
-import walkingkooka.test.ParseStringTesting;
 import walkingkooka.text.CharSequences;
-import walkingkooka.text.HasTextTesting;
-import walkingkooka.text.printer.TreePrintableTesting;
 import walkingkooka.tree.json.JsonNode;
-import walkingkooka.tree.json.marshall.JsonNodeMarshallingTesting;
 import walkingkooka.tree.json.marshall.JsonNodeUnmarshallContext;
 
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertNotSame;
-import static org.junit.jupiter.api.Assertions.assertSame;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+public final class SpreadsheetFormatterSelectorTest implements PluginSelectorLikeTesting<SpreadsheetFormatterSelector, SpreadsheetFormatterName> {
 
-public final class SpreadsheetFormatterSelectorTest implements ClassTesting2<SpreadsheetFormatterSelector>,
-        HashCodeEqualsDefinedTesting2<SpreadsheetFormatterSelector>,
-        HasTextTesting,
-        ToStringTesting<SpreadsheetFormatterSelector>,
-        ParseStringTesting<SpreadsheetFormatterSelector>,
-        JsonNodeMarshallingTesting<SpreadsheetFormatterSelector>,
-        TreePrintableTesting {
-
-    private final static SpreadsheetFormatterName NAME = SpreadsheetFormatterName.with("text-format-pattern");
-
-    private final static String TEXT = "@@";
-
-    @Test
-    public void testWithNullNameFails() {
-        assertThrows(
-                NullPointerException.class,
-                () -> SpreadsheetFormatterSelector.with(
-                        null,
-                        TEXT
-                )
+    @Override
+    public SpreadsheetFormatterSelector createPluginSelectorLike(final SpreadsheetFormatterName name,
+                                                                 final String text) {
+        return SpreadsheetFormatterSelector.with(
+                name,
+                text
         );
     }
 
-    @Test
-    public void testWithNullTextFails() {
-        assertThrows(
-                NullPointerException.class,
-                () -> SpreadsheetFormatterSelector.with(
-                        NAME,
-                        null
-                )
-        );
-    }
-
-    @Test
-    public void testWith() {
-        final SpreadsheetFormatterSelector selector = SpreadsheetFormatterSelector.with(
-                NAME,
-                TEXT
-        );
-
-        this.checkEquals(NAME, selector.name(), "name");
-        this.textAndCheck(
-                selector,
-                TEXT
-        );
-    }
-
-    // setName..........................................................................................................
-
-    @Test
-    public void testSetNameWithNullFails() {
-        assertThrows(
-                NullPointerException.class,
-                () -> SpreadsheetFormatterSelector.with(
-                        NAME,
-                        TEXT
-                ).setName(null)
-        );
-    }
-
-    @Test
-    public void testSetNameWithSame() {
-        final SpreadsheetFormatterSelector selector = SpreadsheetFormatterSelector.with(
-                NAME,
-                TEXT
-        );
-        assertSame(
-                selector,
-                selector.setName(NAME)
-        );
-    }
-
-    @Test
-    public void testSetNameWithDifferent() {
-        final SpreadsheetFormatterSelector selector = SpreadsheetFormatterSelector.with(
-                NAME,
-                TEXT
-        );
-        final SpreadsheetFormatterName differentName = SpreadsheetFormatterName.with("different");
-        final SpreadsheetFormatterSelector different = selector.setName(differentName);
-
-        assertNotSame(
-                different,
-                selector
-        );
-        this.checkEquals(
-                differentName,
-                different.name(),
-                "name"
-        );
-        this.textAndCheck(
-                selector,
-                TEXT
-        );
-    }
-
-    // parse............................................................................................................
-
-    @Test
-    public void testParseInvalidSpreadsheetFormatterNameFails() {
-        this.parseStringFails(
-                "A!34",
-                new InvalidCharacterException("A!34", 1)
-                        .appendToMessage(" in \"A!34\"")
-        );
-    }
-
-    @Test
-    public void testParseSpreadsheetFormatterName() {
-        final String text = "text-format";
-        this.parseStringAndCheck(
-                text,
-                SpreadsheetFormatterSelector.with(
-                        SpreadsheetFormatterName.with(text),
-                        ""
-                )
-        );
-    }
-
-    @Test
-    public void testParseSpreadsheetFormatterNameSpace() {
-        final String text = "text-format";
-        this.parseStringAndCheck(
-                text + " ",
-                SpreadsheetFormatterSelector.with(
-                        SpreadsheetFormatterName.with(text),
-                        ""
-                )
-        );
+    @Override
+    public SpreadsheetFormatterName createName(final String value) {
+        return SpreadsheetFormatterName.with(value);
     }
 
     @Test
@@ -185,43 +58,9 @@ public final class SpreadsheetFormatterSelectorTest implements ClassTesting2<Spr
         );
     }
 
-    // SpreadsheetFormatterSelector.parse must be able to parse all SpreadsheetFormatterSelector.toString.
-
-    @Test
-    public void testParseToString() {
-        final SpreadsheetFormatterSelector selector = SpreadsheetPattern.parseDateFormatPattern("dd/mm/yyyy")
-                .spreadsheetFormatterSelector();
-
-        this.parseStringAndCheck(
-                selector.toString(),
-                selector
-        );
-    }
-
-    @Test
-    public void testParseToStringWithQuotes() {
-        final SpreadsheetFormatterSelector selector = SpreadsheetPattern.parseTextFormatPattern("\"Hello\" @")
-                .spreadsheetFormatterSelector();
-
-        this.parseStringAndCheck(
-                selector.toString(),
-                selector
-        );
-    }
-
     @Override
     public SpreadsheetFormatterSelector parseString(final String text) {
         return SpreadsheetFormatterSelector.parse(text);
-    }
-
-    @Override
-    public Class<? extends RuntimeException> parseStringFailedExpected(final Class<? extends RuntimeException> type) {
-        return type;
-    }
-
-    @Override
-    public RuntimeException parseStringFailedExpected(final RuntimeException thrown) {
-        return thrown;
     }
 
     // spreadsheetFormatPattern.........................................................................................
@@ -266,60 +105,6 @@ public final class SpreadsheetFormatterSelectorTest implements ClassTesting2<Spr
         );
     }
 
-    // equals...........................................................................................................
-
-    @Test
-    public void testEqualsDifferentName() {
-        this.checkNotEquals(
-                SpreadsheetFormatterSelector.with(
-                        SpreadsheetFormatterName.with("different"),
-                        TEXT
-                )
-        );
-    }
-
-    @Test
-    public void testEqualsDifferentText() {
-        this.checkNotEquals(
-                SpreadsheetFormatterSelector.with(
-                        NAME,
-                        "different"
-                )
-        );
-    }
-
-    @Override
-    public SpreadsheetFormatterSelector createObject() {
-        return SpreadsheetFormatterSelector.with(
-                NAME,
-                TEXT
-        );
-    }
-
-    // ToString.........................................................................................................
-
-    @Test
-    public void testToString() {
-        this.toStringAndCheck(
-                SpreadsheetFormatterSelector.with(
-                        NAME,
-                        TEXT
-                ),
-                "text-format-pattern @@"
-        );
-    }
-
-    @Test
-    public void testToStringWithQuotes() {
-        this.toStringAndCheck(
-                SpreadsheetFormatterSelector.with(
-                        NAME,
-                        "\"Hello\""
-                ),
-                "text-format-pattern \"Hello\""
-        );
-    }
-
     // ClassTesting.....................................................................................................
 
     @Override
@@ -361,25 +146,16 @@ public final class SpreadsheetFormatterSelectorTest implements ClassTesting2<Spr
 
     @Override
     public SpreadsheetFormatterSelector createJsonNodeMarshallingValue() {
-        return this.createObject();
-    }
-
-    // TreePrintable....................................................................................................
-
-    @Test
-    public void testTreePrintWithoutText() {
-        this.treePrintAndCheck(
-                SpreadsheetFormatterSelector.parse("abc123"),
-                "abc123\n"
+        return SpreadsheetFormatterSelector.with(
+                SpreadsheetFormatterName.TEXT_FORMAT_PATTERN,
+                "@@"
         );
     }
 
-    @Test
-    public void testTreePrintWithText() {
-        this.treePrintAndCheck(
-                SpreadsheetFormatterSelector.parse("text-format-pattern @@"),
-                "text-format-pattern\n" +
-                        "  \"@@\"\n"
-        );
+    // type name........................................................................................................
+
+    @Override
+    public String typeNamePrefix() {
+        return SpreadsheetFormatter.class.getSimpleName();
     }
 }
