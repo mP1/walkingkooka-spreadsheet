792c9a2629b873c9a019f4a22ec120c718e19c6a
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2024-08-13 20:28:21 +1000

    BasicSpreadsheetProvider

---
+++ src/main/java/walkingkooka/spreadsheet/provider/BasicSpreadsheetProvider.java
@@ -0,0 +1,145 @@
+/*
+ * Copyright 2019 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.provider;
+
+import walkingkooka.Cast;
+import walkingkooka.convert.provider.ConverterProvider;
+import walkingkooka.convert.provider.ConverterProviderDelegator;
+import walkingkooka.spreadsheet.compare.SpreadsheetComparatorProvider;
+import walkingkooka.spreadsheet.compare.SpreadsheetComparatorProviderDelegator;
+import walkingkooka.spreadsheet.format.SpreadsheetFormatterProvider;
+import walkingkooka.spreadsheet.format.SpreadsheetFormatterProviderDelegator;
+import walkingkooka.spreadsheet.parser.SpreadsheetParserProvider;
+import walkingkooka.spreadsheet.parser.SpreadsheetParserProviderDelegator;
+import walkingkooka.tree.expression.function.provider.ExpressionFunctionProvider;
+import walkingkooka.tree.expression.function.provider.ExpressionFunctionProviderDelegator;
+
+import java.util.Objects;
+
+/**
+ * A {@link SpreadsheetProvider} that delegates all methods to the provided {@link walkingkooka.plugin.Provider}.
+ */
+final class BasicSpreadsheetProvider implements SpreadsheetProvider,
+        ConverterProviderDelegator,
+        ExpressionFunctionProviderDelegator,
+        SpreadsheetComparatorProviderDelegator,
+        SpreadsheetFormatterProviderDelegator,
+        SpreadsheetParserProviderDelegator {
+
+    static BasicSpreadsheetProvider with(final ConverterProvider converterProvider,
+                                         final ExpressionFunctionProvider expressionFunctionProvider,
+                                         final SpreadsheetComparatorProvider spreadsheetComparatorProvider,
+                                         final SpreadsheetFormatterProvider spreadsheetFormatterProvider,
+                                         final SpreadsheetParserProvider spreadsheetParserProvider) {
+        return new BasicSpreadsheetProvider(
+                Objects.requireNonNull(converterProvider, "converterProvider"),
+                Objects.requireNonNull(expressionFunctionProvider, "expressionFunctionProvider"),
+                Objects.requireNonNull(spreadsheetComparatorProvider, "spreadsheetComparatorProvider"),
+                Objects.requireNonNull(spreadsheetFormatterProvider, "spreadsheetFormatterProvider"),
+                Objects.requireNonNull(spreadsheetParserProvider, "spreadsheetParserProvider")
+        );
+    }
+
+    private BasicSpreadsheetProvider(final ConverterProvider converterProvider,
+                                     final ExpressionFunctionProvider expressionFunctionProvider,
+                                     final SpreadsheetComparatorProvider spreadsheetComparatorProvider,
+                                     final SpreadsheetFormatterProvider spreadsheetFormatterProvider,
+                                     final SpreadsheetParserProvider spreadsheetParserProvider) {
+        this.converterProvider = converterProvider;
+        this.expressionFunctionProvider = expressionFunctionProvider;
+        this.spreadsheetComparatorProvider = spreadsheetComparatorProvider;
+        this.spreadsheetFormatterProvider = spreadsheetFormatterProvider;
+        this.spreadsheetParserProvider = spreadsheetParserProvider;
+    }
+
+    @Override
+    public ConverterProvider converterProvider() {
+        return this.converterProvider;
+    }
+
+    private final ConverterProvider converterProvider;
+
+    @Override
+    public ExpressionFunctionProvider expressionFunctionProvider() {
+        return expressionFunctionProvider;
+    }
+
+    private final ExpressionFunctionProvider expressionFunctionProvider;
+
+    @Override
+    public SpreadsheetComparatorProvider spreadsheetComparatorProvider() {
+        return this.spreadsheetComparatorProvider;
+    }
+
+    private final SpreadsheetComparatorProvider spreadsheetComparatorProvider;
+
+    @Override
+    public SpreadsheetFormatterProvider spreadsheetFormatterProvider() {
+        return this.spreadsheetFormatterProvider;
+    }
+
+    private final SpreadsheetFormatterProvider spreadsheetFormatterProvider;
+
+    @Override
+    public SpreadsheetParserProvider spreadsheetParserProvider() {
+        return this.spreadsheetParserProvider;
+    }
+
+    private final SpreadsheetParserProvider spreadsheetParserProvider;
+
+    // Object...........................................................................................................
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(
+                this.converterProvider,
+                this.expressionFunctionProvider,
+                this.spreadsheetComparatorProvider,
+                this.spreadsheetFormatterProvider,
+                this.spreadsheetParserProvider
+        );
+    }
+
+    @Override
+    public boolean equals(final Object other) {
+        return this == other ||
+                other instanceof BasicSpreadsheetProvider &&
+                        this.equals0(Cast.to(other));
+    }
+
+    private boolean equals0(final BasicSpreadsheetProvider other) {
+        return this.converterProvider.equals(other.converterProvider) &&
+                this.expressionFunctionProvider.equals(other.expressionFunctionProvider) &&
+                this.spreadsheetComparatorProvider.equals(other.spreadsheetComparatorProvider) &&
+                this.spreadsheetFormatterProvider.equals(other.spreadsheetFormatterProvider) &&
+                this.spreadsheetParserProvider.equals(other.spreadsheetParserProvider);
+    }
+
+    @Override
+    public String toString() {
+        return this.converterProvider +
+                " " +
+                this.expressionFunctionProvider +
+                " " +
+                this.spreadsheetComparatorProvider +
+                " " +
+                this.spreadsheetFormatterProvider +
+                " " +
+                this.spreadsheetParserProvider;
+    }
+}
---
+++ src/test/java/walkingkooka/spreadsheet/provider/BasicSpreadsheetProviderTest.java
@@ -0,0 +1,227 @@
+/*
+ * Copyright 2019 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.provider;
+
+import org.junit.jupiter.api.Test;
+import walkingkooka.HashCodeEqualsDefinedTesting2;
+import walkingkooka.ToStringTesting;
+import walkingkooka.convert.provider.ConverterProviders;
+import walkingkooka.reflect.JavaVisibility;
+import walkingkooka.spreadsheet.compare.SpreadsheetComparatorProviders;
+import walkingkooka.spreadsheet.format.SpreadsheetFormatterProviders;
+import walkingkooka.spreadsheet.meta.SpreadsheetMetadataTesting;
+import walkingkooka.spreadsheet.parser.SpreadsheetParserProviders;
+import walkingkooka.tree.expression.function.provider.ExpressionFunctionProviders;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public final class BasicSpreadsheetProviderTest implements SpreadsheetProviderTesting<BasicSpreadsheetProvider>,
+        HashCodeEqualsDefinedTesting2<BasicSpreadsheetProvider>,
+        ToStringTesting<BasicSpreadsheetProvider>,
+        SpreadsheetMetadataTesting {
+
+    @Override
+    public void testFunctionWithNullNameFails() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void testFunctionWithNullContextFails() {
+        throw new UnsupportedOperationException();
+    }
+
+    // with.............................................................................................................
+
+    @Test
+    public void testWithNullConverterProviderFails() {
+        assertThrows(
+                NullPointerException.class,
+                () -> BasicSpreadsheetProvider.with(
+                        null,
+                        EXPRESSION_FUNCTION_PROVIDER,
+                        SPREADSHEET_COMPARATOR_PROVIDER,
+                        SPREADSHEET_FORMATTER_PROVIDER,
+                        SPREADSHEET_PARSER_PROVIDER
+                )
+        );
+    }
+
+    @Test
+    public void testWithNullExpressionFunctionProviderFails() {
+        assertThrows(
+                NullPointerException.class,
+                () -> BasicSpreadsheetProvider.with(
+                        CONVERTER_PROVIDER,
+                        null,
+                        SPREADSHEET_COMPARATOR_PROVIDER,
+                        SPREADSHEET_FORMATTER_PROVIDER,
+                        SPREADSHEET_PARSER_PROVIDER
+                )
+        );
+    }
+
+    @Test
+    public void testWithNullSpreadsheetComparatorProviderFails() {
+        assertThrows(
+                NullPointerException.class,
+                () -> BasicSpreadsheetProvider.with(
+                        CONVERTER_PROVIDER,
+                        EXPRESSION_FUNCTION_PROVIDER,
+                        null,
+                        SPREADSHEET_FORMATTER_PROVIDER,
+                        SPREADSHEET_PARSER_PROVIDER
+                )
+        );
+    }
+
+    @Test
+    public void testWithNullSpreadsheetFormatterProviderFails() {
+        assertThrows(
+                NullPointerException.class,
+                () -> BasicSpreadsheetProvider.with(
+                        CONVERTER_PROVIDER,
+                        EXPRESSION_FUNCTION_PROVIDER,
+                        SPREADSHEET_COMPARATOR_PROVIDER,
+                        null,
+                        SPREADSHEET_PARSER_PROVIDER
+                )
+        );
+    }
+
+    @Test
+    public void testWithNullSpreadsheetParserProviderFails() {
+        assertThrows(
+                NullPointerException.class,
+                () -> BasicSpreadsheetProvider.with(
+                        CONVERTER_PROVIDER,
+                        EXPRESSION_FUNCTION_PROVIDER,
+                        SPREADSHEET_COMPARATOR_PROVIDER,
+                        SPREADSHEET_FORMATTER_PROVIDER,
+                        null
+                )
+        );
+    }
+
+    @Override
+    public BasicSpreadsheetProvider createSpreadsheetProvider() {
+        return BasicSpreadsheetProvider.with(
+                CONVERTER_PROVIDER,
+                EXPRESSION_FUNCTION_PROVIDER,
+                SPREADSHEET_COMPARATOR_PROVIDER,
+                SPREADSHEET_FORMATTER_PROVIDER,
+                SPREADSHEET_PARSER_PROVIDER
+        );
+    }
+
+    // hashCode/equals...................................................................................................
+    @Test
+    public void testEqualsDifferentConverterProvider() {
+        this.checkNotEquals(
+                BasicSpreadsheetProvider.with(
+                        ConverterProviders.fake(),
+                        EXPRESSION_FUNCTION_PROVIDER,
+                        SPREADSHEET_COMPARATOR_PROVIDER,
+                        SPREADSHEET_FORMATTER_PROVIDER,
+                        SPREADSHEET_PARSER_PROVIDER
+                )
+        );
+    }
+
+    @Test
+    public void testEqualsDifferentExpressionFunctionProvider() {
+        this.checkNotEquals(
+                BasicSpreadsheetProvider.with(
+                        CONVERTER_PROVIDER,
+                        ExpressionFunctionProviders.fake(),
+                        SPREADSHEET_COMPARATOR_PROVIDER,
+                        SPREADSHEET_FORMATTER_PROVIDER,
+                        SPREADSHEET_PARSER_PROVIDER
+                )
+        );
+    }
+
+    @Test
+    public void testEqualsDifferentSpreadsheetComparatorProvider() {
+        this.checkNotEquals(
+                BasicSpreadsheetProvider.with(
+                        CONVERTER_PROVIDER,
+                        EXPRESSION_FUNCTION_PROVIDER,
+                        SpreadsheetComparatorProviders.fake(),
+                        SPREADSHEET_FORMATTER_PROVIDER,
+                        SPREADSHEET_PARSER_PROVIDER
+                )
+        );
+    }
+
+    @Test
+    public void testEqualsDifferentSpreadsheetFormatterProvider() {
+        this.checkNotEquals(
+                BasicSpreadsheetProvider.with(
+                        CONVERTER_PROVIDER,
+                        EXPRESSION_FUNCTION_PROVIDER,
+                        SPREADSHEET_COMPARATOR_PROVIDER,
+                        SpreadsheetFormatterProviders.fake(),
+                        SPREADSHEET_PARSER_PROVIDER
+                )
+        );
+    }
+
+    @Test
+    public void testEqualsDifferentSpreadsheetParserProvider() {
+        this.checkNotEquals(
+                BasicSpreadsheetProvider.with(
+                        CONVERTER_PROVIDER,
+                        EXPRESSION_FUNCTION_PROVIDER,
+                        SPREADSHEET_COMPARATOR_PROVIDER,
+                        SPREADSHEET_FORMATTER_PROVIDER,
+                        SpreadsheetParserProviders.fake()
+                )
+        );
+    }
+
+    @Override
+    public BasicSpreadsheetProvider createObject() {
+        return this.createSpreadsheetProvider();
+    }
+
+    // toString.........................................................................................................
+
+    @Test
+    public void testToString() {
+        this.toStringAndCheck(
+                this.createSpreadsheetProvider(),
+                CONVERTER_PROVIDER + " " +
+                        EXPRESSION_FUNCTION_PROVIDER + " " +
+                        SPREADSHEET_COMPARATOR_PROVIDER + " " +
+                        SPREADSHEET_FORMATTER_PROVIDER + " " +
+                        SPREADSHEET_PARSER_PROVIDER
+        );
+    }
+
+    // Class............................................................................................................
+
+    @Override
+    public Class<BasicSpreadsheetProvider> type() {
+        return BasicSpreadsheetProvider.class;
+    }
+
+    @Override
+    public JavaVisibility typeVisibility() {
+        return JavaVisibility.PACKAGE_PRIVATE;
+    }
+}
