06d74ad5f96ffb43152343844c94028d15d0567e
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2024-08-15 19:40:08 +1000

    SpreadsheetConverters javadoc

--- src/main/java/walkingkooka/spreadsheet/convert/SpreadsheetConverters.java
+++ src/main/java/walkingkooka/spreadsheet/convert/SpreadsheetConverters.java
@@ -43,7 +43,7 @@ import java.util.function.BiFunction;
 import java.util.function.Function;
 
 /**
- * A collection of factors for creating {@link Converter} converters.
+ * A collection of factory methods for creating {@link Converter} converters.
  */
 public final class SpreadsheetConverters implements PublicStaticHelper {
 
