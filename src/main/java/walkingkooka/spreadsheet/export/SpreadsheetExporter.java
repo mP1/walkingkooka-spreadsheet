17ef62cbb88153a78c36ed7581ecd42e8b662415
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2024-08-28 13:44:45 +1000

    SpreadsheetExporter

---
+++ src/main/java/walkingkooka/spreadsheet/export/SpreadsheetExporter.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2019 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.export;
+
+import walkingkooka.net.WebEntity;
+import walkingkooka.spreadsheet.SpreadsheetCellRange;
+import walkingkooka.spreadsheet.importer.SpreadsheetImporterContext;
+
+/**
+ * Exports a range of cells returning a {@link WebEntity} which may hold JSON, HTML or more.
+ */
+public interface SpreadsheetExporter {
+
+    boolean canExport(final SpreadsheetCellRange cells,
+                      final SpreadsheetImporterContext context);
+
+    WebEntity export(final SpreadsheetCellRange cells,
+                     final SpreadsheetImporterContext context);
+}
