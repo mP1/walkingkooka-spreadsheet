07bde577062567de9a8fc2b837efac3bfa6e8988
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2024-08-28 12:52:47 +1000

    SpreadsheetExporterInfo

---
+++ src/main/java/walkingkooka/spreadsheet/export/SpreadsheetExporterInfo.java
@@ -0,0 +1,126 @@
+/*
+ * Copyright 2019 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.export;
+
+import walkingkooka.Cast;
+import walkingkooka.net.AbsoluteUrl;
+import walkingkooka.plugin.PluginInfoLike;
+import walkingkooka.tree.json.JsonNode;
+import walkingkooka.tree.json.marshall.JsonNodeContext;
+import walkingkooka.tree.json.marshall.JsonNodeUnmarshallContext;
+
+import java.util.Objects;
+
+/**
+ * Provides a few bits of info describing a {@link SpreadsheetExporter}. The {@link AbsoluteUrl} must be a unique identifier,
+ * with the {@link SpreadsheetExporterName} being a shorter human friendly reference.
+ */
+public final class SpreadsheetExporterInfo implements PluginInfoLike<SpreadsheetExporterInfo, SpreadsheetExporterName> {
+
+    public static SpreadsheetExporterInfo parse(final String text) {
+        return PluginInfoLike.parse(
+                text,
+                SpreadsheetExporterName::with,
+                SpreadsheetExporterInfo::with
+        );
+    }
+
+    public static SpreadsheetExporterInfo with(final AbsoluteUrl url,
+                                               final SpreadsheetExporterName name) {
+        return new SpreadsheetExporterInfo(
+                Objects.requireNonNull(url, "url"),
+                Objects.requireNonNull(name, "name")
+        );
+    }
+
+    private SpreadsheetExporterInfo(final AbsoluteUrl url,
+                                    final SpreadsheetExporterName name) {
+        this.url = url;
+        this.name = name;
+    }
+
+    // HasAbsoluteUrl...................................................................................................
+
+    @Override
+    public AbsoluteUrl url() {
+        return this.url;
+    }
+
+    private final AbsoluteUrl url;
+
+    // HasName..........................................................................................................
+
+    @Override
+    public SpreadsheetExporterName name() {
+        return this.name;
+    }
+
+    private final SpreadsheetExporterName name;
+
+    // Object...........................................................................................................
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(
+                this.url,
+                this.name
+        );
+    }
+
+    @Override
+    public boolean equals(final Object other) {
+        return this == other ||
+                other instanceof SpreadsheetExporterInfo &&
+                        this.equals0(Cast.to(other));
+    }
+
+    private boolean equals0(final SpreadsheetExporterInfo other) {
+        return this.url.equals(other.url) &&
+                this.name.equals(other.name);
+    }
+
+    @Override
+    public String toString() {
+        return PluginInfoLike.toString(this);
+    }
+
+    // Json.............................................................................................................
+
+    static void register() {
+        // required to FORCE json register
+    }
+
+    static SpreadsheetExporterInfo unmarshall(final JsonNode node,
+                                              final JsonNodeUnmarshallContext context) {
+        return PluginInfoLike.unmarshall(
+                node,
+                context,
+                SpreadsheetExporterName.class,
+                SpreadsheetExporterInfo::with
+        );
+    }
+
+    static {
+        JsonNodeContext.register(
+                JsonNodeContext.computeTypeName(SpreadsheetExporterInfo.class),
+                SpreadsheetExporterInfo::unmarshall,
+                SpreadsheetExporterInfo::marshall,
+                SpreadsheetExporterInfo.class
+        );
+    }
+}
