d63ce11a6c2d229ae9e3473bfe8eacb2af9ffb17
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2024-11-14 14:17:37 +1100

    SpreadsheetExpressionFunctionNames

---
+++ src/main/java/walkingkooka/spreadsheet/SpreadsheetExpressionFunctionNames.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2019 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet;
+
+import walkingkooka.reflect.PublicStaticHelper;
+import walkingkooka.text.CaseSensitivity;
+
+/**
+ * A collection of constants for spreadsheet functions.
+ */
+public final class SpreadsheetExpressionFunctionNames implements PublicStaticHelper {
+
+    /**
+     * Function names are {@link CaseSensitivity#INSENSITIVE}
+     */
+    public static final CaseSensitivity CASE_SENSITIVITY = CaseSensitivity.INSENSITIVE;
+
+    /**
+     * Stop creation
+     */
+    private SpreadsheetExpressionFunctionNames() {
+        throw new UnsupportedOperationException();
+    }
+}
