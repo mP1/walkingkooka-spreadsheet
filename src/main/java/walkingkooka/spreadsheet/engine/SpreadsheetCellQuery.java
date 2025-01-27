559955b524eb71e7285b0c5eb2ad89c41b1bcf91
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2025-01-27 18:31:27 +1100

    Copyright update
    - Fixed a few Copyright 2023/2024 -> 2019

--- src/main/java/walkingkooka/spreadsheet/engine/SpreadsheetCellQuery.java
+++ src/main/java/walkingkooka/spreadsheet/engine/SpreadsheetCellQuery.java
@@ -16,7 +16,7 @@
  */
 
 /*
- * Copyright 2023 Miroslav Pokorny (github.com/mP1)
+ * Copyright 2019 Miroslav Pokorny (github.com/mP1)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
