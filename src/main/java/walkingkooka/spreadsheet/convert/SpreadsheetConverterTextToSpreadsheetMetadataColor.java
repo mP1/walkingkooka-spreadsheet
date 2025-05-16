d90a09615498222b4d6b180773bbe241c07787fe
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2025-05-16 22:11:10 +1000

    SpreadsheetConverterTextToSpreadsheetColor
    - Closes https://github.com/mP1/walkingkooka-spreadsheet/issues/6122
    - Converter supporting Strings "Color-##" to Color via SpreadsheetMetadata

---
+++ src/main/java/walkingkooka/spreadsheet/convert/SpreadsheetConverterTextToSpreadsheetMetadataColor.java
@@ -0,0 +1,69 @@
+
+/*
+ * Copyright 2019 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.convert;
+
+import walkingkooka.color.Color;
+import walkingkooka.convert.Converter;
+import walkingkooka.spreadsheet.format.parser.SpreadsheetFormatParserContexts;
+import walkingkooka.spreadsheet.format.parser.SpreadsheetFormatParsers;
+import walkingkooka.spreadsheet.meta.SpreadsheetMetadata;
+import walkingkooka.text.cursor.parser.InvalidCharacterExceptionFactory;
+
+/**
+ * A {@link Converter} that converts a color-### as a {@link String} into a {@link Color} defined within the {@link SpreadsheetMetadata}.
+ */
+final class SpreadsheetConverterTextToSpreadsheetMetadataColor extends SpreadsheetConverterTextTo {
+
+    /**
+     * Singleton
+     */
+    final static SpreadsheetConverterTextToSpreadsheetMetadataColor INSTANCE = new SpreadsheetConverterTextToSpreadsheetMetadataColor();
+
+    private SpreadsheetConverterTextToSpreadsheetMetadataColor() {
+        super();
+    }
+
+    @Override
+    boolean isType(final Object value,
+                   final Class<?> type,
+                   final SpreadsheetConverterContext context) {
+        return Color.class == type;
+    }
+
+    @Override
+    Color tryConvert(final String text,
+                     final Class<?> type,
+                     final SpreadsheetConverterContext context) {
+        return SpreadsheetConverterTextToSpreadsheetMetadataColorSpreadsheetFormatParserTokenVisitor.color(
+                        SpreadsheetFormatParsers.color()
+                                .parseText(
+                                        text,
+                                        SpreadsheetFormatParserContexts.basic(
+                                                InvalidCharacterExceptionFactory.POSITION
+                                        )
+                                ),
+                context
+        );
+    }
+
+    @Override
+    public String toString() {
+        return "Text to SpreadsheetMetadata " + Color.class.getSimpleName();
+    }
+}
