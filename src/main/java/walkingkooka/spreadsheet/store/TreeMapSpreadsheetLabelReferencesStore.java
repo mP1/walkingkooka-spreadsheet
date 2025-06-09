c9c9f5fdfd4a5c085dc99c6788b0516e6887ca1d
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2025-06-09 13:00:06 +1000

    SpreadsheetLabelReferencesStore

---
+++ src/main/java/walkingkooka/spreadsheet/store/TreeMapSpreadsheetLabelReferencesStore.java
@@ -0,0 +1,186 @@
+
+/*
+ * Copyright 2019 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.store;
+
+import walkingkooka.spreadsheet.reference.SpreadsheetCellReference;
+import walkingkooka.spreadsheet.reference.SpreadsheetLabelName;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.function.Consumer;
+
+/**
+ * A {@link SpreadsheetLabelReferencesStore} that uses a {@link Map} to store a cell or label to its many {@link SpreadsheetLabelName}.
+ */
+final class TreeMapSpreadsheetLabelReferencesStore implements SpreadsheetLabelReferencesStore {
+
+    static TreeMapSpreadsheetLabelReferencesStore empty() {
+        return new TreeMapSpreadsheetLabelReferencesStore();
+    }
+
+    private TreeMapSpreadsheetLabelReferencesStore() {
+        this.store = SpreadsheetExpressionReferenceStores.treeMap();
+    }
+
+    @Override
+    public Set<SpreadsheetCellReference> save(final Set<SpreadsheetCellReference> cells) {
+        return this.store.save(cells);
+    }
+
+    @Override
+    public Runnable addSaveWatcher(final Consumer<Set<SpreadsheetCellReference>> saved) {
+        return this.store.addSaveWatcher(saved);
+    }
+
+    @Override
+    public void saveCells(final SpreadsheetLabelName label,
+                          final Set<SpreadsheetCellReference> cells) {
+        this.store.saveCells(
+                label,
+                cells
+        );
+    }
+
+    @Override
+    public void addCell(final ReferenceAndSpreadsheetCellReference<SpreadsheetLabelName> referenceAndCell) {
+        this.store.addCell(referenceAndCell);
+    }
+
+    @Override
+    public Runnable addAddCellWatcher(final Consumer<ReferenceAndSpreadsheetCellReference<SpreadsheetLabelName>> watcher) {
+        return this.store.addAddCellWatcher(watcher);
+    }
+
+    @Override
+    public void removeCell(final ReferenceAndSpreadsheetCellReference<SpreadsheetLabelName> referenceAndCell) {
+        this.store.removeCell(referenceAndCell);
+    }
+
+    @Override
+    public Set<SpreadsheetCellReference> findCellsWithReference(final SpreadsheetLabelName label,
+                                                                final int offset,
+                                                                final int count) {
+        return this.store.findCellsWithReference(
+                label,
+                offset,
+                count
+        );
+    }
+
+    @Override
+    public int countCellsWithReference(final SpreadsheetLabelName label) {
+        return this.store.countCellsWithReference(label);
+    }
+
+    @Override
+    public Runnable addRemoveCellWatcher(final Consumer<ReferenceAndSpreadsheetCellReference<SpreadsheetLabelName>> watcher) {
+        return this.store.addRemoveCellWatcher(watcher);
+    }
+
+    @Override
+    public Set<SpreadsheetLabelName> findReferencesWithCell(final SpreadsheetCellReference cell,
+                                                            final int offset,
+                                                            final int count) {
+        return this.store.findReferencesWithCell(
+                cell,
+                offset,
+                count
+        );
+    }
+
+    @Override
+    public void removeReferencesWithCell(final SpreadsheetCellReference cell) {
+        this.store.removeReferencesWithCell(cell);
+    }
+
+    // Store............................................................................................................
+
+    @Override
+    public Optional<Set<SpreadsheetCellReference>> load(final SpreadsheetLabelName label) {
+        return this.store.load(label);
+    }
+
+    @Override
+    public void delete(final SpreadsheetLabelName label) {
+        this.store.delete(label);
+    }
+
+    @Override
+    public Runnable addDeleteWatcher(final Consumer<SpreadsheetLabelName> watcher) {
+        return this.store.addDeleteWatcher(watcher);
+    }
+
+    @Override
+    public int count() {
+        return this.store.count();
+    }
+
+    @Override
+    public Set<SpreadsheetLabelName> ids(final int offset,
+                                         final int count) {
+        return this.store.ids(
+                offset,
+                count
+        );
+    }
+
+    @Override
+    public Optional<SpreadsheetLabelName> firstId() {
+        return this.store.firstId();
+    }
+
+    @Override
+    public List<Set<SpreadsheetCellReference>> values(final int offset,
+                                                      final int count) {
+        return this.store.values(
+                offset,
+                count
+        );
+    }
+
+    @Override
+    public Optional<Set<SpreadsheetCellReference>> firstValue() {
+        return this.store.firstValue();
+    }
+
+    @Override
+    public List<Set<SpreadsheetCellReference>> all() {
+        return this.store.all();
+    }
+
+    @Override
+    public List<Set<SpreadsheetCellReference>> between(final SpreadsheetLabelName from,
+                                                       final SpreadsheetLabelName to) {
+        return this.store.between(
+                from,
+                to
+        );
+    }
+
+    private final SpreadsheetExpressionReferenceStore<SpreadsheetLabelName> store;
+
+    // Object...........................................................................................................
+
+    @Override
+    public String toString() {
+        return this.store.toString();
+    }
+}
---
+++ src/test/java/walkingkooka/spreadsheet/store/SpreadsheetLabelReferencesStoreTestingTest.java
@@ -0,0 +1,252 @@
+/*
+ * Copyright 2019 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.store;
+
+import walkingkooka.spreadsheet.reference.SpreadsheetCellReference;
+import walkingkooka.spreadsheet.reference.SpreadsheetLabelName;
+import walkingkooka.spreadsheet.reference.SpreadsheetSelection;
+import walkingkooka.store.Store;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
+import java.util.function.Consumer;
+
+public final class SpreadsheetLabelReferencesStoreTestingTest implements SpreadsheetLabelReferencesStoreTesting<SpreadsheetLabelReferencesStoreTestingTest.TestSpreadsheetLabelReferencesStore> {
+
+    @Override
+    public void testTestNaming() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void testAddCellWithWatcher() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void testAddDeleteWatcherAndRemove() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void testDeleteDoesntFireDeleteWatcher() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void testDeleteWithRemoveCellWatcher() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void testRemoveCellWithWatcher() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void testRemoveLastCellAddDeleteWatcher() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void testSaveCellsAddCellWatcher() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void testSaveCellsDoesntFireDeleteWatchers() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void testSaveCellsReplaceAddCellWatcher() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void testSaveCellsReplaceAddCellWatcher2() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public TestSpreadsheetLabelReferencesStore createStore() {
+        return new TestSpreadsheetLabelReferencesStore();
+    }
+
+    @Override
+    public SpreadsheetLabelName id() {
+        return SpreadsheetSelection.labelName("Label123");
+    }
+
+    @Override
+    public Set<SpreadsheetCellReference> value() {
+        return Set.of(
+                SpreadsheetSelection.parseCell("B2"),
+                SpreadsheetSelection.parseCell("C3")
+        );
+    }
+
+    // class............................................................................................................
+
+    @Override
+    public Class<TestSpreadsheetLabelReferencesStore> type() {
+        return TestSpreadsheetLabelReferencesStore.class;
+    }
+
+    @Override
+    public String typeNamePrefix() {
+        return "";
+    }
+
+    static class TestSpreadsheetLabelReferencesStore implements SpreadsheetLabelReferencesStore {
+        @Override
+        public void saveCells(final SpreadsheetLabelName label,
+                              final Set<SpreadsheetCellReference> cells) {
+            Objects.requireNonNull(label, "label");
+            Objects.requireNonNull(cells, "cells");
+        }
+
+        @Override
+        public void addCell(final ReferenceAndSpreadsheetCellReference<SpreadsheetLabelName> labelAndReference) {
+            Objects.requireNonNull(labelAndReference, "labelAndReference");
+        }
+
+        @Override
+        public Runnable addAddCellWatcher(final Consumer<ReferenceAndSpreadsheetCellReference<SpreadsheetLabelName>> watcher) {
+            Objects.requireNonNull(watcher, "watcher");
+            return null;
+        }
+
+        @Override
+        public void removeCell(final ReferenceAndSpreadsheetCellReference<SpreadsheetLabelName> labelAndReference) {
+            Objects.requireNonNull(labelAndReference, "labelAndReference");
+        }
+
+        @Override
+        public Set<SpreadsheetCellReference> findCellsWithReference(final SpreadsheetLabelName label,
+                                                                    final int offset,
+                                                                    final int count) {
+            Objects.requireNonNull(label, "label");
+            Store.checkOffsetAndCount(
+                    offset,
+                    count
+            );
+            return Set.of();
+        }
+
+//        @Override
+//        public Set<SpreadsheetCellReference> findCellsWithCellOrCellRange(final SpreadsheetCellReferenceOrRange cellOrCellRange,
+//                                                                          final int offset,
+//                                                                          final int count) {
+//            Objects.requireNonNull(cellOrCellRange, "cellOrCellRange");
+//            Store.checkOffsetAndCount(
+//                    offset,
+//                    count
+//            );
+//            return Set.of();
+//        }
+
+        @Override
+        public int countCellsWithReference(final SpreadsheetLabelName label) {
+            Objects.requireNonNull(label, "label");
+            return 0;
+        }
+
+        @Override
+        public Runnable addRemoveCellWatcher(final Consumer<ReferenceAndSpreadsheetCellReference<SpreadsheetLabelName>> watcher) {
+            Objects.requireNonNull(watcher, "watcher");
+            return null;
+        }
+
+        @Override
+        public Set<SpreadsheetLabelName> findReferencesWithCell(final SpreadsheetCellReference cell,
+                                                                    final int offset,
+                                                                    final int count) {
+            Objects.requireNonNull(cell, "cell");
+            Store.checkOffsetAndCount(
+                    offset,
+                    count
+            );
+            return Set.of();
+        }
+
+        @Override
+        public void removeReferencesWithCell(final SpreadsheetCellReference cell) {
+            Objects.requireNonNull(cell, "cell");
+        }
+
+        @Override
+        public Optional<Set<SpreadsheetCellReference>> load(final SpreadsheetLabelName label) {
+            Objects.requireNonNull(label, "label");
+            return Optional.empty();
+        }
+
+        @Override
+        public void delete(final SpreadsheetLabelName label) {
+            Objects.requireNonNull(label, "label");
+        }
+
+        @Override
+        public Runnable addDeleteWatcher(final Consumer<SpreadsheetLabelName> watcher) {
+            Objects.requireNonNull(watcher, "watcher");
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public int count() {
+            return 0;
+        }
+
+        @Override
+        public Set<SpreadsheetLabelName> ids(final int offset,
+                                             final int count) {
+            Store.checkOffsetAndCount(
+                    offset,
+                    count
+            );
+            return Set.of();
+        }
+
+        @Override
+        public List<Set<SpreadsheetCellReference>> values(final int offset,
+                                                          final int count) {
+            Store.checkOffsetAndCount(
+                    offset,
+                    count
+            );
+            return List.of();
+        }
+
+        @Override
+        public List<Set<SpreadsheetCellReference>> between(final SpreadsheetLabelName from,
+                                                           final SpreadsheetLabelName to) {
+            Store.checkBetween(
+                    from,
+                    to
+            );
+            return List.of();
+        }
+
+        @Override
+        public String toString() {
+            return this.getClass().getSimpleName();
+        }
+    }
+}
