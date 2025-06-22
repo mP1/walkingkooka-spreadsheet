9d9ef797c9fc2b403c27366c25cffbb123e16c2f
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2025-06-22 14:58:01 +1000

    SpreadsheetStoreRepositorySpreadsheetExpressionReferenceLoader

---
+++ src/main/java/walkingkooka/spreadsheet/reference/SpreadsheetStoreRepositorySpreadsheetExpressionReferenceLoader.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2019 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.reference;
+
+import walkingkooka.spreadsheet.SpreadsheetCell;
+import walkingkooka.spreadsheet.expression.SpreadsheetExpressionEvaluationContext;
+import walkingkooka.spreadsheet.store.repo.SpreadsheetStoreRepository;
+
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
+
+/**
+ * A {@link SpreadsheetExpressionReferenceLoader} that calls a method on a {@link walkingkooka.spreadsheet.store.SpreadsheetStore}.
+ * Note cells are returned without evaluating expressions, formatting etc.
+ */
+final class SpreadsheetStoreRepositorySpreadsheetExpressionReferenceLoader implements SpreadsheetExpressionReferenceLoader {
+
+    /**
+     * Factory
+     */
+    static SpreadsheetStoreRepositorySpreadsheetExpressionReferenceLoader with(final SpreadsheetStoreRepository repository) {
+        return new SpreadsheetStoreRepositorySpreadsheetExpressionReferenceLoader(
+                Objects.requireNonNull(repository, "repository")
+        );
+    }
+
+    private SpreadsheetStoreRepositorySpreadsheetExpressionReferenceLoader(final SpreadsheetStoreRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public Optional<SpreadsheetCell> loadCell(final SpreadsheetCellReference cell,
+                                              final SpreadsheetExpressionEvaluationContext context) {
+        Objects.requireNonNull(cell, "cell");
+        Objects.requireNonNull(context, "context");
+
+        return this.repository.cells()
+                .load(cell);
+    }
+
+    @Override
+    public Set<SpreadsheetCell> loadCellRange(final SpreadsheetCellRangeReference range,
+                                              final SpreadsheetExpressionEvaluationContext context) {
+        Objects.requireNonNull(range, "cell");
+        Objects.requireNonNull(context, "context");
+
+        return this.repository.cells()
+                .loadCellRange(range);
+    }
+
+    @Override
+    public Optional<SpreadsheetLabelMapping> loadLabel(final SpreadsheetLabelName labelName) {
+        Objects.requireNonNull(labelName, "labelName");
+
+        return this.repository.labels()
+                .load(labelName);
+    }
+
+    @Override
+    public String toString() {
+        return "repository=" + this.repository;
+    }
+
+    private final SpreadsheetStoreRepository repository;
+}
---
+++ src/test/java/walkingkooka/spreadsheet/reference/SpreadsheetStoreRepositorySpreadsheetExpressionReferenceLoaderTest.java
@@ -0,0 +1,151 @@
+/*
+ * Copyright 2019 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.reference;
+
+import org.junit.jupiter.api.Test;
+import walkingkooka.spreadsheet.SpreadsheetCell;
+import walkingkooka.spreadsheet.expression.SpreadsheetExpressionEvaluationContext;
+import walkingkooka.spreadsheet.expression.SpreadsheetExpressionEvaluationContexts;
+import walkingkooka.spreadsheet.formula.SpreadsheetFormula;
+import walkingkooka.spreadsheet.store.SpreadsheetCellStore;
+import walkingkooka.spreadsheet.store.SpreadsheetCellStores;
+import walkingkooka.spreadsheet.store.SpreadsheetLabelStore;
+import walkingkooka.spreadsheet.store.SpreadsheetLabelStores;
+import walkingkooka.spreadsheet.store.repo.FakeSpreadsheetStoreRepository;
+import walkingkooka.spreadsheet.store.repo.SpreadsheetStoreRepositories;
+import walkingkooka.spreadsheet.store.repo.SpreadsheetStoreRepository;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public final class SpreadsheetStoreRepositorySpreadsheetExpressionReferenceLoaderTest implements SpreadsheetExpressionReferenceLoaderTesting<SpreadsheetStoreRepositorySpreadsheetExpressionReferenceLoader> {
+
+    private final static SpreadsheetCellReference CELL = SpreadsheetSelection.A1;
+
+    @Test
+    public void testWithNullSpreadsheetStoreRepositoryFails() {
+        assertThrows(
+                NullPointerException.class,
+                () -> SpreadsheetStoreRepositorySpreadsheetExpressionReferenceLoader.with(null)
+        );
+    }
+
+    @Test
+    public void testLoadCell() {
+        final SpreadsheetStoreRepository repo = new FakeSpreadsheetStoreRepository() {
+
+            @Override
+            public SpreadsheetCellStore cells() {
+                return this.store;
+            }
+
+            private final SpreadsheetCellStore store = SpreadsheetCellStores.treeMap();
+        };
+
+        final SpreadsheetCell cell = CELL.setFormula(
+                SpreadsheetFormula.EMPTY.setValue(
+                        Optional.of(123)
+                )
+        );
+
+        repo.cells()
+                .save(cell);
+
+        this.loadCellAndCheck(
+                SpreadsheetStoreRepositorySpreadsheetExpressionReferenceLoader.with(repo),
+                CELL,
+                this.createContext(),
+                cell
+        );
+    }
+
+    @Test
+    public void testLoadCellRange() {
+        final SpreadsheetStoreRepository repo = new FakeSpreadsheetStoreRepository() {
+
+            @Override
+            public SpreadsheetCellStore cells() {
+                return this.store;
+            }
+
+            private final SpreadsheetCellStore store = SpreadsheetCellStores.treeMap();
+        };
+
+        final SpreadsheetCell a1 = CELL.setFormula(
+                SpreadsheetFormula.EMPTY.setValue(
+                        Optional.of(111)
+                )
+        );
+
+        final SpreadsheetCell b2 = SpreadsheetSelection.parseCell("B2")
+                .setFormula(
+                        SpreadsheetFormula.EMPTY.setValue(
+                                Optional.of(222)
+                        )
+                );
+
+        repo.cells()
+                .save(a1);
+        repo.cells()
+                .save(b2);
+
+        this.loadCellRangeAndCheck(
+                SpreadsheetStoreRepositorySpreadsheetExpressionReferenceLoader.with(repo),
+                SpreadsheetSelection.parseCellRange("A1:B2"),
+                this.createContext(),
+                a1,
+                b2
+        );
+    }
+
+    @Test
+    public void testLoadLabel() {
+        final SpreadsheetStoreRepository repo = new FakeSpreadsheetStoreRepository() {
+
+            @Override
+            public SpreadsheetLabelStore labels() {
+                return this.store;
+            }
+
+            private final SpreadsheetLabelStore store = SpreadsheetLabelStores.treeMap();
+        };
+
+        final SpreadsheetLabelMapping label = SpreadsheetSelection.labelName("Label123")
+                .setLabelMappingReference(CELL);
+
+        repo.labels()
+                .save(label);
+
+        this.loadLabelAndCheck(
+                SpreadsheetStoreRepositorySpreadsheetExpressionReferenceLoader.with(repo),
+                label.label(),
+                label
+        );
+    }
+
+    @Override
+    public SpreadsheetStoreRepositorySpreadsheetExpressionReferenceLoader createSpreadsheetExpressionReferenceLoader() {
+        return SpreadsheetStoreRepositorySpreadsheetExpressionReferenceLoader.with(SpreadsheetStoreRepositories.fake());
+    }
+
+    @Override
+    public SpreadsheetExpressionEvaluationContext createContext() {
+        return SpreadsheetExpressionEvaluationContexts.fake();
+    }
+}
