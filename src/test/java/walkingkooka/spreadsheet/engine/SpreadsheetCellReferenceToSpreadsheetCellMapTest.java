de845d34d41df3e7bab9fa6a87c1d81371648695
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2025-07-07 11:37:39 +1000

    SpreadsheetCellReferenceToSpreadsheetCellMap

---
+++ src/test/java/walkingkooka/spreadsheet/engine/SpreadsheetCellReferenceToSpreadsheetCellMapTest.java
@@ -0,0 +1,198 @@
+/*
+ * Copyright 2019 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.engine;
+
+import org.junit.jupiter.api.Test;
+import walkingkooka.collect.map.MapTesting2;
+import walkingkooka.collect.map.Maps;
+import walkingkooka.reflect.ClassTesting2;
+import walkingkooka.reflect.JavaVisibility;
+import walkingkooka.spreadsheet.SpreadsheetCell;
+import walkingkooka.spreadsheet.formula.SpreadsheetFormula;
+import walkingkooka.spreadsheet.reference.SpreadsheetCellReference;
+import walkingkooka.spreadsheet.reference.SpreadsheetSelection;
+
+import java.util.Iterator;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public final class SpreadsheetCellReferenceToSpreadsheetCellMapTest implements MapTesting2<SpreadsheetCellReferenceToSpreadsheetCellMap, SpreadsheetCellReference, SpreadsheetCell>,
+        ClassTesting2<SpreadsheetCellReferenceToSpreadsheetCellMap> {
+
+    private final static SpreadsheetCellReference KEY1 = SpreadsheetCellReference.A1;
+
+    private final static SpreadsheetCell VALUE1 = KEY1.setFormula(
+            SpreadsheetFormula.EMPTY.setText("=1")
+    );
+
+    private final static SpreadsheetCellReference KEY2 = SpreadsheetCellReference.parseCell("A2");
+
+    private final static SpreadsheetCell VALUE2 = KEY2.setFormula(
+            SpreadsheetFormula.EMPTY.setText("=2")
+    );
+
+    private final static Map<SpreadsheetCellReference, SpreadsheetCell> MAP = Maps.of(
+            KEY1,
+            VALUE1,
+            KEY2,
+            VALUE2
+    );
+
+    @Test
+    public void testWithNullMapFails() {
+        assertThrows(
+                NullPointerException.class,
+                () -> SpreadsheetCellReferenceToSpreadsheetCellMap.with(null)
+        );
+    }
+
+    @Test
+    public void testWithIncludesNullSpreadsheetCellFails() {
+        final Map<SpreadsheetCellReference, SpreadsheetCell> map = Maps.sorted(SpreadsheetSelection.IGNORES_REFERENCE_KIND_COMPARATOR);
+        map.put(KEY1, VALUE1);
+        map.put(KEY2, null);
+
+        assertThrows(
+                NullPointerException.class,
+                () -> SpreadsheetCellReferenceToSpreadsheetCellMap.with(map)
+        );
+    }
+
+    @Test
+    public void testWithSpreadsheetCellReferenceToSpreadsheetCellMapDoesntWrap() {
+        final SpreadsheetCellReferenceToSpreadsheetCellMap map = this.createMap();
+
+        assertSame(
+                map,
+                SpreadsheetCellReferenceToSpreadsheetCellMap.with(map)
+        );
+    }
+
+    @Test
+    public void testWithMap() {
+        final SpreadsheetCellReferenceToSpreadsheetCellMap map = this.createMap();
+
+        this.checkEquals(
+                MAP,
+                map
+        );
+    }
+
+    @Test
+    public void testGetWithNonSpreadsheetCellReferenceFails() {
+        this.getAndCheckAbsent(
+                KEY1.toString()
+        );
+    }
+
+    @Test
+    public void testGetWithUnknownSpreadsheetCellReferenceFails() {
+        this.getAndCheckAbsent(
+                SpreadsheetSelection.parseCell("Z99")
+        );
+    }
+
+    @Test
+    public void testSize() {
+        this.sizeAndCheck(
+                this.createMap(),
+                MAP.size()
+        );
+    }
+
+    @Test
+    public void testGet() {
+        this.getAndCheck(
+                KEY1,
+                VALUE1
+        );
+    }
+
+    @Test
+    public void testGetDifferentSpreadsheetCellReferenceKind() {
+        final SpreadsheetCellReference reference = KEY1.toAbsolute();
+        this.checkNotEquals(
+                KEY1,
+                reference
+        );
+
+        this.getAndCheck(
+                reference,
+                VALUE1
+        );
+    }
+
+    @Test
+    public void testPutFails() {
+        this.putFails(
+                this.createMap(),
+                KEY1,
+                VALUE1
+        );
+    }
+
+    @Test
+    public void testRemoveFails() {
+        this.removeFails(
+                this.createMap(),
+                KEY1
+        );
+    }
+
+    @Test
+    public void testIteratorRemoveFails() {
+        final Iterator<Map.Entry<SpreadsheetCellReference, SpreadsheetCell>> iterator = this.createMap()
+                .entrySet()
+                .iterator();
+        iterator.next();
+
+        assertThrows(
+                UnsupportedOperationException.class,
+                () -> iterator.remove()
+        );
+    }
+
+    @Override
+    public SpreadsheetCellReferenceToSpreadsheetCellMap createMap() {
+        return SpreadsheetCellReferenceToSpreadsheetCellMap.with(MAP);
+    }
+
+    // toString.........................................................................................................
+
+    @Test
+    public void testToString() {
+        this.toStringAndCheck(
+                this.createMap(),
+                MAP.toString()
+        );
+    }
+
+    // class............................................................................................................
+
+    @Override
+    public Class<SpreadsheetCellReferenceToSpreadsheetCellMap> type() {
+        return SpreadsheetCellReferenceToSpreadsheetCellMap.class;
+    }
+
+    @Override
+    public JavaVisibility typeVisibility() {
+        return JavaVisibility.PUBLIC;
+    }
+}
