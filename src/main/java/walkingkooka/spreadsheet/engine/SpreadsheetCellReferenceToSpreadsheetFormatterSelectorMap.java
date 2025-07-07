540bed8335e3ca53501916467657f27e88eba849
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2025-07-07 17:07:25 +1000

    SpreadsheetCellReferenceToSpreadsheetFormatterSelectorMap

---
+++ src/main/java/walkingkooka/spreadsheet/engine/SpreadsheetCellReferenceToSpreadsheetFormatterSelectorMap.java
@@ -0,0 +1,97 @@
+/*
+ * Copyright 2019 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.engine;
+
+import walkingkooka.collect.map.Maps;
+import walkingkooka.spreadsheet.format.SpreadsheetFormatterSelector;
+import walkingkooka.spreadsheet.reference.SpreadsheetCellReference;
+import walkingkooka.spreadsheet.reference.SpreadsheetSelection;
+import walkingkooka.tree.json.JsonNode;
+import walkingkooka.tree.json.marshall.JsonNodeContext;
+import walkingkooka.tree.json.marshall.JsonNodeMarshallContext;
+import walkingkooka.tree.json.marshall.JsonNodeUnmarshallContext;
+
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+
+/**
+ * An immutable {@link Map} with {@link SpreadsheetCellReference} keys and {@link Optional} {@link SpreadsheetFormatterSelector >} values.
+ * Its primary utility is for marshalling/unmarshalling to JSON.
+ */
+public final class SpreadsheetCellReferenceToSpreadsheetFormatterSelectorMap extends SpreadsheetCellReferenceToMap<Optional<SpreadsheetFormatterSelector>> {
+
+    public static SpreadsheetCellReferenceToSpreadsheetFormatterSelectorMap with(final Map<SpreadsheetCellReference, Optional<SpreadsheetFormatterSelector>> cellReferenceToSpreadsheetFormatterSelector) {
+        return cellReferenceToSpreadsheetFormatterSelector instanceof SpreadsheetCellReferenceToSpreadsheetFormatterSelectorMap ?
+            (SpreadsheetCellReferenceToSpreadsheetFormatterSelectorMap) cellReferenceToSpreadsheetFormatterSelector :
+            new SpreadsheetCellReferenceToSpreadsheetFormatterSelectorMap(
+                copy(cellReferenceToSpreadsheetFormatterSelector)
+            );
+    }
+
+    private static Map<SpreadsheetCellReference, Optional<SpreadsheetFormatterSelector>> copy(final Map<SpreadsheetCellReference, Optional<SpreadsheetFormatterSelector>> cellReferenceToSpreadsheetFormatterSelector) {
+        Objects.requireNonNull(cellReferenceToSpreadsheetFormatterSelector, "cellReferenceToSpreadsheetFormatterSelector");
+
+        final Map<SpreadsheetCellReference, Optional<SpreadsheetFormatterSelector>> copy = Maps.sorted(SpreadsheetSelection.IGNORES_REFERENCE_KIND_COMPARATOR);
+        for (final Entry<SpreadsheetCellReference, Optional<SpreadsheetFormatterSelector>> referenceAndCell : cellReferenceToSpreadsheetFormatterSelector.entrySet()) {
+            copy.put(
+                referenceAndCell.getKey(),
+                Objects.requireNonNull(
+                    referenceAndCell.getValue(),
+                    "null SpreadsheetFormatterSelector included"
+                )
+            );
+        }
+        return copy;
+    }
+
+    private SpreadsheetCellReferenceToSpreadsheetFormatterSelectorMap(final Map<SpreadsheetCellReference, Optional<SpreadsheetFormatterSelector>> cellReferenceToSpreadsheetFormatterSelector) {
+        super(cellReferenceToSpreadsheetFormatterSelector);
+    }
+
+    @Override
+    JsonNode marshallValue(final Optional<SpreadsheetFormatterSelector> value,
+                           final JsonNodeMarshallContext context) {
+        return context.marshallOptional(value);
+    }
+
+    // Json.............................................................................................................
+
+    static SpreadsheetCellReferenceToSpreadsheetFormatterSelectorMap unmarshall(final JsonNode node,
+                                                                                final JsonNodeUnmarshallContext context) {
+        return new SpreadsheetCellReferenceToSpreadsheetFormatterSelectorMap(
+            unmarshallMap(
+                node,
+                (JsonNode value) -> context.unmarshallOptional(
+                    value,
+                    SpreadsheetFormatterSelector.class
+                ),
+                context
+            )
+        );
+    }
+
+    static {
+        JsonNodeContext.register(
+            JsonNodeContext.computeTypeName(SpreadsheetCellReferenceToSpreadsheetFormatterSelectorMap.class),
+            SpreadsheetCellReferenceToSpreadsheetFormatterSelectorMap::unmarshall,
+            SpreadsheetCellReferenceToSpreadsheetFormatterSelectorMap::marshall,
+            SpreadsheetCellReferenceToSpreadsheetFormatterSelectorMap.class
+        );
+    }
+}
