e0cea21d7d642cd5319b96c5badea11a0d4bc9b4
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2025-07-25 11:42:41 +1000

    SpreadsheetRowReferenceOrRange formatting

--- src/main/java/walkingkooka/spreadsheet/reference/SpreadsheetRowReferenceOrRange.java
+++ src/main/java/walkingkooka/spreadsheet/reference/SpreadsheetRowReferenceOrRange.java
@@ -32,7 +32,8 @@ public abstract class SpreadsheetRowReferenceOrRange extends SpreadsheetColumnOr
         }
     }
 
-    @Override final boolean testColumnNonNull(final SpreadsheetColumnReference column) {
+    @Override //
+    final boolean testColumnNonNull(final SpreadsheetColumnReference column) {
         return false;
     }
 
