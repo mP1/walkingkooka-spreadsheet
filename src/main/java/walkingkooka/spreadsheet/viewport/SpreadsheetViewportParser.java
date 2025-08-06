69ad1cede612890b7a2de6452e65b59f37953028
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2025-08-06 17:21:42 +1000

    SpreadsheetViewportParser extracted

---
+++ src/main/java/walkingkooka/spreadsheet/viewport/SpreadsheetViewportParser.java
@@ -0,0 +1,176 @@
+/*
+ * Copyright 2019 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.viewport;
+
+import walkingkooka.datetime.DateTimeContexts;
+import walkingkooka.math.DecimalNumberContexts;
+import walkingkooka.net.UrlFragment;
+import walkingkooka.spreadsheet.formula.SpreadsheetFormulaParsers;
+import walkingkooka.spreadsheet.formula.parser.CellSpreadsheetFormulaParserToken;
+import walkingkooka.spreadsheet.parser.SpreadsheetParserContext;
+import walkingkooka.spreadsheet.parser.SpreadsheetParserContexts;
+import walkingkooka.spreadsheet.reference.SpreadsheetCellReference;
+import walkingkooka.text.CaseSensitivity;
+import walkingkooka.text.cursor.TextCursor;
+import walkingkooka.text.cursor.parser.DoubleParserToken;
+import walkingkooka.text.cursor.parser.InvalidCharacterExceptionFactory;
+import walkingkooka.text.cursor.parser.Parser;
+import walkingkooka.text.cursor.parser.ParserReporters;
+import walkingkooka.text.cursor.parser.Parsers;
+import walkingkooka.tree.expression.ExpressionNumberContexts;
+import walkingkooka.tree.expression.ExpressionNumberKind;
+
+import java.math.MathContext;
+
+/**
+ * A {@link Parser} that includes helpers to expect and parse individual tokens.
+ */
+final class SpreadsheetViewportParser {
+
+    static SpreadsheetViewportParser with(final TextCursor cursor) {
+        return new SpreadsheetViewportParser(cursor);
+    }
+
+    private SpreadsheetViewportParser(final TextCursor cursor) {
+        this.cursor = cursor;
+    }
+
+    /**
+     * Parses a {@link SpreadsheetViewportRectangle} but does not complain about extra components.
+     */
+    // /home/A1/width/200/height/300
+    SpreadsheetViewportRectangle parseSpreadsheetViewportRectangle() {
+        this.slash();
+        this.homeToken();
+        this.slash();
+        final SpreadsheetCellReference home = this.homeCellReference();
+        
+        this.slash();
+        this.widthToken();
+        this.slash();
+        final double width = this.width();
+
+        this.slash();
+        this.heightToken();
+        this.slash();
+        final double height = this.height();
+
+        return SpreadsheetViewportRectangle.with(
+            home,
+            width,
+            height
+        );
+    }
+    
+    // helpers...........................................................................................................
+
+    void slash() {
+        SLASH_PARSER.parse(cursor, PARSER_CONTEXT);
+    }
+
+    private final static Parser<SpreadsheetParserContext> SLASH_PARSER = Parsers.string("/", CaseSensitivity.SENSITIVE)
+        .orReport(ParserReporters.basic())
+        .cast();
+
+    void homeToken() {
+        this.parseTokenOrFail(
+            HOME_TOKEN_PARSER,
+            HOME_TOKEN
+        );
+    }
+
+    void widthToken() {
+        this.parseTokenOrFail(
+            WIDTH_TOKEN_PARSER,
+            WIDTH_TOKEN
+        );
+    }
+
+    void heightToken() {
+        this.parseTokenOrFail(
+            HEIGHT_TOKEN_PARSER,
+            HEIGHT_TOKEN
+        );
+    }
+
+    private final static String HOME_TOKEN = "home";
+    private final static Parser<SpreadsheetParserContext> HOME_TOKEN_PARSER = Parsers.string(HOME_TOKEN, CaseSensitivity.SENSITIVE);
+
+    private final static String WIDTH_TOKEN = "width";
+    private final static Parser<SpreadsheetParserContext> WIDTH_TOKEN_PARSER = Parsers.string(WIDTH_TOKEN, CaseSensitivity.SENSITIVE);
+
+    private final static String HEIGHT_TOKEN = "height";
+    private final static Parser<SpreadsheetParserContext> HEIGHT_TOKEN_PARSER = Parsers.string(HEIGHT_TOKEN, CaseSensitivity.SENSITIVE);
+
+    private void parseTokenOrFail(final Parser<SpreadsheetParserContext> parser,
+                                  final String label) {
+        if(false == parser.parse(this.cursor, PARSER_CONTEXT).isPresent()) {
+            throw new IllegalArgumentException("Missing " + label);
+        }
+    }
+
+    SpreadsheetCellReference homeCellReference() {
+        return SpreadsheetFormulaParsers.cell()
+            .parse(
+                cursor,
+                PARSER_CONTEXT
+            ).orElseThrow(() -> new IllegalArgumentException("Missing home"))
+            .cast(CellSpreadsheetFormulaParserToken.class)
+            .cell();
+    }
+
+    double width() {
+        return this.parseDoubleOrFail(WIDTH_TOKEN);
+    }
+
+    double height() {
+        return this.parseDoubleOrFail(HEIGHT_TOKEN);
+    }
+
+    /**
+     * Used to parse the width or height values within a {@link UrlFragment}.
+     */
+    private double parseDoubleOrFail(final String label) {
+        return Parsers.doubleParser()
+            .parse(
+                this.cursor,
+                PARSER_CONTEXT
+            ).orElseThrow(() -> new IllegalArgumentException("Missing " + label))
+            .cast(DoubleParserToken.class)
+            .value();
+    }
+
+    private final static SpreadsheetParserContext PARSER_CONTEXT = SpreadsheetParserContexts.basic(
+        InvalidCharacterExceptionFactory.POSITION_EXPECTED,
+        DateTimeContexts.fake(),
+        ExpressionNumberContexts.basic(
+            ExpressionNumberKind.BIG_DECIMAL,
+            DecimalNumberContexts.american(MathContext.DECIMAL32)
+        ),
+        ';' // not actually used/
+    );
+
+    final TextCursor cursor;
+
+    // Object...........................................................................................................
+
+    @Override
+    public String toString() {
+        return this.cursor.toString();
+    }
+}
--- src/test/java/walkingkooka/spreadsheet/viewport/SpreadsheetViewportRectangleTest.java
+++ src/test/java/walkingkooka/spreadsheet/viewport/SpreadsheetViewportRectangleTest.java
@@ -414,7 +414,7 @@ public final class SpreadsheetViewportRectangleTest implements ClassTesting2<Spr
     public void testFromUrlFragmentWithEmptyFails() {
         this.fromUrlFragmentFails(
             "",
-            "Missing home"
+            "End of text at (1,1) expected \"/\""
         );
     }
 
@@ -446,7 +446,7 @@ public final class SpreadsheetViewportRectangleTest implements ClassTesting2<Spr
     public void testFromUrlFragmentWithOnlyHomeFails() {
         this.fromUrlFragmentFails(
             "/home/A1",
-            "Missing width"
+            "End of text at (9,1) expected \"/\""
         );
     }
 
@@ -478,7 +478,7 @@ public final class SpreadsheetViewportRectangleTest implements ClassTesting2<Spr
     public void testFromUrlFragmentWithOnlyHomeAndWidthFails() {
         this.fromUrlFragmentFails(
             "/home/A1/width/200",
-            "Missing height"
+            "End of text at (19,1) expected \"/\""
         );
     }
 
