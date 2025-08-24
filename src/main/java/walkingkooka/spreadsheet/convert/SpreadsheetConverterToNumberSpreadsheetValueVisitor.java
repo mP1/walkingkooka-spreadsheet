bfac9ca23c958928ce0a1f4cbd360f12fc0e6ef6
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2025-08-24 18:17:32 +1000

    SpreadsheetConverterToNumber
    - handles converting boolean, date, datetime and string to number.

---
+++ src/main/java/walkingkooka/spreadsheet/convert/SpreadsheetConverterToNumberSpreadsheetValueVisitor.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2019 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.convert;
+
+import walkingkooka.convert.Converter;
+import walkingkooka.convert.Converters;
+import walkingkooka.spreadsheet.SpreadsheetValueVisitor;
+import walkingkooka.tree.expression.convert.ExpressionNumberConverters;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+/**
+ * A {@link SpreadsheetValueVisitor} that converts a value into a {@link walkingkooka.tree.expression.ExpressionNumber}.
+ */
+final class SpreadsheetConverterToNumberSpreadsheetValueVisitor extends SpreadsheetValueVisitor {
+
+    static Converter<SpreadsheetConverterContext> converter(final Object value) {
+        final SpreadsheetConverterToNumberSpreadsheetValueVisitor visitor = new SpreadsheetConverterToNumberSpreadsheetValueVisitor();
+        visitor.accept(value);
+        return visitor.converter;
+    }
+
+    // @VisibleForTesting
+    SpreadsheetConverterToNumberSpreadsheetValueVisitor() {
+        super();
+    }
+
+    @Override
+    protected void visit(final Boolean value) {
+        this.converter = BOOLEAN;
+    }
+
+    private final static Converter<SpreadsheetConverterContext> BOOLEAN = ExpressionNumberConverters.toNumberOrExpressionNumber(Converters.booleanToNumber());
+
+    @Override
+    protected void visit(final LocalDate value) {
+        this.converter = DATE;
+    }
+
+    private final static Converter<SpreadsheetConverterContext> DATE = ExpressionNumberConverters.toNumberOrExpressionNumber(Converters.localDateToNumber());
+
+    @Override
+    protected void visit(final LocalDateTime value) {
+        this.converter = DATE_TIME;
+    }
+
+    private final static Converter<SpreadsheetConverterContext> DATE_TIME = ExpressionNumberConverters.toNumberOrExpressionNumber(Converters.localDateTimeToNumber());
+
+    @Override
+    protected void visit(final String value) {
+        this.converter = STRING;
+    }
+
+    private final static Converter<SpreadsheetConverterContext> STRING = SpreadsheetConverterToNumberSpreadsheetValueVisitorStringConverter.INSTANCE;
+
+    private Converter<SpreadsheetConverterContext> converter;
+
+    // Object...........................................................................................................
+
+    @Override
+    public String toString() {
+        return String.valueOf(this.converter);
+    }
+}
---
+++ src/main/java/walkingkooka/spreadsheet/convert/SpreadsheetConverterToNumberSpreadsheetValueVisitorStringConverter.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright 2019 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.convert;
+
+import walkingkooka.Either;
+import walkingkooka.convert.Converter;
+import walkingkooka.convert.Converters;
+import walkingkooka.datetime.DateTimeContexts;
+import walkingkooka.math.DecimalNumberContexts;
+import walkingkooka.text.cursor.parser.BigDecimalParserToken;
+import walkingkooka.text.cursor.parser.InvalidCharacterExceptionFactory;
+import walkingkooka.text.cursor.parser.Parser;
+import walkingkooka.text.cursor.parser.ParserContexts;
+import walkingkooka.text.cursor.parser.ParserToken;
+import walkingkooka.text.cursor.parser.Parsers;
+
+import java.math.BigDecimal;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+/**
+ * A {@link Converter} that inserts a {@link BigDecimal} as the type to {@link Converters#parser(Class, Parser, Function, BiFunction)}.
+ */
+final class SpreadsheetConverterToNumberSpreadsheetValueVisitorStringConverter extends SpreadsheetConverter {
+
+    /**
+     * Singleton
+     */
+    final static SpreadsheetConverterToNumberSpreadsheetValueVisitorStringConverter INSTANCE = new SpreadsheetConverterToNumberSpreadsheetValueVisitorStringConverter();
+
+    private SpreadsheetConverterToNumberSpreadsheetValueVisitorStringConverter() {
+        super();
+    }
+
+    @Override
+    public boolean canConvert(final Object value,
+                              final Class<?> type,
+                              final SpreadsheetConverterContext context) {
+        return true; // always true because assumes SpreadsheetConverterToNumber#canConvert
+    }
+
+    @Override
+    public <T> Either<T, String> doConvert(final Object value,
+                                           final Class<T> type,
+                                           final SpreadsheetConverterContext context) {
+        final Either<BigDecimal, String> parsed = BIG_DECIMAL_PARSER.convert(
+            value,
+            BigDecimal.class, // wont accept other Number types
+            context
+        );
+
+        final Either<T, String> result;
+
+        if(parsed.isLeft()) {
+            result = context.convert(
+                parsed.leftValue(),
+                type
+            );
+
+        } else {
+            result = this.failConversion(
+                value,
+                type
+            );
+        }
+
+        return result;
+    }
+
+    private final static Converter<SpreadsheetConverterContext> BIG_DECIMAL_PARSER = Converters.parser(
+        BigDecimal.class,
+        Parsers.bigDecimal(), // parser
+        (final SpreadsheetConverterContext c) -> ParserContexts.basic(
+            InvalidCharacterExceptionFactory.POSITION,
+            DateTimeContexts.fake(),
+            DecimalNumberContexts.american(c.mathContext())
+        ),
+        (ParserToken t, SpreadsheetConverterContext c) -> t.cast(BigDecimalParserToken.class).value()
+    );
+
+    // Object...........................................................................................................
+
+    @Override
+    public String toString() {
+        return "String to Number";
+    }
+}
