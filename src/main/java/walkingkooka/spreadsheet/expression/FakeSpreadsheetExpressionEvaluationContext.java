6d15f9ac54b6bbecce89822691b79ab25bfc8dbf
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2025-12-12 15:02:42 +1100

    https://github.com/mP1/walkingkooka-terminal/pull/106 TerminalContext.terminalExpressionEvaluationContext()
    - https://github.com/mP1/walkingkooka-terminal/pull/106
    - TerminalContext.terminalExpressionEvaluationContext()

--- src/main/java/walkingkooka/spreadsheet/expression/FakeSpreadsheetExpressionEvaluationContext.java
+++ src/main/java/walkingkooka/spreadsheet/expression/FakeSpreadsheetExpressionEvaluationContext.java
@@ -43,6 +43,7 @@ import walkingkooka.spreadsheet.validation.SpreadsheetValidatorContext;
 import walkingkooka.storage.Storage;
 import walkingkooka.storage.expression.function.StorageExpressionEvaluationContext;
 import walkingkooka.terminal.TerminalId;
+import walkingkooka.terminal.expression.TerminalExpressionEvaluationContext;
 import walkingkooka.text.LineEnding;
 import walkingkooka.text.cursor.TextCursor;
 import walkingkooka.text.printer.Printer;
@@ -420,6 +421,11 @@ public class FakeSpreadsheetExpressionEvaluationContext extends FakeExpressionEv
 
     @Override
     public Printer error() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public TerminalExpressionEvaluationContext terminalExpressionEvaluationContext() {
         throw new UnsupportedOperationException();
     }
 }
