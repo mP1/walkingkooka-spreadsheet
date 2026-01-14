dcdfc80e1190bd5601b3b7d117809ff369fc8dcc
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2026-01-14 18:16:33 +1100

    Revert "SpreadsheetStorageContext.xxxCells"
    This reverts (previous) commit f57787d9832d841f4354b8d0fce6cf1f2bea39ce.

--- src/main/java/walkingkooka/spreadsheet/storage/SpreadsheetStorageContextTesting.java
+++ src/main/java/walkingkooka/spreadsheet/storage/SpreadsheetStorageContextTesting.java
@@ -17,86 +17,13 @@
 
 package walkingkooka.spreadsheet.storage;
 
-import org.junit.jupiter.api.Test;
-import walkingkooka.collect.set.Sets;
 import walkingkooka.convert.ConverterLikeTesting;
 import walkingkooka.spreadsheet.environment.SpreadsheetEnvironmentContextTesting2;
-import walkingkooka.spreadsheet.reference.SpreadsheetExpressionReference;
-import walkingkooka.spreadsheet.value.SpreadsheetCell;
 import walkingkooka.storage.StorageContextTesting;
-
-import java.util.Set;
-
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public interface SpreadsheetStorageContextTesting<C extends SpreadsheetStorageContext> extends StorageContextTesting<C>,
     SpreadsheetEnvironmentContextTesting2<C>,
     ConverterLikeTesting<C> {
-
-    // loadCells........................................................................................................
-
-    @Test
-    default void testLoadCellsWithNullCellOrLabelsFails() {
-        assertThrows(
-            NullPointerException.class,
-            () -> this.createContext()
-                .loadCells(null)
-        );
-    }
-
-    default void loadCellsAndCheck(final C context,
-                                   final SpreadsheetExpressionReference cellsOrLabel,
-                                   final SpreadsheetCell... expected) {
-        this.loadCellsAndCheck(
-            context,
-            cellsOrLabel,
-            Sets.of(expected)
-        );
-    }
-
-    default void loadCellsAndCheck(final C context,
-                                   final SpreadsheetExpressionReference cellsOrLabel,
-                                   final Set<SpreadsheetCell> expected) {
-        this.checkEquals(
-            expected,
-            context.loadCells(cellsOrLabel),
-            () -> "loadCells " + cellsOrLabel
-        );
-    }
-
-    // saveCells........................................................................................................
-
-    @Test
-    default void testSaveCellsWithNullCellOrLabelsFails() {
-        assertThrows(
-            NullPointerException.class,
-            () -> this.createContext()
-                .saveCells(null)
-        );
-    }
-
-    default void saveCellsAndCheck(final C context,
-                                   final Set<SpreadsheetCell> cells,
-                                   final SpreadsheetCell... expected) {
-        this.checkEquals(
-            expected,
-            context.saveCells(cells),
-            () -> "saveCells " + cells
-        );
-    }
-
-    // deleteCells......................................................................................................
-
-    @Test
-    default void testDeleteCellsWithNullCellOrLabelsFails() {
-        assertThrows(
-            NullPointerException.class,
-            () -> this.createContext()
-                .deleteCells(null)
-        );
-    }
-    
-    // ConverterLike....................................................................................................
 
     @Override
     default C createConverterLike() {
