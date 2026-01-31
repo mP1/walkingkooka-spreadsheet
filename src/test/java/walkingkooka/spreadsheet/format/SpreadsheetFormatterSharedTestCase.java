452f1eda8065af7ccdc30da26dbb21e2249c219b
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2026-01-31 12:09:05 +1100

    SpreadsheetFormatterShared

---
+++ src/test/java/walkingkooka/spreadsheet/format/SpreadsheetFormatterSharedTestCase.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2019 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.format;
+
+import walkingkooka.ToStringTesting;
+import walkingkooka.reflect.ClassTesting2;
+import walkingkooka.reflect.JavaVisibility;
+import walkingkooka.spreadsheet.format.provider.SpreadsheetFormatterSelector;
+
+public abstract class SpreadsheetFormatterSharedTestCase<F extends SpreadsheetFormatterShared> implements SpreadsheetFormatterTesting2<F>,
+    ToStringTesting<F>,
+    ClassTesting2<F> {
+
+    SpreadsheetFormatterSharedTestCase() {
+        super();
+    }
+
+    // class............................................................................................................
+
+    @Override
+    public final String typeNamePrefix() {
+        return SpreadsheetFormatterSelector.class.getSimpleName();
+    }
+
+    @Override
+    public final String typeNameSuffix() {
+        return "";
+    }
+
+    @Override
+    public final JavaVisibility typeVisibility() {
+        return JavaVisibility.PACKAGE_PRIVATE;
+    }
+}
