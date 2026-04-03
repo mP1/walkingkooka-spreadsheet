916308be7385ab467cbbb1b802856e1f9099c125
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2026-04-03 14:03:57 +1100

    SpreadsheetStorageForm
    - Closes https://github.com/mP1/walkingkooka-spreadsheet/issues/8974
    - SpreadsheetStorageForm

---
+++ src/main/java/walkingkooka/spreadsheet/storage/SpreadsheetStorageForm.java
@@ -0,0 +1,196 @@
+
+/*
+ * Copyright 2025 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.storage;
+
+import walkingkooka.Cast;
+import walkingkooka.collect.list.ImmutableList;
+import walkingkooka.net.header.MediaType;
+import walkingkooka.spreadsheet.net.SpreadsheetMediaTypes;
+import walkingkooka.spreadsheet.validation.SpreadsheetValidationReference;
+import walkingkooka.storage.Storage;
+import walkingkooka.storage.StorageName;
+import walkingkooka.storage.StoragePath;
+import walkingkooka.storage.StorageValue;
+import walkingkooka.storage.StorageValueInfo;
+import walkingkooka.validation.form.Form;
+import walkingkooka.validation.form.FormName;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * A {@link Storage} that maps {@link Form} to paths, assuming the context handles the parent {@link walkingkooka.spreadsheet.meta.SpreadsheetId}.
+ * <pre>
+ * /form/{@link FormName}
+ * </pre>
+ * for the {@link StorageValue}.
+ */
+final class SpreadsheetStorageForm extends SpreadsheetStorage {
+
+    /**
+     * Singleton
+     */
+    final static SpreadsheetStorageForm INSTANCE = new SpreadsheetStorageForm();
+
+    private final static MediaType MEDIA_TYPE = SpreadsheetMediaTypes.MEMORY_FORM;
+
+    private SpreadsheetStorageForm() {
+        super();
+    }
+
+    @Override
+    Optional<StorageValue> loadNonNull(final StoragePath path,
+                                       final SpreadsheetStorageContext context) {
+        final List<StorageName> names = path.namesList();
+
+        final FormName formName;
+
+        switch (names.size()) {
+            case 0:
+            case 1:
+                formName = null;
+                break;
+            case 2:
+                formName = parseFormName(
+                    names.get(1)
+                );
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid extra path after FormName");
+        }
+
+        StorageValue value = null;
+
+        if (null != formName) {
+            final Optional<Form<SpreadsheetValidationReference>> form = context.loadForm(formName);
+            if (false == form.isEmpty()) {
+                value = StorageValue.with(
+                    path,
+                    Cast.to(form)
+                ).setContentType(MEDIA_TYPE);
+            }
+        }
+
+        return Optional.ofNullable(value);
+    }
+
+    @Override
+    StorageValue saveNonNull(final StorageValue value,
+                             final SpreadsheetStorageContext context) {
+        switch (value.path()
+            .namesList()
+            .size()) {
+            case 0:
+            case 1:
+            case 2:
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid path after FormName");
+        }
+
+        final Form<SpreadsheetValidationReference> form = context.convertOrFail(
+            value.value()
+                .orElseThrow(() -> new IllegalArgumentException("Missing Form")),
+            Form.class
+        );
+
+        return value.setValue(
+            Optional.of(
+                context.saveForm(form)
+            )
+        ).setContentType(MEDIA_TYPE);
+    }
+
+    /**
+     * Deletes the given forms. Note if the path contains additional path components a {@link IllegalArgumentException}
+     * will be thrown.
+     */
+    @Override
+    void deleteNonNull(final StoragePath path,
+                       final SpreadsheetStorageContext context) {
+        final List<StorageName> storageNames = path.namesList();
+        switch (storageNames.size()) {
+            case 0:
+            case 1:
+                throw new IllegalArgumentException("Missing FormName");
+            case 2:
+                context.deleteForm(
+                    parseFormName(
+                        storageNames.get(1)
+                    )
+                );
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid extra path after FormName");
+        }
+    }
+
+    @Override
+    List<StorageValueInfo> listNonNull(final StoragePath path,
+                                       final int offset,
+                                       final int count,
+                                       final SpreadsheetStorageContext context) {
+        final List<StorageName> storageNames = path.namesList();
+
+        final String formName;
+
+        switch (storageNames.size()) {
+            case 0:
+            case 1:
+                formName = "";
+                break;
+            case 2:
+                formName = storageNames.get(1)
+                    .value();
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid path after form");
+        }
+
+        return context.findFormsByName(
+                formName,
+                offset,
+                count
+            ).stream()
+            .map(
+                (Form<SpreadsheetValidationReference> f) -> StorageValueInfo.with(
+                    StoragePath.ROOT.append(
+                        StorageName.with(
+                            f.name()
+                                .text()
+                        )
+                    ),
+                    context.createdAuditInfo()
+                )
+            ).collect(ImmutableList.collector());
+    }
+
+    private static FormName parseFormName(final StorageName name) {
+        return FormName.with(
+            name.withoutFileExtension()
+        );
+    }
+
+    // Object...........................................................................................................
+
+    @Override
+    public String toString() {
+        return SpreadsheetStorageForm.class.getSimpleName();
+    }
+}
---
+++ src/test/java/walkingkooka/spreadsheet/storage/SpreadsheetStorageFormTest.java
@@ -0,0 +1,494 @@
+
+/*
+ * Copyright 2025 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.storage;
+
+import org.junit.jupiter.api.Test;
+import walkingkooka.convert.Converters;
+import walkingkooka.currency.CurrencyContexts;
+import walkingkooka.environment.AuditInfo;
+import walkingkooka.environment.EnvironmentContext;
+import walkingkooka.environment.EnvironmentContexts;
+import walkingkooka.locale.LocaleContext;
+import walkingkooka.locale.LocaleContexts;
+import walkingkooka.net.Url;
+import walkingkooka.net.email.EmailAddress;
+import walkingkooka.plugin.ProviderContext;
+import walkingkooka.plugin.ProviderContexts;
+import walkingkooka.spreadsheet.SpreadsheetContext;
+import walkingkooka.spreadsheet.SpreadsheetContexts;
+import walkingkooka.spreadsheet.compare.provider.SpreadsheetComparatorProviders;
+import walkingkooka.spreadsheet.convert.provider.SpreadsheetConvertersConverterProviders;
+import walkingkooka.spreadsheet.engine.SpreadsheetEngines;
+import walkingkooka.spreadsheet.environment.SpreadsheetEnvironmentContext;
+import walkingkooka.spreadsheet.environment.SpreadsheetEnvironmentContexts;
+import walkingkooka.spreadsheet.export.provider.SpreadsheetExporterProviders;
+import walkingkooka.spreadsheet.expression.SpreadsheetExpressionFunctions;
+import walkingkooka.spreadsheet.format.provider.SpreadsheetFormatterProviders;
+import walkingkooka.spreadsheet.importer.provider.SpreadsheetImporterProviders;
+import walkingkooka.spreadsheet.meta.SpreadsheetId;
+import walkingkooka.spreadsheet.meta.SpreadsheetMetadata;
+import walkingkooka.spreadsheet.meta.SpreadsheetMetadataPropertyName;
+import walkingkooka.spreadsheet.meta.store.SpreadsheetMetadataStore;
+import walkingkooka.spreadsheet.meta.store.SpreadsheetMetadataStores;
+import walkingkooka.spreadsheet.net.SpreadsheetMediaTypes;
+import walkingkooka.spreadsheet.parser.provider.SpreadsheetParserProviders;
+import walkingkooka.spreadsheet.provider.SpreadsheetProviders;
+import walkingkooka.spreadsheet.store.repo.SpreadsheetStoreRepositories;
+import walkingkooka.spreadsheet.validation.SpreadsheetValidationReference;
+import walkingkooka.spreadsheet.validation.form.SpreadsheetForms;
+import walkingkooka.storage.Storage;
+import walkingkooka.storage.StoragePath;
+import walkingkooka.storage.StorageValue;
+import walkingkooka.storage.StorageValueInfo;
+import walkingkooka.storage.Storages;
+import walkingkooka.text.Indentation;
+import walkingkooka.text.LineEnding;
+import walkingkooka.tree.expression.function.provider.ExpressionFunctionProviders;
+import walkingkooka.validation.form.Form;
+import walkingkooka.validation.form.FormName;
+import walkingkooka.validation.form.provider.FormHandlerProviders;
+import walkingkooka.validation.provider.ValidatorProviders;
+
+import java.time.LocalDateTime;
+import java.util.Currency;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public final class SpreadsheetStorageFormTest extends SpreadsheetStorageTestCase<SpreadsheetStorageForm> {
+
+    private final static FormName FORM_NAME1 = FormName.with("Form111");
+
+    private final static Form<SpreadsheetValidationReference> FORM1 = SpreadsheetForms.form(FORM_NAME1);
+
+    private final static FormName FORM_NAME2 = FormName.with("Form222");
+
+    private final static Form<SpreadsheetValidationReference> FORM2 = SpreadsheetForms.form(FORM_NAME2);
+
+    private final static FormName FORM_NAME3 = FormName.with("Form333");
+
+    private final static Form<SpreadsheetValidationReference> FORM3 = SpreadsheetForms.form(FORM_NAME3);
+
+    private final static StorageValueInfo INFO1 = StorageValueInfo.with(
+        StoragePath.parse("/" + FORM_NAME1),
+        SPREADSHEET_ENVIRONMENT_CONTEXT.createdAuditInfo()
+    );
+
+    private final static StorageValueInfo INFO2 = StorageValueInfo.with(
+        StoragePath.parse("/" + FORM_NAME2),
+        SPREADSHEET_ENVIRONMENT_CONTEXT.createdAuditInfo()
+    );
+
+    private final static StorageValueInfo INFO3 = StorageValueInfo.with(
+        StoragePath.parse("/" + FORM_NAME3),
+        SPREADSHEET_ENVIRONMENT_CONTEXT.createdAuditInfo()
+    );
+
+    @Test
+    public void testLoadWithExtraPathFails() {
+        final IllegalArgumentException thrown = assertThrows(
+            IllegalArgumentException.class,
+            () -> this.createStorage()
+                .load(
+                    StoragePath.parse("/" + FORM_NAME1 + "/extra"),
+                    this.createContext()
+                )
+        );
+
+        this.checkEquals(
+            "Invalid extra path after FormName",
+            thrown.getMessage()
+        );
+    }
+
+    @Test
+    public void testLoadMissingForm() {
+        final SpreadsheetStorageContext context = this.createContext();
+
+        final StoragePath path = StoragePath.parse("/" + FORM_NAME1);
+
+        this.loadAndCheck(
+            this.createStorage(),
+            path,
+            context
+        );
+    }
+
+    @Test
+    public void testLoad() {
+        final SpreadsheetContext spreadsheetContext = this.createSpreadsheetContext();
+
+        SpreadsheetEngines.basic()
+            .saveForm(
+                FORM1,
+                spreadsheetContext.spreadsheetEngineContext()
+            );
+        final SpreadsheetStorageContext context = this.createContext(spreadsheetContext);
+
+
+        final StoragePath path = StoragePath.parse("/" + FORM_NAME1);
+
+        this.loadAndCheck(
+            this.createStorage(),
+            path,
+            context,
+            StorageValue.with(
+                path,
+                Optional.of(FORM1)
+            ).setContentType(SpreadsheetMediaTypes.MEMORY_FORM)
+        );
+    }
+
+    @Test
+    public void testLoadWIthFileExtension() {
+        final SpreadsheetContext spreadsheetContext = this.createSpreadsheetContext();
+
+        SpreadsheetEngines.basic()
+            .saveForm(
+                FORM1,
+                spreadsheetContext.spreadsheetEngineContext()
+            );
+        final SpreadsheetStorageContext context = this.createContext(spreadsheetContext);
+
+
+        final StoragePath path = StoragePath.parse("/" + FORM_NAME1 + ".json");
+
+        this.loadAndCheck(
+            this.createStorage(),
+            path,
+            context,
+            StorageValue.with(
+                path,
+                Optional.of(FORM1)
+            ).setContentType(SpreadsheetMediaTypes.MEMORY_FORM)
+        );
+    }
+
+    @Test
+    public void testSaveWithExtraPathFails() {
+        final IllegalArgumentException thrown = assertThrows(
+            IllegalArgumentException.class,
+            () -> this.createStorage()
+                .save(
+                    StorageValue.with(
+                        StoragePath.parse("/" + FORM_NAME1 + "/extra"),
+                        Optional.of(FORM1)
+                    ),
+                    this.createContext()
+                )
+        );
+
+        this.checkEquals(
+            "Invalid path after FormName",
+            thrown.getMessage()
+        );
+    }
+
+    @Test
+    public void testSaveWithStorageValueMissingForm() {
+        final IllegalArgumentException thrown = assertThrows(
+            IllegalArgumentException.class,
+            () -> this.createStorage()
+                .save(
+                    StorageValue.with(
+                        StoragePath.parse("/" + FORM_NAME1),
+                        Optional.empty()
+                    ),
+                    this.createContext()
+                )
+        );
+
+        this.checkEquals(
+            "Missing Form",
+            thrown.getMessage()
+        );
+    }
+
+    @Test
+    public void testSave() {
+        final SpreadsheetStorageContext context = this.createContext();
+
+        final StoragePath path = StoragePath.parse("/" + FORM_NAME1);
+
+        this.saveAndCheck(
+            this.createStorage(),
+            StorageValue.with(
+                path,
+                Optional.of(FORM1)
+            ),
+            context,
+            StorageValue.with(
+                path,
+                Optional.of(FORM1)
+            ).setContentType(SpreadsheetMediaTypes.MEMORY_FORM)
+        );
+    }
+
+    @Test
+    public void testDeleteMissingFormFails() {
+        final IllegalArgumentException thrown = assertThrows(
+            IllegalArgumentException.class,
+            () -> this.createStorage()
+                .delete(
+                    StoragePath.ROOT,
+                    this.createContext()
+                )
+        );
+
+        this.checkEquals(
+            "Missing FormName",
+            thrown.getMessage()
+        );
+    }
+
+    @Test
+    public void testDeleteWithExtraPathFails() {
+        final IllegalArgumentException thrown = assertThrows(
+            IllegalArgumentException.class,
+            () -> this.createStorage()
+                .delete(
+                    StoragePath.parse("/" + FORM_NAME1 + "/extra"),
+                    this.createContext()
+                )
+        );
+
+        this.checkEquals(
+            "Invalid extra path after FormName",
+            thrown.getMessage()
+        );
+    }
+
+    @Test
+    public void testDelete() {
+        final SpreadsheetStorageContext context = this.createContext();
+
+        context.saveForm(FORM1);
+
+        final StoragePath path = StoragePath.parse("/" + FORM_NAME1);
+
+        final SpreadsheetStorageForm storage = this.createStorage();
+        storage.delete(
+            path,
+            context
+        );
+
+        this.loadAndCheck(
+            storage,
+            path,
+            context
+        );
+    }
+
+    @Test
+    public void testListWithExtraPathFails() {
+        final IllegalArgumentException thrown = assertThrows(
+            IllegalArgumentException.class,
+            () -> this.createStorage()
+                .list(
+                    StoragePath.parse("/" + FORM_NAME1 + "/extra"),
+                    0,
+                    1,
+                    this.createContext()
+                )
+        );
+
+        this.checkEquals(
+            "Invalid path after form",
+            thrown.getMessage()
+        );
+    }
+
+    @Test
+    public void testListMissingForm() {
+        final SpreadsheetStorageContext context = this.createContext();
+
+        context.saveForm(FORM1);
+        context.saveForm(FORM2);
+        context.saveForm(FORM3);
+
+        this.listAndCheck(
+            this.createStorage(),
+            StoragePath.ROOT,
+            0,
+            4,
+            context,
+            INFO1,
+            INFO2,
+            INFO3
+        );
+    }
+
+    @Test
+    public void testListMissingFormWithOffset() {
+        final SpreadsheetStorageContext context = this.createContext();
+
+        context.saveForm(FORM1);
+        context.saveForm(FORM2);
+        context.saveForm(FORM3);
+
+        this.listAndCheck(
+            this.createStorage(),
+            StoragePath.ROOT,
+            1,
+            4,
+            context,
+            INFO2,
+            INFO3
+        );
+    }
+
+    @Test
+    public void testListMissingFormWithCount() {
+        final SpreadsheetStorageContext context = this.createContext();
+
+        context.saveForm(FORM1);
+        context.saveForm(FORM2);
+        context.saveForm(FORM3);
+
+        this.listAndCheck(
+            this.createStorage(),
+            StoragePath.ROOT,
+            0,
+            2,
+            context,
+            INFO1,
+            INFO2
+        );
+    }
+
+    @Test
+    public void testListWithPrefix() {
+        final SpreadsheetStorageContext context = this.createContext();
+
+        context.saveForm(FORM1);
+        context.saveForm(FORM2);
+        context.saveForm(FORM3);
+
+        this.listAndCheck(
+            this.createStorage(),
+            StoragePath.parse("/Form"),
+            0,
+            2,
+            context,
+            INFO1,
+            INFO2
+        );
+    }
+
+    @Override
+    public SpreadsheetStorageForm createStorage() {
+        return SpreadsheetStorageForm.INSTANCE;
+    }
+
+    @Override
+    public SpreadsheetStorageContext createContext() {
+        return this.createContext(
+            this.createSpreadsheetContext()
+        );
+    }
+
+    private SpreadsheetStorageContext createContext(final SpreadsheetContext spreadsheetContext) {
+        return SpreadsheetStorageContexts.spreadsheetContext(spreadsheetContext);
+    }
+
+    private SpreadsheetContext createSpreadsheetContext() {
+        final SpreadsheetId spreadsheetId = SpreadsheetId.with(1);
+
+        final LocaleContext localeContext = LocaleContexts.jre(LOCALE);
+
+        final SpreadsheetMetadataStore metadataStore = SpreadsheetMetadataStores.treeMap();
+
+        metadataStore.save(
+            SpreadsheetMetadata.EMPTY.set(
+                    SpreadsheetMetadataPropertyName.LOCALE,
+                    LOCALE
+                ).loadFromLocale(
+                    CURRENCY_CONTEXT.setLocaleContext(localeContext)
+                ).set(
+                    SpreadsheetMetadataPropertyName.AUDIT_INFO,
+                    AuditInfo.create(
+                        EmailAddress.parse("user@example.com"),
+                        LocalDateTime.MIN
+                    )
+                ).setDefaults(
+                    SpreadsheetMetadata.NON_LOCALE_DEFAULTS
+                )
+        );
+
+        final EnvironmentContext environmentContext = EnvironmentContexts.map(
+            EnvironmentContexts.empty(
+                Currency.getInstance("AUD"),
+                Indentation.SPACES4,
+                LineEnding.NL,
+                LOCALE,
+                HAS_NOW,
+                Optional.of(
+                    EmailAddress.parse("user@example.com")
+                )
+            )
+        );
+        environmentContext.setEnvironmentValue(
+            SpreadsheetEnvironmentContext.SPREADSHEET_ID,
+            spreadsheetId
+        );
+        environmentContext.setEnvironmentValue(
+            SpreadsheetEnvironmentContext.SERVER_URL,
+            Url.parseAbsolute("https://example.com")
+        );
+
+        final Storage<SpreadsheetStorageContext> storage = Storages.treeMapStore();
+
+        return SpreadsheetContexts.fixedSpreadsheetId(
+            SpreadsheetEngines.basic(),
+            SpreadsheetStoreRepositories.treeMap(metadataStore),
+            (c) -> {
+                throw new UnsupportedOperationException();
+            }, // HttpRouter
+            CurrencyContexts.fake()
+                .setLocaleContext(localeContext),
+            SpreadsheetEnvironmentContexts.basic(
+                storage,
+                environmentContext
+            ),
+            SpreadsheetProviders.basic(
+                SpreadsheetConvertersConverterProviders.spreadsheetConverters(
+                    (ProviderContext p) -> Converters.never()
+                ),
+                ExpressionFunctionProviders.empty(
+                    SpreadsheetExpressionFunctions.NAME_CASE_SENSITIVITY
+                ),
+                SpreadsheetComparatorProviders.empty(),
+                SpreadsheetExporterProviders.empty(),
+                SpreadsheetFormatterProviders.spreadsheetFormatters(),
+                FormHandlerProviders.empty(),
+                SpreadsheetImporterProviders.empty(),
+                SpreadsheetParserProviders.empty(),
+                ValidatorProviders.empty()
+            ),
+            ProviderContexts.fake()
+        );
+    }
+
+    // class............................................................................................................
+
+    @Override
+    public Class<SpreadsheetStorageForm> type() {
+        return SpreadsheetStorageForm.class;
+    }
+}
