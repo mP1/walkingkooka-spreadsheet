74890f53856e58eab166bae7828f28392ece6658
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2026-04-04 12:50:01 +1100

    https://github.com/mP1/walkingkooka-currency/pull/92 CanLocalesForCurrencyCode.localesForCurrencyCode(CurrencyCode) replaces String
    - https://github.com/mP1/walkingkooka-currency/pull/92
    - CanLocalesForCurrencyCode.localesForCurrencyCode(CurrencyCode) replaces String

--- src/main/java/walkingkooka/spreadsheet/engine/FakeSpreadsheetEngineContext.java
+++ src/main/java/walkingkooka/spreadsheet/engine/FakeSpreadsheetEngineContext.java
@@ -237,7 +237,7 @@ public class FakeSpreadsheetEngineContext extends FakeSpreadsheetProvider implem
     }
 
     @Override
-    public Set<Locale> localesForCurrencyCode(final String currencyCode) {
+    public Set<Locale> localesForCurrencyCode(final CurrencyCode currencyCode) {
         throw new UnsupportedOperationException();
     }
 
