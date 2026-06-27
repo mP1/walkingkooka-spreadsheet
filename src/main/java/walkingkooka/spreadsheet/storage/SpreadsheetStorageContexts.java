90a9cdb167549121d5fbd96537fda031ac853d4f
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2026-06-27 20:05:56 +1000

    SpreadsheetStorageContexts.basic

--- src/main/java/walkingkooka/spreadsheet/storage/SpreadsheetStorageContexts.java
+++ src/main/java/walkingkooka/spreadsheet/storage/SpreadsheetStorageContexts.java
@@ -19,8 +19,34 @@ package walkingkooka.spreadsheet.storage;
 
 import walkingkooka.reflect.PublicStaticHelper;
 import walkingkooka.spreadsheet.SpreadsheetContext;
+import walkingkooka.spreadsheet.engine.SpreadsheetEngine;
+import walkingkooka.spreadsheet.engine.SpreadsheetEngineContext;
+import walkingkooka.spreadsheet.environment.SpreadsheetEnvironmentContext;
+import walkingkooka.spreadsheet.meta.SpreadsheetId;
+import walkingkooka.spreadsheet.meta.SpreadsheetMetadataContext;
+import walkingkooka.storage.StorageContext;
+
+import java.util.Optional;
+import java.util.function.Function;
 
 public final class SpreadsheetStorageContexts implements PublicStaticHelper {
+
+    /**
+     * {@see BasicSpreadsheetStorageContext}
+     */
+    public static SpreadsheetStorageContext basic(final SpreadsheetEngine spreadsheetEngine,
+                                                  final SpreadsheetEnvironmentContext spreadsheetEnvironmentContext,
+                                                  final Function<SpreadsheetId, Optional<SpreadsheetEngineContext>> spreadsheetIdToSpreadsheetEngineContext,
+                                                  final SpreadsheetMetadataContext spreadsheetMetadataContext,
+                                                  final StorageContext storageContext) {
+        return BasicSpreadsheetStorageContext.with(
+            spreadsheetEngine,
+            spreadsheetEnvironmentContext,
+            spreadsheetIdToSpreadsheetEngineContext,
+            spreadsheetMetadataContext,
+            storageContext
+        );
+    }
 
     /**
      * {@see FakeSpreadsheetStorageContext}
