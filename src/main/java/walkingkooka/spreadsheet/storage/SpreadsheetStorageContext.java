a6abbb6aaba9c5707eb9d9d972980762df60001c
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2026-08-14 17:28:01 +1000

    SpreadsheetStorageContext.parseStoragePath default method removed
    - Closes https://github.com/mP1/walkingkooka-spreadsheet/issues/9772
    - SpreadsheetStorageContext.parseStoragePath remove

--- src/main/java/walkingkooka/spreadsheet/storage/SpreadsheetStorageContext.java
+++ src/main/java/walkingkooka/spreadsheet/storage/SpreadsheetStorageContext.java
@@ -27,7 +27,6 @@ import walkingkooka.spreadsheet.reference.SpreadsheetLabelName;
 import walkingkooka.spreadsheet.validation.SpreadsheetValidationReference;
 import walkingkooka.spreadsheet.value.SpreadsheetCell;
 import walkingkooka.storage.StorageContext;
-import walkingkooka.storage.StoragePath;
 import walkingkooka.store.StoreWatcher;
 import walkingkooka.validation.form.Form;
 import walkingkooka.validation.form.FormName;
@@ -89,16 +88,6 @@ public interface SpreadsheetStorageContext extends StorageContext,
     Runnable addLabelWatcher(final StoreWatcher<SpreadsheetLabelMapping> watcher);
 
     Runnable addLabelWatcherOnce(final StoreWatcher<SpreadsheetLabelMapping> watcher);
-
-    // StorageContext...................................................................................................
-
-    @Override
-    default StoragePath parseStoragePath(final String text) {
-        return StoragePath.parseSpecial(
-            text,
-            this // HasUserDirectories
-        );
-    }
 
     // SpreadsheetEnvironmentContext....................................................................................
 
