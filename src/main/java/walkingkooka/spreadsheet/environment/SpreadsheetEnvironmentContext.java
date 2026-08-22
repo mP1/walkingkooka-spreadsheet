ee4a459cf17bb85a029299a5b688430745a9ecbd
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2026-08-22 18:52:40 +1000

    https://github.com/mP1/walkingkooka-environment/pull/434 EnvironmentContext.ENVIRONMENT_CONTEXT_PARSE deleted
    - https://github.com/mP1/walkingkooka-environment/pull/434
    - EnvironmentContext.ENVIRONMENT_CONTEXT_PARSE deleted

--- src/main/java/walkingkooka/spreadsheet/environment/SpreadsheetEnvironmentContext.java
+++ src/main/java/walkingkooka/spreadsheet/environment/SpreadsheetEnvironmentContext.java
@@ -17,7 +17,6 @@
 
 package walkingkooka.spreadsheet.environment;
 
-import walkingkooka.environment.CanParseEnvironmentValueName;
 import walkingkooka.environment.EnvironmentContext;
 import walkingkooka.environment.EnvironmentValueName;
 import walkingkooka.net.AbsoluteUrl;
@@ -26,9 +25,7 @@ import walkingkooka.spreadsheet.net.HasSpreadsheetServerUrl;
 import walkingkooka.spreadsheet.storage.SpreadsheetStorageContext;
 import walkingkooka.storage.Storage;
 import walkingkooka.storage.StorageEnvironmentContext;
-import walkingkooka.terminal.TerminalContext;
 
-import java.util.Objects;
 import java.util.Optional;
 
 /**
@@ -46,29 +43,6 @@ public interface SpreadsheetEnvironmentContext extends StorageEnvironmentContext
         "spreadsheetId",
         SpreadsheetId.class
     );
-
-    /**
-     * A {@link CanParseEnvironmentValueName} that only works for {@link SpreadsheetEnvironmentContext}.
-     */
-    CanParseEnvironmentValueName SPREADSHEET_ENVIRONMENT_CONTEXT_PARSE = (final String name) -> {
-        Objects.requireNonNull(name, "name");
-
-        final EnvironmentValueName<?> environmentValueName;
-
-        // assumes Case insensitive
-        switch (name.toLowerCase()) {
-            case "serverurl":
-                environmentValueName = SERVER_URL;
-                break;
-            case "spreadsheetid":
-                environmentValueName = SPREADSHEET_ID;
-                break;
-            default:
-                environmentValueName = TerminalContext.TERMINAL_CONTEXT_PARSE.parseEnvironmentValueName(name);
-        }
-
-        return environmentValueName;
-    };
 
     /**
      * The {@link SpreadsheetId} that identifies a spreadsheet.
