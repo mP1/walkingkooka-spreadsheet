d4a6b41eff9a13c7b3a3d441f1f8fa0f6db77cee
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2026-09-05 08:01:59 +1000

    SpreadsheetCurrencyExchangeRaterContext

---
+++ src/main/java/walkingkooka/spreadsheet/currency/SpreadsheetCurrencyExchangeRaterContext.java
@@ -0,0 +1,6 @@
+package walkingkooka.spreadsheet.currency;
+
+import walkingkooka.currency.CurrencyExchangeRaterContext;
+
+public interface SpreadsheetCurrencyExchangeRaterContext extends CurrencyExchangeRaterContext {
+}
