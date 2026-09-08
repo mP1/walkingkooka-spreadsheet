eef25441dfa9b3041587109ab045f834a5bb41ed
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2026-09-08 13:42:05 +1000

    SpreadsheetConverterContext unextends EnvironmentConverterContext
    - StorageConverterContext extends EnvironmentConverterContext

--- src/main/java/walkingkooka/spreadsheet/convert/SpreadsheetConverterContextTesting.java
+++ src/main/java/walkingkooka/spreadsheet/convert/SpreadsheetConverterContextTesting.java
@@ -17,15 +17,13 @@
 
 package walkingkooka.spreadsheet.convert;
 
-import walkingkooka.environment.convert.EnvironmentConverterContextTesting;
 import walkingkooka.spreadsheet.meta.SpreadsheetMetadataLoaderTesting;
 import walkingkooka.spreadsheet.provider.SpreadsheetProviderContextTesting;
 import walkingkooka.spreadsheet.reference.SpreadsheetLabelNameResolverTesting;
 import walkingkooka.storage.convert.StorageConverterContextTesting;
 import walkingkooka.tree.json.convert.JsonNodeConverterContextTesting;
 
-public interface SpreadsheetConverterContextTesting extends EnvironmentConverterContextTesting,
-    JsonNodeConverterContextTesting,
+public interface SpreadsheetConverterContextTesting extends JsonNodeConverterContextTesting,
     SpreadsheetLabelNameResolverTesting,
     SpreadsheetMetadataLoaderTesting,
     StorageConverterContextTesting,
