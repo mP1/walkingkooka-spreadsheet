730d2a4b7b2e62f8fb4878f92b1eea76070553cf
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2026-09-26 18:52:10 +1000

    SpreadsheetFormula.parserError()

--- src/main/java/walkingkooka/spreadsheet/formula/SpreadsheetFormula.java
+++ src/main/java/walkingkooka/spreadsheet/formula/SpreadsheetFormula.java
@@ -429,6 +429,15 @@ public final class SpreadsheetFormula implements CanBeEmpty,
     public Optional<SpreadsheetError> formatterError() {
         return this.error.filter(SpreadsheetError::isFormatter);
     }
+
+    // parserError......................................................................................................
+
+    /**
+     * If any present error is a {@link SpreadsheetError#isParser()} it will be returned.
+     */
+    public Optional<SpreadsheetError> parserError() {
+        return this.error.filter(SpreadsheetError::isParser);
+    }
     
     // validationError..................................................................................................
 
