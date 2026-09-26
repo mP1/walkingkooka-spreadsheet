c33f1a8f3f355be96001aec8d5d4144f7a1c9aed
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2026-09-26 12:39:12 +1000

    https://github.com/mP1/walkingkooka-environment/pull/576 EnvironmentContextSharedMap fire value change debug message
    - https://github.com/mP1/walkingkooka-environment/pull/576
    - EnvironmentContextSharedMap fire value change debug message

--- src/test/java/walkingkooka/spreadsheet/SpreadsheetContextSharedTestCase.java
+++ src/test/java/walkingkooka/spreadsheet/SpreadsheetContextSharedTestCase.java
@@ -454,7 +454,8 @@ public abstract class SpreadsheetContextSharedTestCase<C extends SpreadsheetCont
         context.debug(MESSAGE1);
 
         this.checkEquals(
-            "DEBUG " + MESSAGE1 + LINE_ENDING,
+            "DEBUG fire value change loggingLevel=NONE to loggingLevel=DEBUG\n" +
+                "DEBUG Message111\n",
             b.toString()
         );
     }
